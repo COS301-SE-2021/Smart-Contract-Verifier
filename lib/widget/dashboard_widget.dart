520e87aa2a46425d0e14d552beb4a79abf284d6e
Kevin <kevindupreez8@gmail.com>
2021-05-30 14:28:09 +0200

    Dynamically add to dashboard

--- lib/widget/dashboard_widget.dart
+++ lib/widget/dashboard_widget.dart
@@ -1,17 +1,31 @@
 import 'package:flutter/material.dart';
 
 class Dashboard extends StatelessWidget {
+  final dashDataList = [
+    {'name': 'Contract A','percentage':10},
+    {'name': 'Contract B','percentage':40},
+    {'name': 'Contract C','percentage':60},
+  ];
+
   @override
   Widget build(BuildContext context) {
+    var name;
+    var percentage;
     return GridView.count(
-        crossAxisCount: 2,
-        children: List.generate(100, (index) {
+      crossAxisCount: 1,
+      children: List.generate(
+        dashDataList.length,
+        (index) {
+          name = dashDataList[index]['name'];
+          percentage = dashDataList[index]['percentage'];
           return Center(
             child: Text(
-              'Item $index',
+              "$name $percentage",
               style: Theme.of(context).textTheme.headline5,
             ),
           );
-        }));
+        },
+      ),
+    );
   }
 }
