931d255198a6ef9ec623eb94050c5ef1839ab745
Kaleb-Bruwer <u19064242@tuks.co.za>
2021-05-31 15:43:05 +0200

    Created acceptAgreement method

--- src/contracts/Verifier.sol
+++ src/contracts/Verifier.sol
@@ -9,16 +9,25 @@ contract Verifier{
     struct Agreement{
         address party1;
         address party2;
+        bool accepted;
     }
 
     // Non-existent entries will return a struct filled with 0's
     mapping(uint => Agreement) agreements;
 
     function createAgreement(address party2) public returns(uint agreementID){
-        
-        agreements[nextAgreeID] = Agreement(msg.sender, party2);
+        agreements[nextAgreeID] = Agreement(msg.sender, party2, false);
         nextAgreeID++;
         return nextAgreeID - 1;
+    }
+
+    function acceptAgreement(uint agreeID) public{
+        // if(agreements[agreeID].party1 == address(0))
+        //     return;
+
+        if(msg.sender == agreements[agreeID].party2){
+            agreements[agreeID].accepted = true;
+        }
     }
 
     function getAgreement(uint agreeID) public view returns(Agreement memory){
