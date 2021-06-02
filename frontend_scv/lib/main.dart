15c0e8638b9e52c5a4dc808ff9adfb7cf9f54935
Kevin <kevindupreez8@gmail.com>
2021-06-02 09:13:37 +0200

    Dev branch added

--- frontend_scv/lib/main.dart
+++ frontend_scv/lib/main.dart
@@ -5,7 +5,7 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+  // DEV BRANCH
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
