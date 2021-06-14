2a956a0c1696b4acfe9af7fcd1325c4f027ac5e7
JasonMaritz <u19053292@tuks.co.za>
2021-06-14 22:26:25 +0200

    changed ID field of MessageStatus.kt from Int to UUID

--- Smart-Contract-Verifier-Server/src/main/kotlin/com/savannasolutions/SmartContractVerifierServer/models/MessageStatus.kt
+++ Smart-Contract-Verifier-Server/src/main/kotlin/com/savannasolutions/SmartContractVerifierServer/models/MessageStatus.kt
@@ -5,7 +5,7 @@ import javax.persistence.*
 
 @Entity
 @Table(name = "MessageStatus")
-data class MessageStatus(@Id @GeneratedValue val MessageStatusID:Int,
+data class MessageStatus(@Id @GeneratedValue val MessageStatusID:UUID,
                          @OneToOne val MessagesID:Messages,
                          @OneToOne val RecipientID:User,
                          val ReadDate:Date,)
