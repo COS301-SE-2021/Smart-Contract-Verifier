44d1f201078d48badae3038167c008e8e69da195
Cobus Redelinghuys <u19089296@tuks.co.za>
2021-06-14 22:28:24 +0200

    Changed contactListID from string to UUID in ContactList.kt

--- Smart-Contract-Verifier-Server/src/main/kotlin/com/savannasolutions/SmartContractVerifierServer/models/ContactList.kt
+++ Smart-Contract-Verifier-Server/src/main/kotlin/com/savannasolutions/SmartContractVerifierServer/models/ContactList.kt
@@ -1,11 +1,12 @@
 package com.savannasolutions.SmartContractVerifierServer.models
 
+import java.util.*
 import javax.persistence.Entity
 import javax.persistence.Id
 import javax.persistence.Table
 
 @Entity
 @Table(name = "ContactList")
-data class ContactList(@Id val contactListID: String,
+data class ContactList(@Id val contactListID: UUID,
                         val ownerID: String,
                         var contactListName: String)
