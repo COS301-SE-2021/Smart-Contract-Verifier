35508ca93be0ddc60238a422e24f16eacaf5a5aa
Cobus Redelinghuys <u19089296@tuks.co.za>
2021-06-14 22:20:26 +0200

    Added the @Table(name = "User") annotation to the User.kt file

--- Smart-Contract-Verifier-Server/src/main/kotlin/com/savannasolutions/SmartContractVerifierServer/models/User.kt
+++ Smart-Contract-Verifier-Server/src/main/kotlin/com/savannasolutions/SmartContractVerifierServer/models/User.kt
@@ -2,6 +2,7 @@ package com.savannasolutions.SmartContractVerifierServer.models
 import javax.persistence.*
 
 @Entity
+@Table(name = "User")
 data class User(@Id val publicWalletID: String,
                 val email: String,
                 val alias: String,
