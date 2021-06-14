2f8f90fdc075a61fd9f0de7fa1d63d54e8134f8d
Cobus Redelinghuys <u19089296@tuks.co.za>
2021-06-14 21:51:33 +0200

    Added the @Table(name = "ContactList") annotation to the ContactList.kt file

--- Smart-Contract-Verifier-Server/src/main/kotlin/com/savannasolutions/SmartContractVerifierServer/models/ContactList.kt
+++ Smart-Contract-Verifier-Server/src/main/kotlin/com/savannasolutions/SmartContractVerifierServer/models/ContactList.kt
@@ -2,8 +2,10 @@ package com.savannasolutions.SmartContractVerifierServer.models
 
 import javax.persistence.Entity
 import javax.persistence.Id
+import javax.persistence.Table
 
 @Entity
+@Table(name = "ContactList")
 data class ContactList(@Id val contactListID: String,
                         val ownerID: String,
                         var contactListName: String)
