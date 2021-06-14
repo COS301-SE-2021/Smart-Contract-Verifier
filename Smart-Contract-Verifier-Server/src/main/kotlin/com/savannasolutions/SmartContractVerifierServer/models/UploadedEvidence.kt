3a5fdd0661b059963c876cf94d254cd1c8a41d40
Cobus Redelinghuys <u19089296@tuks.co.za>
2021-06-14 22:11:24 +0200

    Added the @Table(name = "UploadedEvidence") annotation to the UploadedEvidence.kt file

--- Smart-Contract-Verifier-Server/src/main/kotlin/com/savannasolutions/SmartContractVerifierServer/models/UploadedEvidence.kt
+++ Smart-Contract-Verifier-Server/src/main/kotlin/com/savannasolutions/SmartContractVerifierServer/models/UploadedEvidence.kt
@@ -3,8 +3,10 @@ package com.savannasolutions.SmartContractVerifierServer.models
 import javax.persistence.Entity
 import javax.persistence.Id
 import javax.persistence.OneToOne
+import javax.persistence.Table
 
 @Entity
+@Table(name = "UploadedEvidence")
 data class UploadedEvidence(@OneToOne @Id val evidenceID: Evidence,
                             val filename: String,
                             val fileMimeType: String,)
