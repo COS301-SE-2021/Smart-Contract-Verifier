2f18d4b38a1130b6b05243694befd4ccf9be33f1
Cobus Redelinghuys <u19089296@tuks.co.za>
2021-06-14 21:54:13 +0200

    Added the @Table(name = "LinkedEvidence") annotation to the LinkedEvidence.kt file

--- Smart-Contract-Verifier-Server/src/main/kotlin/com/savannasolutions/SmartContractVerifierServer/models/LinkedEvidence.kt
+++ Smart-Contract-Verifier-Server/src/main/kotlin/com/savannasolutions/SmartContractVerifierServer/models/LinkedEvidence.kt
@@ -3,7 +3,9 @@ package com.savannasolutions.SmartContractVerifierServer.models
 import javax.persistence.Entity
 import javax.persistence.Id
 import javax.persistence.OneToOne
+import javax.persistence.Table
 
 @Entity
+@Table(name = "LinkedEvidence")
 data class LinkedEvidence(@OneToOne @Id val evidenceID: Evidence,
                             val evidenceUrl: String,)
