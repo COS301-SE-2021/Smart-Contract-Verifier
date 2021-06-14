6e5fe4bd42c279d9d841ecd8738e02fcf2db0a4e
Cobus Redelinghuys <u19089296@tuks.co.za>
2021-06-14 22:52:34 +0200

    Moved UploadedEvidenceRepository.kt to correct directory

--- Smart-Contract-Verifier-Server/src/main/kotlin/com/savannasolutions/SmartContractVerifierServer/repositories/UploadedEvidenceRepository.kt
+++ Smart-Contract-Verifier-Server/src/main/kotlin/com/savannasolutions/SmartContractVerifierServer/repositories/UploadedEvidenceRepository.kt
@@ -1,5 +1,6 @@
-package com.savannasolutions.SmartContractVerifierServer.models
+package com.savannasolutions.SmartContractVerifierServer.repositories
 
+import com.savannasolutions.SmartContractVerifierServer.models.UploadedEvidence
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 import java.util.*
