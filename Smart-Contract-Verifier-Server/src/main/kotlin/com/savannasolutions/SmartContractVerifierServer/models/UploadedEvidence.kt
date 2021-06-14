d56e7a11d320cf68ecedefb72136d7efe36c20cf
JasonMaritz <u19053292@tuks.co.za>
2021-06-14 21:55:28 +0200

    fixed UploadedEvidence.kt to have serializable ID

--- Smart-Contract-Verifier-Server/src/main/kotlin/com/savannasolutions/SmartContractVerifierServer/models/UploadedEvidence.kt
+++ Smart-Contract-Verifier-Server/src/main/kotlin/com/savannasolutions/SmartContractVerifierServer/models/UploadedEvidence.kt
@@ -1,10 +1,12 @@
 package com.savannasolutions.SmartContractVerifierServer.models
 
 import javax.persistence.Entity
+import javax.persistence.GeneratedValue
 import javax.persistence.Id
 import javax.persistence.OneToOne
 
 @Entity
-data class UploadedEvidence(@OneToOne @Id val evidenceID: Evidence,
+data class UploadedEvidence(@Id @GeneratedValue val evidenceID: Int,
+                            @OneToOne val evidence: Evidence,
                             val filename: String,
                             val fileMimeType: String,)
