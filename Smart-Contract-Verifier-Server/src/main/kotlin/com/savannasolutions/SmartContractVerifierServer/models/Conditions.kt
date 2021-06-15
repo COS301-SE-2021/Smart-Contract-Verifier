9771d1edad271258a478837065f8f41247fe1ed8
JasonMaritz <u19053292@tuks.co.za>
2021-06-15 22:43:10 +0200

    Used generated value in Conditions.kt

--- Smart-Contract-Verifier-Server/src/main/kotlin/com/savannasolutions/SmartContractVerifierServer/models/Conditions.kt
+++ Smart-Contract-Verifier-Server/src/main/kotlin/com/savannasolutions/SmartContractVerifierServer/models/Conditions.kt
@@ -1,18 +1,15 @@
 package com.savannasolutions.SmartContractVerifierServer.models
 
 import java.util.*
-import javax.persistence.Entity
-import javax.persistence.Id
-import javax.persistence.OneToOne
-import javax.persistence.Table
+import javax.persistence.*
 
 enum class ConditionStatus{ACCEPTED, REJECTED, PENDING}
 
 @Entity
 @Table(name = "Conditions")
-data class Conditions(@Id val conditionID: UUID,
-                        var conditionDescription: String,
-                        var conditionStatus: Enum<ConditionStatus>,
-                        val proposingUser: String,
-                        val proposalDate: Date,
-                        @OneToOne val contract: Agreements,)
+data class Conditions(@Id @GeneratedValue val conditionID: UUID,
+                      var conditionDescription: String,
+                      var conditionStatus: Enum<ConditionStatus>,
+                      val proposingUser: String,
+                      val proposalDate: Date,
+                      @OneToOne val contract: Agreements,)
