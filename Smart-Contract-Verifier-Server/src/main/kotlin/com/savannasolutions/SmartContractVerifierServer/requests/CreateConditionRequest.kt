efa23e0c102cd2b829398019152692dbc9c8548c
Cobus Redelinghuys <u19089296@tuks.co.za>
2021-06-15 22:51:33 +0200

    Created data class CreateConditionRequest with constructor

---
+++ Smart-Contract-Verifier-Server/src/main/kotlin/com/savannasolutions/SmartContractVerifierServer/requests/CreateConditionRequest.kt
@@ -0,0 +1,3 @@
+package com.savannasolutions.SmartContractVerifierServer.requests
+
+data class CreateConditionRequest(val PreposedUser: String, val AgreementID: String, val ConditionDescription: String)
