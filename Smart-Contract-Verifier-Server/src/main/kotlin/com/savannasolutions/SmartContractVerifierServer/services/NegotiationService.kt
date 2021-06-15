9ea75db7ea5991de85e96914cbc436743e7385dc
JasonMaritz <u19053292@tuks.co.za>
2021-06-15 23:31:08 +0200

    added empty function stubs for acceptCondition, createAgreement, createCondition, and getAgreementDetails

--- Smart-Contract-Verifier-Server/src/main/kotlin/com/savannasolutions/SmartContractVerifierServer/services/NegotiationService.kt
+++ Smart-Contract-Verifier-Server/src/main/kotlin/com/savannasolutions/SmartContractVerifierServer/services/NegotiationService.kt
@@ -2,6 +2,11 @@ package com.savannasolutions.SmartContractVerifierServer.services
 
 import com.savannasolutions.SmartContractVerifierServer.repositories.AgreementsRepository
 import com.savannasolutions.SmartContractVerifierServer.repositories.ConditionsRepository
+import com.savannasolutions.SmartContractVerifierServer.requests.AcceptConditionRequest
+import com.savannasolutions.SmartContractVerifierServer.requests.CreateAgreementRequest
+import com.savannasolutions.SmartContractVerifierServer.requests.CreateConditionRequest
+import com.savannasolutions.SmartContractVerifierServer.requests.GetAgreementDetailsRequest
+import com.savannasolutions.SmartContractVerifierServer.responses.GetAgreementDetailsResponse
 import org.springframework.stereotype.Service
 
 
@@ -9,5 +14,13 @@ import org.springframework.stereotype.Service
 class NegotiationService constructor(val agreementsRepository: AgreementsRepository,
                                      val conditionsRepository: ConditionsRepository,
                                      ){
+
+    fun acceptCondition(acceptConditionRequest: AcceptConditionRequest): AcceptConditionRequest{}
+
+    fun createAgreement(createAgreementRequest: CreateAgreementRequest): CreateAgreementRequest {}
+
+    fun createCondition(createConditionRequest: CreateConditionRequest): CreateConditionRequest{}
+
+    fun getAgreementDetails(getAgreementDetailsRequest: GetAgreementDetailsRequest): GetAgreementDetailsResponse{}
 
 }
