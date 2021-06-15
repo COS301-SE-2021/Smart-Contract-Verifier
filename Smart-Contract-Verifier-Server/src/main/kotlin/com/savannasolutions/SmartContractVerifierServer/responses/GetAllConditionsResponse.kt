58d7ebf014045acb3be752897a22687240c8f5c8
JasonMaritz <u19053292@tuks.co.za>
2021-06-15 23:11:26 +0200

    Defined GetAllConditionsResponse.kt

---
+++ Smart-Contract-Verifier-Server/src/main/kotlin/com/savannasolutions/SmartContractVerifierServer/responses/GetAllConditionsResponse.kt
@@ -0,0 +1,5 @@
+package com.savannasolutions.SmartContractVerifierServer.responses
+
+import com.savannasolutions.SmartContractVerifierServer.models.Conditions
+
+data class GetAllConditionsResponse(val conditions: List<Conditions>,)
