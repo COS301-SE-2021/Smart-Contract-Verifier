20ae5b2e4ed912591acac229d8809df4faeef1db
Cobus Redelinghuys <u19089296@tuks.co.za>
2021-06-15 23:16:35 +0200

    Changed conditionID from String to UUID

--- Smart-Contract-Verifier-Server/src/main/kotlin/com/savannasolutions/SmartContractVerifierServer/requests/RejectConditionRequest.kt
+++ Smart-Contract-Verifier-Server/src/main/kotlin/com/savannasolutions/SmartContractVerifierServer/requests/RejectConditionRequest.kt
@@ -1,3 +1,5 @@
 package com.savannasolutions.SmartContractVerifierServer.requests
 
-data class RejectConditionRequest(val conditionID:String)
+import java.util.*
+
+data class RejectConditionRequest(val conditionID:UUID)
