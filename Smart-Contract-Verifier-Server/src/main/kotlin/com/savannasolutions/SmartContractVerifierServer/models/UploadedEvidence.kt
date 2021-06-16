f342c34adb74fedb400979a36c19df8a3dc1a53d
JasonMaritz <u19053292@tuks.co.za>
2021-06-16 23:17:07 +0200

    Added lazyFetch option

--- Smart-Contract-Verifier-Server/src/main/kotlin/com/savannasolutions/SmartContractVerifierServer/models/UploadedEvidence.kt
+++ Smart-Contract-Verifier-Server/src/main/kotlin/com/savannasolutions/SmartContractVerifierServer/models/UploadedEvidence.kt
@@ -1,16 +1,12 @@
 package com.savannasolutions.SmartContractVerifierServer.models
 
 import java.util.*
-import javax.persistence.Entity
-import javax.persistence.GeneratedValue
-import javax.persistence.Id
-import javax.persistence.OneToOne
-import javax.persistence.Table
+import javax.persistence.*
 
 @Entity
 @Table(name = "UploadedEvidence")
 data class UploadedEvidence(@Id @GeneratedValue val evidenceID: UUID,
-                            @OneToOne val evidence: Evidence,
+                            @OneToOne(fetch = FetchType.LAZY) val evidence: Evidence,
                             val filename: String,
                             val fileMimeType: String,)
 
