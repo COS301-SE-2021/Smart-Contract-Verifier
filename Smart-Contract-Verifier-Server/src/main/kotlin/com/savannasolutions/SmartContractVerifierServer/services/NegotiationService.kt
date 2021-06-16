f0fc8c84a30564b01def40591aef57e8d3444690
JasonMaritz <u19053292@tuks.co.za>
2021-06-16 23:04:51 +0200

    Added implementations for acceptCondition, rejectCondition, and getAllConditions

--- Smart-Contract-Verifier-Server/src/main/kotlin/com/savannasolutions/SmartContractVerifierServer/services/NegotiationService.kt
+++ Smart-Contract-Verifier-Server/src/main/kotlin/com/savannasolutions/SmartContractVerifierServer/services/NegotiationService.kt
@@ -1,11 +1,11 @@
 package com.savannasolutions.SmartContractVerifierServer.services
 
+import com.savannasolutions.SmartContractVerifierServer.models.ConditionStatus
+import com.savannasolutions.SmartContractVerifierServer.models.Conditions
 import com.savannasolutions.SmartContractVerifierServer.repositories.AgreementsRepository
 import com.savannasolutions.SmartContractVerifierServer.repositories.ConditionsRepository
 import com.savannasolutions.SmartContractVerifierServer.requests.*
-import com.savannasolutions.SmartContractVerifierServer.responses.GetAgreementDetailsResponse
-import com.savannasolutions.SmartContractVerifierServer.responses.GetAllConditionsResponse
-import com.savannasolutions.SmartContractVerifierServer.responses.SealAgreementResponse
+import com.savannasolutions.SmartContractVerifierServer.responses.*
 import org.springframework.stereotype.Service
 
 
@@ -14,7 +14,13 @@ class NegotiationService constructor(val agreementsRepository: AgreementsReposit
                                      val conditionsRepository: ConditionsRepository,
                                      ){
 
-    fun acceptCondition(acceptConditionRequest: AcceptConditionRequest): AcceptConditionRequest? = null
+    fun acceptCondition(acceptConditionRequest: AcceptConditionRequest): AcceptConditionResponse{
+        if(conditionsRepository.existsById(acceptConditionRequest.conditionID)){
+            conditionsRepository.getById(acceptConditionRequest.conditionID).conditionStatus = ConditionStatus.ACCEPTED
+            return AcceptConditionResponse(ResponseStatus.SUCCESSFUL)
+        }
+        return AcceptConditionResponse(ResponseStatus.FAILED)
+    }
 
     fun createAgreement(createAgreementRequest: CreateAgreementRequest): CreateAgreementRequest? = null
 
@@ -22,9 +28,19 @@ class NegotiationService constructor(val agreementsRepository: AgreementsReposit
 
     fun getAgreementDetails(getAgreementDetailsRequest: GetAgreementDetailsRequest): GetAgreementDetailsResponse? = null
 
-    fun rejectCondition(rejectConditionRequest: RejectConditionRequest): RejectConditionRequest? = null
+    fun rejectCondition(rejectConditionRequest: RejectConditionRequest): RejectConditionResponse {
+        if(conditionsRepository.existsById(rejectConditionRequest.conditionID)){
+            conditionsRepository.getById(rejectConditionRequest.conditionID).conditionStatus = ConditionStatus.REJECTED
+            return RejectConditionResponse(ResponseStatus.SUCCESSFUL)
+        }
+        return RejectConditionResponse(ResponseStatus.FAILED)
+    }
 
-    fun getAllConditions(getAllConditionsRequest: GetAllConditionsRequest):GetAllConditionsResponse? = null
+    fun getAllConditions(getAllConditionsRequest: GetAllConditionsRequest):GetAllConditionsResponse{
+        if(agreementsRepository.existsById(getAllConditionsRequest.AgreementID))
+            return GetAllConditionsResponse(agreementsRepository.getById(getAllConditionsRequest.AgreementID).conditions)
+        return GetAllConditionsResponse(emptyList())
+    }
 
     fun sealAgreement(sealAgreementRequest: SealAgreementRequest): SealAgreementResponse? = null
 }
