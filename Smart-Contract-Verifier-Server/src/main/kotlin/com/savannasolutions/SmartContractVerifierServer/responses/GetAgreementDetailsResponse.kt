af23528a44ba0fe973c460c0d98abb8d23dfbfb5
Cobus Redelinghuys <u19089296@tuks.co.za>
2021-06-17 23:07:25 +0200

    Changed the getAgreementDetailsResponse to have a status field and such that the following fields can be nullable: duration, partyA, partyB, createdDate, sealedDate, movedToBlockchain and conditions

--- Smart-Contract-Verifier-Server/src/main/kotlin/com/savannasolutions/SmartContractVerifierServer/responses/GetAgreementDetailsResponse.kt
+++ Smart-Contract-Verifier-Server/src/main/kotlin/com/savannasolutions/SmartContractVerifierServer/responses/GetAgreementDetailsResponse.kt
@@ -5,10 +5,12 @@ import java.time.Duration
 import java.util.*
 
 data class GetAgreementDetailsResponse(val agreementID: UUID,
-                                        val duration: Duration,
-                                        val partyA: String,
-                                        val partyB: String,
-                                        val createdDate: Date,
-                                        val sealedDate: Date,
-                                        val movedToBlockchain: Boolean,
-                                        val conditions: List<Conditions>,)
+                                        val duration: Duration?,
+                                        val partyA: String?,
+                                        val partyB: String?,
+                                        val createdDate: Date?,
+                                        val sealedDate: Date?,
+                                        val movedToBlockchain: Boolean?,
+                                        val conditions: List<Conditions>?,
+                                        val status: Enum<ResponseStatus>,)
+
