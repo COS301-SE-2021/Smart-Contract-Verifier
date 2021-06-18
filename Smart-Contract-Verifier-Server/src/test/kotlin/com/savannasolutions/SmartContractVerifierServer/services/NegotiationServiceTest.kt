3c3e7f18d31ab91ead214c127e736c135d988707
Cobus Redelinghuys <u19089296@tuks.co.za>
2021-06-18 18:47:24 +0200

    Setup empty NegotiationServiceTest file for testing

---
+++ Smart-Contract-Verifier-Server/src/test/kotlin/com/savannasolutions/SmartContractVerifierServer/services/NegotiationServiceTest.kt
@@ -0,0 +1,58 @@
+package com.savannasolutions.SmartContractVerifierServer.services
+
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+
+internal class NegotiationServiceTest {
+
+    @BeforeEach
+    fun setUp() {
+    }
+
+    @AfterEach
+    fun tearDown() {
+    }
+
+    @Test
+    fun acceptCondition() {
+    }
+
+    @Test
+    fun createAgreement() {
+    }
+
+    @Test
+    fun createCondition() {
+    }
+
+    @Test
+    fun getAgreementDetails() {
+    }
+
+    @Test
+    fun rejectCondition() {
+    }
+
+    @Test
+    fun getAllConditions() {
+    }
+
+    @Test
+    fun sealAgreement() {
+    }
+
+    @Test
+    fun getAgreementsRepository() {
+    }
+
+    @Test
+    fun getConditionsRepository() {
+    }
+
+    @Test
+    fun getUserRepository() {
+    }
+}
