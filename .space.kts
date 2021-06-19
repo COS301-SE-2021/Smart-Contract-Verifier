abf0a40ea9640b2bf9f03f189ce293bca0ed7147
JasonMaritz <u19053292@tuks.co.za>
2021-06-19 14:12:24 +0200

    Debugging space connection problems

--- .space.kts
+++ .space.kts
@@ -15,6 +15,9 @@ job("Build and run tests") {
     	shellScript{
         	content = """
             			cd ./Smart-Contract-Verifier-Server
+						echo "pinging DB"
+						ping -c 10 postgresql://db-postgresql-nyc3-98163-do-user-8880908-0.b.db.ondigitalocean.com
+						echo "ending ping"
 						mvn clean install
                     """
         }
