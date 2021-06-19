5c9f48e12a97e5edc45915d9e07b3d918b6c68f1
Cobus Redelinghuys <u19089296@tuks.co.za>
2021-06-19 14:26:39 +0200

    Fixed capitalization for JSONProperty

--- Smart-Contract-Verifier-Server/src/main/kotlin/com/savannasolutions/SmartContractVerifierServer/requests/AcceptConditionRequest.kt
+++ Smart-Contract-Verifier-Server/src/main/kotlin/com/savannasolutions/SmartContractVerifierServer/requests/AcceptConditionRequest.kt
@@ -3,4 +3,4 @@ package com.savannasolutions.SmartContractVerifierServer.requests
 import com.fasterxml.jackson.annotation.JsonProperty
 import java.util.*
 
-data class AcceptConditionRequest(@JsonProperty("conditionID") val conditionID: UUID,)
+data class AcceptConditionRequest(@JsonProperty("ConditionID") val conditionID: UUID,)
