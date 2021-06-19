ac3f6a70dcfd1316dc1592a7825f2c29ddb9474e
Cobus Redelinghuys <u19089296@tuks.co.za>
2021-06-19 13:27:50 +0200

    Added the getConditionDetailsResponse response object

---
+++ Smart-Contract-Verifier-Server/src/main/kotlin/com/savannasolutions/SmartContractVerifierServer/responses/GetConditionDetailsResponse.kt
@@ -0,0 +1,9 @@
+package com.savannasolutions.SmartContractVerifierServer.responses
+
+import java.util.*
+
+data class GetConditionDetailsResponse(val conditionID: UUID,
+                                       val conditionDescription: String,
+                                       val proposingUser : String,
+                                        val proposalDate : Date,
+                                        val agreementID : UUID)
