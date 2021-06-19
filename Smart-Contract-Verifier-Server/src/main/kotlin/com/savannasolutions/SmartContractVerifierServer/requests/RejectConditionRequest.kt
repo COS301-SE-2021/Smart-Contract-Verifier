a553ccb45f4a71e8563b43511db144307819318d
Cobus Redelinghuys <u19089296@tuks.co.za>
2021-06-19 13:07:55 +0200

    Added the @JSONProperty annotation

--- Smart-Contract-Verifier-Server/src/main/kotlin/com/savannasolutions/SmartContractVerifierServer/requests/RejectConditionRequest.kt
+++ Smart-Contract-Verifier-Server/src/main/kotlin/com/savannasolutions/SmartContractVerifierServer/requests/RejectConditionRequest.kt
@@ -1,5 +1,6 @@
 package com.savannasolutions.SmartContractVerifierServer.requests
 
+import com.fasterxml.jackson.annotation.JsonProperty
 import java.util.*
 
-data class RejectConditionRequest(val conditionID:UUID,)
+data class RejectConditionRequest(@JsonProperty("conditionID") val conditionID:UUID,)
