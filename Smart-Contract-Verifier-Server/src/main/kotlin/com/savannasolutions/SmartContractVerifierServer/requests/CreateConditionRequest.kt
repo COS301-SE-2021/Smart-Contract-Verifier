1ba7829988552bba2a1f8049c480bdd29dc90ef5
Cobus Redelinghuys <u19089296@tuks.co.za>
2021-06-19 14:54:13 +0200

    Fixed spelling mistake with the JSONProperty annotation

--- Smart-Contract-Verifier-Server/src/main/kotlin/com/savannasolutions/SmartContractVerifierServer/requests/CreateConditionRequest.kt
+++ Smart-Contract-Verifier-Server/src/main/kotlin/com/savannasolutions/SmartContractVerifierServer/requests/CreateConditionRequest.kt
@@ -3,6 +3,6 @@ package com.savannasolutions.SmartContractVerifierServer.requests
 import com.fasterxml.jackson.annotation.JsonProperty
 import java.util.*
 
-data class CreateConditionRequest(@JsonProperty("PreposedUser") val PreposedUser: String,
+data class CreateConditionRequest(@JsonProperty("ProposedUser") val PreposedUser: String,
                                   @JsonProperty("AgreementID") val AgreementID: UUID,
                                   @JsonProperty("ConditionDescription") val ConditionDescription: String,)
