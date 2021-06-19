ba107c18a7429bf9ee517dc9945dbadf72fb9289
JasonMaritz <u19053292@tuks.co.za>
2021-06-19 14:45:13 +0200

    Debugging space connection problems

--- .space.kts
+++ .space.kts
@@ -13,6 +13,7 @@ job("Build and run tests") {
 		shellScript{
 			content = """
             			echo "pinging DB"
+						apt install inetutils-ping
 						ping -c 10 postgresql://db-postgresql-nyc3-98163-do-user-8880908-0.b.db.ondigitalocean.com
 						echo "ending ping"
                     """
