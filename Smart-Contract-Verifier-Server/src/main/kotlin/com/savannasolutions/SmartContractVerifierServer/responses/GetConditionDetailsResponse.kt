0a11683f04fcd37f2691f43e647e9ffe4941f2c1
Cobus Redelinghuys <u19089296@tuks.co.za>
2021-06-20 03:13:04 +0200

    Added the conditionStatus result field

--- Smart-Contract-Verifier-Server/src/main/kotlin/com/savannasolutions/SmartContractVerifierServer/responses/GetConditionDetailsResponse.kt
+++ Smart-Contract-Verifier-Server/src/main/kotlin/com/savannasolutions/SmartContractVerifierServer/responses/GetConditionDetailsResponse.kt
@@ -1,5 +1,6 @@
 package com.savannasolutions.SmartContractVerifierServer.responses
 
+import com.savannasolutions.SmartContractVerifierServer.models.ConditionStatus
 import java.util.*
 
 data class GetConditionDetailsResponse(val conditionID: UUID,
@@ -7,4 +8,5 @@ data class GetConditionDetailsResponse(val conditionID: UUID,
                                        val proposingUser : String?,
                                         val proposalDate : Date?,
                                         val agreementID : UUID?,
-                                        val status : ResponseStatus)
+                                       val conditionStatus: Enum<ConditionStatus>?,
+                                        val status : ResponseStatus,)
