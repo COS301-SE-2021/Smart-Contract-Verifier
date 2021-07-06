54c875762b76cbb18bdbae313e14785f21710af1
Cobus Redelinghuys <u19089296@tuks.co.za>
2021-07-06 18:39:09 +0200

    Implemented JPA relations between:  *)User and Condition  *)User and ContactListProfile  *)User and ContactList

--- Smart-Contract-Verifier-Server/src/main/kotlin/com/savannasolutions/SmartContractVerifierServer/user/models/User.kt
+++ Smart-Contract-Verifier-Server/src/main/kotlin/com/savannasolutions/SmartContractVerifierServer/user/models/User.kt
@@ -1,5 +1,6 @@
 package com.savannasolutions.SmartContractVerifierServer.user.models
 import com.savannasolutions.SmartContractVerifierServer.negotiation.models.Agreements
+import com.savannasolutions.SmartContractVerifierServer.negotiation.models.Conditions
 import javax.persistence.*
 
 @Entity
@@ -9,4 +10,13 @@ data class User(@Id val publicWalletID: String,
 {
                 @OneToMany(fetch = FetchType.LAZY, mappedBy = "user", orphanRemoval = true, cascade = [CascadeType.ALL])
                 var agreements: List<Agreements>? = emptyList()
+
+                @OneToMany(fetch = FetchType.LAZY, mappedBy = "user", orphanRemoval = true, cascade = [CascadeType.ALL])
+                var conditions: List<Conditions>? = emptyList()
+
+                @OneToMany(fetch = FetchType.LAZY, mappedBy = "user", orphanRemoval = true, cascade = [CascadeType.ALL])
+                var contactListProfiles : List<ContactListProfile>? = emptyList()
+
+                @OneToMany(fetch = FetchType.LAZY, mappedBy = "user", orphanRemoval = true, cascade = [CascadeType.ALL])
+                var contactList : List<ContactList>?= emptyList()
 }
