2e8367baa1dcf784b53c04eb0e61c59f43ce3cc4
Cobus Redelinghuys <u19089296@tuks.co.za>
2021-07-06 17:29:27 +0200

    Added default value to createAgreementResponse constructor

--- Smart-Contract-Verifier-Server/src/main/kotlin/com/savannasolutions/SmartContractVerifierServer/negotiation/responses/CreateAgreementResponse.kt
+++ Smart-Contract-Verifier-Server/src/main/kotlin/com/savannasolutions/SmartContractVerifierServer/negotiation/responses/CreateAgreementResponse.kt
@@ -3,5 +3,5 @@ package com.savannasolutions.SmartContractVerifierServer.negotiation.responses
 import com.savannasolutions.SmartContractVerifierServer.common.ResponseStatus
 import java.util.*
 
-data class CreateAgreementResponse(val agreementID: UUID?,
+data class CreateAgreementResponse(val agreementID: UUID? = null,
                                    val  status: Enum<ResponseStatus>,)
