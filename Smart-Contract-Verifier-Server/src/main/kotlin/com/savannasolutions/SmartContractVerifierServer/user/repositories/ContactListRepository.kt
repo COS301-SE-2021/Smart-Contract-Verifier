2d54db797be4ef51012c06c1315e542bd8670d50
Cobus Redelinghuys <u19089296@tuks.co.za>
2021-07-06 20:31:39 +0200

    Added the existsByOwnerAndContactListName function to the ContactListRepository

--- Smart-Contract-Verifier-Server/src/main/kotlin/com/savannasolutions/SmartContractVerifierServer/user/repositories/ContactListRepository.kt
+++ Smart-Contract-Verifier-Server/src/main/kotlin/com/savannasolutions/SmartContractVerifierServer/user/repositories/ContactListRepository.kt
@@ -1,9 +1,12 @@
 package com.savannasolutions.SmartContractVerifierServer.user.repositories
 
 import com.savannasolutions.SmartContractVerifierServer.user.models.ContactList
+import com.savannasolutions.SmartContractVerifierServer.user.models.User
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 import java.util.*
 
 @Repository
-interface ContactListRepository: JpaRepository<ContactList, UUID>
+interface ContactListRepository: JpaRepository<ContactList, UUID>{
+    fun existsByOwnerAndContactListName(owner: User, contactListName:String):Boolean
+}
