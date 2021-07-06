06c3c72e4cc71a842f8903675619d02dd0432676
Cobus Redelinghuys <u19089296@tuks.co.za>
2021-07-06 22:11:07 +0200

    Implemented the removeUserFromContactList and retrieveUserAgreement functions

--- Smart-Contract-Verifier-Server/src/main/kotlin/com/savannasolutions/SmartContractVerifierServer/user/services/ContactListService.kt
+++ Smart-Contract-Verifier-Server/src/main/kotlin/com/savannasolutions/SmartContractVerifierServer/user/services/ContactListService.kt
@@ -1,6 +1,7 @@
 package com.savannasolutions.SmartContractVerifierServer.user.services
 
 import com.savannasolutions.SmartContractVerifierServer.common.ResponseStatus
+import com.savannasolutions.SmartContractVerifierServer.negotiation.repositories.AgreementsRepository
 import com.savannasolutions.SmartContractVerifierServer.user.models.ContactList
 import com.savannasolutions.SmartContractVerifierServer.user.models.ContactListProfile
 import com.savannasolutions.SmartContractVerifierServer.user.repositories.ContactListProfileRepository
@@ -9,11 +10,13 @@ import com.savannasolutions.SmartContractVerifierServer.user.repositories.UserRe
 import com.savannasolutions.SmartContractVerifierServer.user.requests.*
 import com.savannasolutions.SmartContractVerifierServer.user.responses.*
 import org.springframework.stereotype.Service
+import java.util.*
+import kotlin.collections.ArrayList
 
 @Service
 class ContactListService(   val contactListRepository: ContactListRepository,
                             val userRepository: UserRepository,
-                            val contactListProfileRepository: ContactListProfileRepository,){
+                            val contactListProfileRepository: ContactListProfileRepository, ){
 
     fun addUserToContactList(addUserToContactListRequest: AddUserToContactListRequest ): AddUserToContactListResponse{
         if(addUserToContactListRequest.UserAlias.isEmpty())
@@ -73,15 +76,64 @@ class ContactListService(   val contactListRepository: ContactListRepository,
     }
 
     fun removeUserFromContactList(removeUserFromContactListRequest: RemoveUserFromContactListRequest): RemoveUserFromContactListResponse{
-        return RemoveUserFromContactListResponse(ResponseStatus.FAILED)
+        if(removeUserFromContactListRequest.RemoveUserID.isEmpty())
+            return RemoveUserFromContactListResponse(ResponseStatus.FAILED)
+
+        if(!userRepository.existsById(removeUserFromContactListRequest.RemoveUserID))
+            return RemoveUserFromContactListResponse(ResponseStatus.FAILED)
+
+        if(!contactListRepository.existsById(removeUserFromContactListRequest.ContactListID))
+            return RemoveUserFromContactListResponse(ResponseStatus.FAILED)
+
+        val user = userRepository.getById(removeUserFromContactListRequest.RemoveUserID)
+        val contactList = contactListRepository.getById(removeUserFromContactListRequest.ContactListID)
+
+        if(!contactListProfileRepository.exitsByContactListAndUser(contactList, user))
+            return RemoveUserFromContactListResponse(ResponseStatus.FAILED)
+
+        val contactListProfile = contactListProfileRepository.getByContactListAndUser(contactList, user)!!
+
+        contactListProfileRepository.delete(contactListProfile)
+
+        if(contactListProfileRepository.existsById(contactListProfile.ProfileID!!))
+            return RemoveUserFromContactListResponse(ResponseStatus.FAILED)
+
+        return RemoveUserFromContactListResponse(ResponseStatus.SUCCESSFUL)
     }
 
-    fun retrieveContactList(retrieveContactListRequest: RetrieveContactListRequest):RetrieveContactListResponse{
-        return RetrieveContactListResponse(status = ResponseStatus.FAILED)
+    fun retrieveContactList(retrieveContactListRequest: RetrieveContactListRequest):RetrieveContactListResponse
+    {
+        if(!contactListRepository.existsById(retrieveContactListRequest.ContactListID))
+            return RetrieveContactListResponse(status = ResponseStatus.FAILED)
+
+        val contactList = contactListRepository.getById(retrieveContactListRequest.ContactListID)
+        contactList.contactListProfiles?: return RetrieveContactListResponse(emptyList(), ResponseStatus.SUCCESSFUL)
+
+        val list = ArrayList<Pair<UUID,String>>()
+
+        for(profiles in contactList.contactListProfiles!!)
+            list.add(Pair(profiles.ProfileID!!,profiles.ContactAlias))
+
+        return RetrieveContactListResponse(list,ResponseStatus.SUCCESSFUL)
     }
 
     fun retrieveUserAgreements(retrieveUserAgreementsRequest: RetrieveUserAgreementsRequest): RetrieveUserAgreementsResponse{
-        return RetrieveUserAgreementsResponse(status = ResponseStatus.FAILED)
+        if(retrieveUserAgreementsRequest.UserID.isEmpty())
+            return RetrieveUserAgreementsResponse(status = ResponseStatus.FAILED)
+
+        if(!userRepository.existsById(retrieveUserAgreementsRequest.UserID))
+            return RetrieveUserAgreementsResponse(status = ResponseStatus.FAILED)
+
+        val user = userRepository.getById(retrieveUserAgreementsRequest.UserID)
+
+        user.agreements?:return RetrieveUserAgreementsResponse(emptyList(),ResponseStatus.SUCCESSFUL)
+
+        val list = ArrayList<UUID>()
+
+        for(agreement in user.agreements!!)
+            list.add(agreement.ContractID)
+
+        return RetrieveUserAgreementsResponse(list,ResponseStatus.SUCCESSFUL)
     }
 
     fun retrieveUserContactLists(retrieveUserContactListRequest: RetrieveUserContactListRequest): RetrieveUserContactListResponse{
