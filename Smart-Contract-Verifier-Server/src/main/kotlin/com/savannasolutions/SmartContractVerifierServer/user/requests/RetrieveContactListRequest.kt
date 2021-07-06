4188dbf13f3037aa872f2f444da10f4698b49745
Cobus Redelinghuys <u19089296@tuks.co.za>
2021-07-06 22:12:30 +0200

    Fixed the RetrieveContactListRequest constructor structure

--- Smart-Contract-Verifier-Server/src/main/kotlin/com/savannasolutions/SmartContractVerifierServer/user/requests/RetrieveContactListRequest.kt
+++ Smart-Contract-Verifier-Server/src/main/kotlin/com/savannasolutions/SmartContractVerifierServer/user/requests/RetrieveContactListRequest.kt
@@ -1,5 +1,6 @@
 package com.savannasolutions.SmartContractVerifierServer.user.requests
 
 import com.fasterxml.jackson.annotation.JsonProperty
+import java.util.*
 
-data class RetrieveContactListRequest(@JsonProperty("UserID") val UserID: String,)
+data class RetrieveContactListRequest(@JsonProperty("ContactListID") val ContactListID : UUID,)
