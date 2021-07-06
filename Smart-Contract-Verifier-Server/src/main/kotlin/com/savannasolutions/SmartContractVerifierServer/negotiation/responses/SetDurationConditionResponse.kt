12bd2660a14130711848347374ed54231004abeb
Cobus Redelinghuys <u19089296@tuks.co.za>
2021-07-06 17:35:02 +0200

    Added the default values for the SetDurationConditionResponse constructor

--- Smart-Contract-Verifier-Server/src/main/kotlin/com/savannasolutions/SmartContractVerifierServer/negotiation/responses/SetDurationConditionResponse.kt
+++ Smart-Contract-Verifier-Server/src/main/kotlin/com/savannasolutions/SmartContractVerifierServer/negotiation/responses/SetDurationConditionResponse.kt
@@ -3,5 +3,5 @@ package com.savannasolutions.SmartContractVerifierServer.negotiation.responses
 import com.savannasolutions.SmartContractVerifierServer.common.ResponseStatus
 import java.util.*
 
-data class SetDurationConditionResponse(val conditionID: UUID?,
+data class SetDurationConditionResponse(val conditionID: UUID? = null,
                                         val  status: Enum<ResponseStatus>,)
