b37f3a17999f1d8e3938aadc1b3399e3d9c16198
Cobus Redelinghuys <u19089296@tuks.co.za>
2021-07-07 18:41:51 +0200

    Removed email property from constructor

--- Smart-Contract-Verifier-Server/src/main/kotlin/com/savannasolutions/SmartContractVerifierServer/user/requests/AddUserRequest.kt
+++ Smart-Contract-Verifier-Server/src/main/kotlin/com/savannasolutions/SmartContractVerifierServer/user/requests/AddUserRequest.kt
@@ -2,5 +2,4 @@ package com.savannasolutions.SmartContractVerifierServer.user.requests
 
 import com.fasterxml.jackson.annotation.JsonProperty
 
-data class AddUserRequest(@JsonProperty("UserEmail") val UserEmail: String,
-                          @JsonProperty("WalletID") val WalletID: String,)
+data class AddUserRequest(@JsonProperty("WalletID") val WalletID: String,)
