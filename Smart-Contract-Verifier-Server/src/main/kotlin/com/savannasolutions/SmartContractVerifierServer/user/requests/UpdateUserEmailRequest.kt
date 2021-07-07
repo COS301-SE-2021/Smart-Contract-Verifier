3c81aec4a04e4ebbf0120f5da178ac5e380fb1a3
Cobus Redelinghuys <u19089296@tuks.co.za>
2021-07-07 18:46:34 +0200

    Removed as email wont be used

--- Smart-Contract-Verifier-Server/src/main/kotlin/com/savannasolutions/SmartContractVerifierServer/user/requests/UpdateUserEmailRequest.kt
+++
@@ -1,6 +0,0 @@
-package com.savannasolutions.SmartContractVerifierServer.user.requests
-
-import com.fasterxml.jackson.annotation.JsonProperty
-
-data class UpdateUserEmailRequest(@JsonProperty("UserID") val UserID: String,
-                                  @JsonProperty("ReplacementEmail") val ReplacementEmail: String,)
