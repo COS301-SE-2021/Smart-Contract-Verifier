cfe69ad1050a6d6a249fa2c357aa9c3cb5f5886a
Cobus Redelinghuys <u19089296@tuks.co.za>
2021-07-07 15:22:21 +0200

    Implemented 6 unit tests for RemoveUserFromContactList

--- Smart-Contract-Verifier-Server/src/test/kotlin/com/savannasolutions/SmartContractVerifierServer/user/services/ContactListServiceTest.kt
+++ Smart-Contract-Verifier-Server/src/test/kotlin/com/savannasolutions/SmartContractVerifierServer/user/services/ContactListServiceTest.kt
@@ -9,6 +9,7 @@ import com.savannasolutions.SmartContractVerifierServer.user.repositories.Contac
 import com.savannasolutions.SmartContractVerifierServer.user.repositories.UserRepository
 import com.savannasolutions.SmartContractVerifierServer.user.requests.AddUserToContactListRequest
 import com.savannasolutions.SmartContractVerifierServer.user.requests.CreateContactListRequest
+import com.savannasolutions.SmartContractVerifierServer.user.requests.RemoveUserFromContactListRequest
 import org.junit.Test
 import org.mockito.kotlin.any
 import org.mockito.kotlin.mock
@@ -306,7 +307,7 @@ class ContactListServiceTest {
     fun `CreateContactList exists by owner and contact list name returns true`(){
         //Given
         val user = User("0x743Fb032c0bE976e1178d8157f911a9e825d9E23", "test@test.com","TestA")
-        var contactList = ContactList(UUID.fromString("2b4dc93a-92f5-4425-9a11-073ce06d14c7"), "TestName")
+        val contactList = ContactList(UUID.fromString("2b4dc93a-92f5-4425-9a11-073ce06d14c7"), "TestName")
 
         whenever(userRepository.existsById(user.publicWalletID)).thenReturn(true)
         whenever(userRepository.getById(user.publicWalletID)).thenReturn(user)
@@ -315,6 +316,126 @@ class ContactListServiceTest {
 
         //When
         val response = contactListService.createContactList(CreateContactListRequest(user.publicWalletID,contactList.contactListName))
+
+        //Then
+        assertEquals(response.status, ResponseStatus.FAILED)
+    }
+
+    @Test
+    fun `RemoveUserFromContactList successful`()
+    {
+        //Given
+        val user = User("0x743Fb032c0bE976e1178d8157f911a9e825d9E23", "test@test.com","TestA")
+        val contactList = ContactList(UUID.fromString("2b4dc93a-92f5-4425-9a11-073ce06d14c7"), "TestName")
+        val contactListProfile = ContactListProfile(UUID.fromString("8a4904c5-0ffb-4567-88de-233b160d7ddc"), "Test")
+        contactListProfile.apply { this.contactList = contactList }
+        contactListProfile.apply { this.user = user }
+
+
+        whenever(userRepository.existsById(user.publicWalletID)).thenReturn(true)
+        whenever(userRepository.getById(user.publicWalletID)).thenReturn(user)
+        whenever(contactListRepository.existsById(contactList.contactListID!!)).thenReturn(true)
+        whenever(contactListRepository.getById(contactList.contactListID!!)).thenReturn(contactList)
+        whenever(contactListProfileRepository.exitsByContactListAndUser(contactList,user)).thenReturn(true)
+        whenever(contactListProfileRepository.getByContactListAndUser(contactList,user)).thenReturn(contactListProfile)
+        whenever(contactListProfileRepository.existsById(contactListProfile.ProfileID!!)).thenReturn(false)
+
+        //When
+        val response = contactListService.removeUserFromContactList(RemoveUserFromContactListRequest(user.publicWalletID,contactList.contactListID!!))
+
+        //Then
+        assertEquals(response.status, ResponseStatus.SUCCESSFUL)
+    }
+
+    @Test
+    fun `RemoveUserFromContactList userid is empty`()
+    {
+        //Given
+        val user = User("0x743Fb032c0bE976e1178d8157f911a9e825d9E23", "test@test.com","TestA")
+        val contactList = ContactList(UUID.fromString("2b4dc93a-92f5-4425-9a11-073ce06d14c7"), "TestName")
+
+        //When
+        val response = contactListService.removeUserFromContactList(RemoveUserFromContactListRequest("",contactList.contactListID!!))
+
+        //Then
+        assertEquals(response.status, ResponseStatus.FAILED)
+    }
+
+    @Test
+    fun `RemoveUserFromContactList userid does not exist`()
+    {
+        //Given
+        val user = User("0x743Fb032c0bE976e1178d8157f911a9e825d9E23", "test@test.com","TestA")
+        val contactList = ContactList(UUID.fromString("2b4dc93a-92f5-4425-9a11-073ce06d14c7"), "TestName")
+
+        whenever(userRepository.existsById(user.publicWalletID)).thenReturn(false)
+
+        //When
+        val response = contactListService.removeUserFromContactList(RemoveUserFromContactListRequest(user.publicWalletID,contactList.contactListID!!))
+
+        //Then
+        assertEquals(response.status, ResponseStatus.FAILED)
+    }
+
+    @Test
+    fun `RemoveUserFromContactList contactlist does not exist`()
+    {
+        //Given
+        val user = User("0x743Fb032c0bE976e1178d8157f911a9e825d9E23", "test@test.com","TestA")
+        val contactList = ContactList(UUID.fromString("2b4dc93a-92f5-4425-9a11-073ce06d14c7"), "TestName")
+
+        whenever(userRepository.existsById(user.publicWalletID)).thenReturn(true)
+        whenever(userRepository.getById(user.publicWalletID)).thenReturn(user)
+        whenever(contactListRepository.existsById(contactList.contactListID!!)).thenReturn(false)
+
+        //When
+        val response = contactListService.removeUserFromContactList(RemoveUserFromContactListRequest(user.publicWalletID,contactList.contactListID!!))
+
+        //Then
+        assertEquals(response.status, ResponseStatus.FAILED)
+    }
+
+    @Test
+    fun `RemoveUserFromContactList exitsByContactListAndUser will return false`()
+    {
+        //Given
+        val user = User("0x743Fb032c0bE976e1178d8157f911a9e825d9E23", "test@test.com","TestA")
+        val contactList = ContactList(UUID.fromString("2b4dc93a-92f5-4425-9a11-073ce06d14c7"), "TestName")
+
+        whenever(userRepository.existsById(user.publicWalletID)).thenReturn(true)
+        whenever(userRepository.getById(user.publicWalletID)).thenReturn(user)
+        whenever(contactListRepository.existsById(contactList.contactListID!!)).thenReturn(true)
+        whenever(contactListRepository.getById(contactList.contactListID!!)).thenReturn(contactList)
+        whenever(contactListProfileRepository.exitsByContactListAndUser(contactList,user)).thenReturn(false)
+
+        //When
+        val response = contactListService.removeUserFromContactList(RemoveUserFromContactListRequest(user.publicWalletID,contactList.contactListID!!))
+
+        //Then
+        assertEquals(response.status, ResponseStatus.FAILED)
+    }
+
+    @Test
+    fun `RemoveUserFromContactList failed to delete from database, exits returns true`()
+    {
+        //Given
+        val user = User("0x743Fb032c0bE976e1178d8157f911a9e825d9E23", "test@test.com","TestA")
+        val contactList = ContactList(UUID.fromString("2b4dc93a-92f5-4425-9a11-073ce06d14c7"), "TestName")
+        val contactListProfile = ContactListProfile(UUID.fromString("8a4904c5-0ffb-4567-88de-233b160d7ddc"), "Test")
+        contactListProfile.apply { this.contactList = contactList }
+        contactListProfile.apply { this.user = user }
+
+
+        whenever(userRepository.existsById(user.publicWalletID)).thenReturn(true)
+        whenever(userRepository.getById(user.publicWalletID)).thenReturn(user)
+        whenever(contactListRepository.existsById(contactList.contactListID!!)).thenReturn(true)
+        whenever(contactListRepository.getById(contactList.contactListID!!)).thenReturn(contactList)
+        whenever(contactListProfileRepository.exitsByContactListAndUser(contactList,user)).thenReturn(true)
+        whenever(contactListProfileRepository.getByContactListAndUser(contactList,user)).thenReturn(contactListProfile)
+        whenever(contactListProfileRepository.existsById(contactListProfile.ProfileID!!)).thenReturn(true)
+
+        //When
+        val response = contactListService.removeUserFromContactList(RemoveUserFromContactListRequest(user.publicWalletID,contactList.contactListID!!))
 
         //Then
         assertEquals(response.status, ResponseStatus.FAILED)
