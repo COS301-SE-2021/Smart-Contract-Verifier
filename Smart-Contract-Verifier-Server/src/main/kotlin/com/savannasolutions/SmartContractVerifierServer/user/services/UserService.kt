3063dc989147a18ea843aa1f5d40d02cfee03f91
Cobus Redelinghuys <u19089296@tuks.co.za>
2021-07-07 19:23:37 +0200

    Moved retrieveUserAgreements from contactListService to userService

--- Smart-Contract-Verifier-Server/src/main/kotlin/com/savannasolutions/SmartContractVerifierServer/user/services/UserService.kt
+++ Smart-Contract-Verifier-Server/src/main/kotlin/com/savannasolutions/SmartContractVerifierServer/user/services/UserService.kt
@@ -4,9 +4,13 @@ import com.savannasolutions.SmartContractVerifierServer.common.ResponseStatus
 import com.savannasolutions.SmartContractVerifierServer.negotiation.repositories.AgreementsRepository
 import com.savannasolutions.SmartContractVerifierServer.user.repositories.UserRepository
 import com.savannasolutions.SmartContractVerifierServer.user.requests.AddUserRequest
+import com.savannasolutions.SmartContractVerifierServer.user.requests.RetrieveUserAgreementsRequest
 import com.savannasolutions.SmartContractVerifierServer.user.requests.RetrieveUserInfoRequest
 import com.savannasolutions.SmartContractVerifierServer.user.responses.AddUserResponse
+import com.savannasolutions.SmartContractVerifierServer.user.responses.RetrieveUserAgreementsResponse
 import com.savannasolutions.SmartContractVerifierServer.user.responses.RetrieveUserInfoResponse
+import java.util.*
+import kotlin.collections.ArrayList
 
 class UserService(  val userRepository: UserRepository,
                     val agreementsRepository: AgreementsRepository) {
@@ -17,5 +21,25 @@ class UserService(  val userRepository: UserRepository,
 
     fun addUser(addUserRequest: AddUserRequest): AddUserResponse {
         return AddUserResponse(ResponseStatus.FAILED)
+    }
+
+
+    fun retrieveUserAgreements(retrieveUserAgreementsRequest: RetrieveUserAgreementsRequest): RetrieveUserAgreementsResponse {
+        if(retrieveUserAgreementsRequest.UserID.isEmpty())
+            return RetrieveUserAgreementsResponse(status = ResponseStatus.FAILED)
+
+        if(!userRepository.existsById(retrieveUserAgreementsRequest.UserID))
+            return RetrieveUserAgreementsResponse(status = ResponseStatus.FAILED)
+
+        val user = userRepository.getById(retrieveUserAgreementsRequest.UserID)
+
+        user.agreements?:return RetrieveUserAgreementsResponse(emptyList(),ResponseStatus.SUCCESSFUL)
+
+        val list = ArrayList<UUID>()
+
+        for(agreement in user.agreements!!)
+            list.add(agreement.ContractID)
+
+        return RetrieveUserAgreementsResponse(list,ResponseStatus.SUCCESSFUL)
     }
 }
