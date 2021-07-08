371262f6e191f9be43c128d78d6408ddf6c2f3ca
Kevin <kevindupreez8@gmail.com>
2021-07-08 08:33:16 +0200

    Added Agreement Condition Description dialog

--- frontend_scv/lib/widgets/condition_item.dart
+++ frontend_scv/lib/widgets/condition_item.dart
@@ -12,30 +12,51 @@ class ConditionItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListTile(
-      title: Text(contractCondition.toString()),
-      leading: CircleAvatar(
-        backgroundColor: Colors.black12,
-        // backgroundImage: NetworkImage(contract.imageUrl),
-      ),
-      trailing: Row(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          IconButton(
-            icon: Icon(
-              Icons.thumb_down_outlined,
-              color: Colors.deepOrangeAccent,
+    _showConditionDialog(dynamic contractCondition) {
+      showDialog(
+          context: context,
+          builder: (_) => new AlertDialog(
+                title: new Text(contractCondition.toString()), //TODO
+                content: new Text("Condition description (TODO)"),
+                actions: <Widget>[
+                  TextButton(
+                    child: Text('Close'),
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                    },
+                  )
+                ],
+              ));
+    }
+
+    return GestureDetector(
+      onTap: () => _showConditionDialog(contractCondition),
+      child: ListTile(
+        title: Text(contractCondition.toString()),
+        leading: CircleAvatar(
+          backgroundColor: Colors.black12,
+          // backgroundImage: NetworkImage(contract.imageUrl),
+        ),
+        subtitle: Text('Status: Todo'),
+        trailing: Row(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            IconButton(
+              icon: Icon(
+                Icons.thumb_down_outlined,
+                color: Colors.deepOrangeAccent,
+              ),
+              onPressed: null,
             ),
-            onPressed: null,
-          ),
-          IconButton(
-            icon: Icon(
-              Icons.thumb_up_outlined,
-              color: Colors.cyan,
+            IconButton(
+              icon: Icon(
+                Icons.thumb_up_outlined,
+                color: Colors.cyan,
+              ),
+              onPressed: null,
             ),
-            onPressed: null,
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
