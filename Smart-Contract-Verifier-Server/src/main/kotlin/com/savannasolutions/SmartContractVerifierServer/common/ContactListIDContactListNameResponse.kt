3a0c1c0057833982d298f104ed5a1012bcebe5b7
Cobus Redelinghuys <u19089296@tuks.co.za>
2021-07-09 20:27:29 +0200

    Added custom response object for ContactListID and ContactListName

---
+++ Smart-Contract-Verifier-Server/src/main/kotlin/com/savannasolutions/SmartContractVerifierServer/common/ContactListIDContactListNameResponse.kt
@@ -0,0 +1,5 @@
+package com.savannasolutions.SmartContractVerifierServer.common
+
+import java.util.*
+
+data class ContactListIDContactListNameResponse(val contactListName : String, val contactListID: UUID)
