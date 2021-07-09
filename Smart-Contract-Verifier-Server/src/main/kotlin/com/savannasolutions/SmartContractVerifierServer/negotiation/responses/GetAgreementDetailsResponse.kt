08bd68bc7daa3d567c88e80aacee929eee41fdf7
Cobus Redelinghuys <u19089296@tuks.co.za>
2021-07-09 20:32:21 +0200

    Implemented usage of custom response objects in response

--- Smart-Contract-Verifier-Server/src/main/kotlin/com/savannasolutions/SmartContractVerifierServer/negotiation/responses/GetAgreementDetailsResponse.kt
+++ Smart-Contract-Verifier-Server/src/main/kotlin/com/savannasolutions/SmartContractVerifierServer/negotiation/responses/GetAgreementDetailsResponse.kt
@@ -1,16 +1,9 @@
 package com.savannasolutions.SmartContractVerifierServer.negotiation.responses
 
+import com.savannasolutions.SmartContractVerifierServer.common.AgreementResponse
 import com.savannasolutions.SmartContractVerifierServer.common.ResponseStatus
+import com.savannasolutions.SmartContractVerifierServer.negotiation.models.Conditions
 import java.util.*
 
-data class GetAgreementDetailsResponse(val agreementID: UUID,
-                                       val durationID: UUID? = null,
-                                       val paymentID: UUID? = null,
-                                       val partyA: String? = null,
-                                       val partyB: String? = null,
-                                       val createdDate: Date? = null,
-                                       val sealedDate: Date? = null,
-                                       val movedToBlockchain: Boolean? = null,
-                                       val conditions: List<UUID>? = null,
-                                       val status: Enum<ResponseStatus>,)
+data class GetAgreementDetailsResponse(val agreementResponse : AgreementResponse?=null,val status: Enum<ResponseStatus>,)
 
