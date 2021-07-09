a889edf2853d7a3929f0346679845580074ae8bc
Cobus Redelinghuys <u19089296@tuks.co.za>
2021-07-09 21:38:08 +0200

    Updated response with custom response object

--- Smart-Contract-Verifier-Server/src/main/kotlin/com/savannasolutions/SmartContractVerifierServer/negotiation/responses/GetAllConditionsResponse.kt
+++ Smart-Contract-Verifier-Server/src/main/kotlin/com/savannasolutions/SmartContractVerifierServer/negotiation/responses/GetAllConditionsResponse.kt
@@ -1,7 +1,8 @@
 package com.savannasolutions.SmartContractVerifierServer.negotiation.responses
 
+import com.savannasolutions.SmartContractVerifierServer.common.ConditionResponse
 import com.savannasolutions.SmartContractVerifierServer.common.ResponseStatus
 import java.util.*
 
-data class GetAllConditionsResponse(val conditions: List<UUID>?,
+data class GetAllConditionsResponse(val conditions: List<ConditionResponse>?,
                                     val status: ResponseStatus,)
