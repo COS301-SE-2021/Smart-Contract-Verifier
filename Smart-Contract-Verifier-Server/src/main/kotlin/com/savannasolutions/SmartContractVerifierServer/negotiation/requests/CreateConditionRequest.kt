a48f91470387c4df58c52c51803eac7cd295a8f0
Cobus Redelinghuys <u19089296@tuks.co.za>
2021-07-09 20:59:40 +0200

    Added condition title as requested by front end

--- Smart-Contract-Verifier-Server/src/main/kotlin/com/savannasolutions/SmartContractVerifierServer/negotiation/requests/CreateConditionRequest.kt
+++ Smart-Contract-Verifier-Server/src/main/kotlin/com/savannasolutions/SmartContractVerifierServer/negotiation/requests/CreateConditionRequest.kt
@@ -4,5 +4,6 @@ import com.fasterxml.jackson.annotation.JsonProperty
 import java.util.*
 
 data class CreateConditionRequest(@JsonProperty("ProposedUser") val PreposedUser: String,
+                                  @JsonProperty("ConditionTitle") val Title : String,
                                   @JsonProperty("AgreementID") val AgreementID: UUID,
                                   @JsonProperty("ConditionDescription") val ConditionDescription: String,)
