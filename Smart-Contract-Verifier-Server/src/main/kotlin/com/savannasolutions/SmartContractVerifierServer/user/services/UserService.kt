8280ad3c8e7851c1bac5b361ac7cd79012de60e2
Cobus Redelinghuys <u19089296@tuks.co.za>
2021-07-09 20:38:50 +0200

    Implemented custom response objects

--- Smart-Contract-Verifier-Server/src/main/kotlin/com/savannasolutions/SmartContractVerifierServer/user/services/UserService.kt
+++ Smart-Contract-Verifier-Server/src/main/kotlin/com/savannasolutions/SmartContractVerifierServer/user/services/UserService.kt
@@ -1,24 +1,33 @@
 package com.savannasolutions.SmartContractVerifierServer.user.services
 
+import com.savannasolutions.SmartContractVerifierServer.common.AgreementResponse
+import com.savannasolutions.SmartContractVerifierServer.common.ConditionResponse
 import com.savannasolutions.SmartContractVerifierServer.common.ResponseStatus
+import com.savannasolutions.SmartContractVerifierServer.common.UserResponse
+import com.savannasolutions.SmartContractVerifierServer.negotiation.models.Conditions
 import com.savannasolutions.SmartContractVerifierServer.negotiation.repositories.AgreementsRepository
+import com.savannasolutions.SmartContractVerifierServer.negotiation.repositories.ConditionsRepository
 import com.savannasolutions.SmartContractVerifierServer.user.models.User
 import com.savannasolutions.SmartContractVerifierServer.user.repositories.UserRepository
 import com.savannasolutions.SmartContractVerifierServer.user.requests.AddUserRequest
 import com.savannasolutions.SmartContractVerifierServer.user.requests.RetrieveUserAgreementsRequest
 import com.savannasolutions.SmartContractVerifierServer.user.responses.AddUserResponse
 import com.savannasolutions.SmartContractVerifierServer.user.responses.RetrieveUserAgreementsResponse
+import org.springframework.stereotype.Service
 import java.util.*
 import kotlin.collections.ArrayList
 
+@Service
 class UserService(  val userRepository: UserRepository,
-                    val agreementsRepository: AgreementsRepository) {
+                    val agreementsRepository: AgreementsRepository,
+                    val conditionsRepository: ConditionsRepository) {
 
     fun addUser(addUserRequest: AddUserRequest): AddUserResponse {
-        if(userRepository.existsById(addUserRequest.WalletID))
+        val wID = addUserRequest.WalletID;
+        if(userRepository.existsById(wID))
             return AddUserResponse(status = ResponseStatus.FAILED)
 
-        val user = User(publicWalletID = addUserRequest.WalletID, alias = "")
+        val user = User(publicWalletID = addUserRequest.WalletID, alias = addUserRequest.Alias)
 
         userRepository.save(user)
 
@@ -37,10 +46,42 @@ class UserService(  val userRepository: UserRepository,
 
         user.agreements?:return RetrieveUserAgreementsResponse(emptyList(),ResponseStatus.SUCCESSFUL)
 
-        val list = ArrayList<UUID>()
+        val agreementList = agreementsRepository.getAllByPartyAOrPartyB(user,user)
+        val list = ArrayList<AgreementResponse>()
 
-        for(agreement in user.agreements!!)
-            list.add(agreement.ContractID)
+        for(agreement in agreementList)
+        {
+            val conditions = conditionsRepository.getAllByContract(agreement)
+            val conditionList = ArrayList<ConditionResponse>()
+            for (cond in conditions)
+            {
+                conditionList.add(
+                    ConditionResponse(cond.conditionID,
+                                        cond.conditionDescription,
+                                        UserResponse(cond.proposingUser.publicWalletID),
+                                        cond.proposalDate,
+                                        agreement.ContractID,
+                                        cond.conditionStatus
+                    )
+                )
+            }
+
+
+            val tempArg = AgreementResponse(agreement.ContractID,
+                                            agreement.AgreementTitle,
+                                            agreement.AgreementDescription,
+                                            agreement.DurationConditionUUID,
+                                            agreement.PaymentConditionUUID,
+                                            UserResponse(agreement.partyA.publicWalletID),
+                                            UserResponse(agreement.partyB.publicWalletID),
+                                            agreement.CreatedDate,
+                                            agreement.SealedDate,
+                                            agreement.MovedToBlockChain,
+                                            conditionList,
+                                            agreement.AgreementImageURL,)
+
+            list.add(tempArg)
+        }
 
         return RetrieveUserAgreementsResponse(list,ResponseStatus.SUCCESSFUL)
     }
