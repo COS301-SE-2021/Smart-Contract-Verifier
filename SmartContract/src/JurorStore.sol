949a3d5f307ca1c81116b8b0b50906b7663a1ada
Kaleb-Bruwer <u19064242@tuks.co.za>
2021-07-09 12:55:26 +0200

    Added removeJuror function to JuryStore

--- SmartContract/src/JurorStore.sol
+++ SmartContract/src/JurorStore.sol
@@ -18,6 +18,9 @@ contract JurorStore{
 
     constructor(address _owner){
         owner = _owner;
+
+        // index 0 is kept out of use since a 0 in the mapping means that value doesn't exist
+        jurors.push(address(0));
     }
 
     function addJuror(address j) public onlyOwner(){
@@ -27,6 +30,27 @@ contract JurorStore{
         emit AddJuror(j, index);
     }
 
+    function removeJuror(address j) public onlyOwner(){
+        // Address 0 can't be removed since:
+        // a) it isn't in jurorIndex so it's index is 0
+        // b) It's index really is 0 so it would return even if it were in the mapping
+
+        uint256 index = jurorIndex[j];
+        if(index == 0)
+            return;
+        
+        uint256 last = jurors.length -1;
+
+        if(index != last)
+            jurors[index] = jurors[last];
+
+        jurors.pop();
+        jurorIndex[j] = 0;
+
+        emit RemoveJuror(j);
+    }
+
     event AddJuror(address j, uint256 index);
+    event RemoveJuror(address j);
 
 }
