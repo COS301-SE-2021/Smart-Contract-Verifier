b79c62435f4e2d63a2f9e64e5abe8f4887ac81a5
Cobus Redelinghuys <u19089296@tuks.co.za>
2021-07-09 20:36:46 +0200

    Removed redundant curly brackets

--- Smart-Contract-Verifier-Server/src/main/kotlin/com/savannasolutions/SmartContractVerifierServer/user/repositories/UserRepository.kt
+++ Smart-Contract-Verifier-Server/src/main/kotlin/com/savannasolutions/SmartContractVerifierServer/user/repositories/UserRepository.kt
@@ -5,5 +5,4 @@ import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 
 @Repository
-interface UserRepository: JpaRepository<User, String> {
-}
+interface UserRepository: JpaRepository<User, String>
