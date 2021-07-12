f72efae0712882f655d82203dda73698236d59aa
Kaleb-Bruwer <u19064242@tuks.co.za>
2021-07-12 15:04:30 +0200

    Added getRandVal function that takes a seed

--- SmartContract/src/Randomness/RandomSource.sol
+++ SmartContract/src/Randomness/RandomSource.sol
@@ -7,8 +7,24 @@ pragma solidity ^0.8.0;
 contract RandomSource{
     // A pseudorandom value is calculated by taking the XOR of the last two block hashes 
     // XOR should maintain a 50/50 ratio between 0's and 1's, keeping the distribution uniform
+
+    // Only generates one random value per block
     function getRandVal() public view returns(uint256){
         uint256 newBlock = uint256(blockhash(block.number));
+
+        if(block.number-1 > 0){
+            // Should be the case every time, the check might be needed on fresh blockchains, though
+            uint256 oldBlock = uint256(blockhash(block.number-1));
+            newBlock  = newBlock ^ oldBlock;
+        }
+        return newBlock;
+    }
+
+    // Combines seed with block value, so seed is not repeatable in different blocks
+    function getRandVal(uint256 seed) public view returns(uint256){
+        uint256 newBlock = uint256(blockhash(block.number));
+        newBlock = newBlock * seed;
+
         if(block.number-1 > 0){
             uint256 oldBlock = uint256(blockhash(block.number-1));
             newBlock  = newBlock ^ oldBlock;
