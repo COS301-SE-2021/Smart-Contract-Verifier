ed769c60b5c7a046684a19e66e48a39aa97e58de
Cobus Redelinghuys <u19089296@tuks.co.za>
2021-07-28 13:16:08 +0200

    Added default value for messages parameter

--- Smart-Contract-Verifier-Server/src/main/kotlin/com/savannasolutions/SmartContractVerifierServer/messenger/responses/GetAllMessagesByAgreementResponse.kt
+++ Smart-Contract-Verifier-Server/src/main/kotlin/com/savannasolutions/SmartContractVerifierServer/messenger/responses/GetAllMessagesByAgreementResponse.kt
@@ -3,4 +3,4 @@ package com.savannasolutions.SmartContractVerifierServer.messenger.responses
 import com.savannasolutions.SmartContractVerifierServer.common.MessageResponse
 import com.savannasolutions.SmartContractVerifierServer.common.ResponseStatus
 
-data class GetAllMessagesByAgreementResponse(val messages: List<MessageResponse>?, val status: ResponseStatus)
+data class GetAllMessagesByAgreementResponse(val messages: List<MessageResponse>? = null, val status: ResponseStatus)
