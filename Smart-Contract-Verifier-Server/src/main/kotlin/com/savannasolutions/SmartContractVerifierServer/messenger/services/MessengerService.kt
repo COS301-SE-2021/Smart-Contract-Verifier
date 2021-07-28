c12b18532ac4ec36c07da3d574f2545dd0220dde
Cobus Redelinghuys <u19089296@tuks.co.za>
2021-07-28 13:17:59 +0200

    Added skeleton structure for service functions

--- Smart-Contract-Verifier-Server/src/main/kotlin/com/savannasolutions/SmartContractVerifierServer/messenger/services/MessengerService.kt
+++ Smart-Contract-Verifier-Server/src/main/kotlin/com/savannasolutions/SmartContractVerifierServer/messenger/services/MessengerService.kt
@@ -1,10 +1,34 @@
 package com.savannasolutions.SmartContractVerifierServer.messenger.services
 
+import com.savannasolutions.SmartContractVerifierServer.common.ResponseStatus
 import com.savannasolutions.SmartContractVerifierServer.messenger.repositories.MessageStatusRepository
 import com.savannasolutions.SmartContractVerifierServer.messenger.repositories.MessagesRepository
+import com.savannasolutions.SmartContractVerifierServer.messenger.requests.*
+import com.savannasolutions.SmartContractVerifierServer.messenger.responses.*
 import org.springframework.stereotype.Service
 
 @Service
 class MessengerService constructor(val messagesRepository: MessagesRepository,
                                     val messageStatusRepository: MessageStatusRepository,){
+
+    fun getAllMessagesByAgreement(getAllMessagesByAgreementRequest: GetAllMessagesByAgreementRequest): GetAllMessagesByAgreementResponse{
+        return GetAllMessagesByAgreementResponse(status = ResponseStatus.FAILED)
+    }
+
+    fun getAllMessagesByUser(getAllMessagesByUserRequest: GetAllMessagesByUserRequest): GetAllMessagesByUserResponse{
+        return GetAllMessagesByUserResponse(status = ResponseStatus.FAILED)
+    }
+
+    fun getMessageDetail(getMessageDetailRequest: GetMessageDetailRequest): GetMessageDetailResponse{
+        return GetMessageDetailResponse(status = ResponseStatus.FAILED)
+    }
+
+    fun sendMessage(sendMessageRequest: SendMessageRequest): SendMessageResponse{
+        return SendMessageResponse(status = ResponseStatus.FAILED)
+    }
+
+    fun setMessageAsRead(setMessageAsReadRequest: SetMessageAsReadRequest): SetMessageAsReadResponse{
+        return SetMessageAsReadResponse(status = ResponseStatus.FAILED)
+    }
+
 }
