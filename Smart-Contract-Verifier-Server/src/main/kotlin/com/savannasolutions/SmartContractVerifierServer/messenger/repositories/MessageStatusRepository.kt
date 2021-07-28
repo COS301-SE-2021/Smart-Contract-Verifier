a6641e234a3efcdb95a44129c2dd8925768b31f4
Cobus Redelinghuys <u19089296@tuks.co.za>
2021-07-28 19:58:13 +0200

    Added the getAllByMessage function

--- Smart-Contract-Verifier-Server/src/main/kotlin/com/savannasolutions/SmartContractVerifierServer/messenger/repositories/MessageStatusRepository.kt
+++ Smart-Contract-Verifier-Server/src/main/kotlin/com/savannasolutions/SmartContractVerifierServer/messenger/repositories/MessageStatusRepository.kt
@@ -1,10 +1,12 @@
 package com.savannasolutions.SmartContractVerifierServer.messenger.repositories
 
 import com.savannasolutions.SmartContractVerifierServer.messenger.models.MessageStatus
+import com.savannasolutions.SmartContractVerifierServer.messenger.models.Messages
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 import java.util.*
 
 @Repository
 interface MessageStatusRepository: JpaRepository<MessageStatus, UUID> {
+    fun getAllByMessage(messages: Messages): List<MessageStatus>?= emptyList()
 }
