53d57f7e81a4d66fb5d5ae905014e8a8ef9b8e82
Cobus Redelinghuys <u19089296@tuks.co.za>
2021-07-28 13:21:43 +0200

    Added default value for MessageID

--- Smart-Contract-Verifier-Server/src/main/kotlin/com/savannasolutions/SmartContractVerifierServer/messenger/responses/SendMessageResponse.kt
+++ Smart-Contract-Verifier-Server/src/main/kotlin/com/savannasolutions/SmartContractVerifierServer/messenger/responses/SendMessageResponse.kt
@@ -3,5 +3,5 @@ package com.savannasolutions.SmartContractVerifierServer.messenger.responses
 import com.savannasolutions.SmartContractVerifierServer.common.ResponseStatus
 import java.util.*
 
-data class SendMessageResponse(val MessageID: UUID?,
+data class SendMessageResponse(val MessageID: UUID? = null,
                                val status: ResponseStatus)
