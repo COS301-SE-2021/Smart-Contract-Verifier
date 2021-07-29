21e2dee87fa86c722c1ff4e6c21c1e192c5a1ee5
Cobus Redelinghuys <u19089296@tuks.co.za>
2021-07-29 15:54:07 +0200

    Added getAllByRecipient function

--- Smart-Contract-Verifier-Server/src/main/kotlin/com/savannasolutions/SmartContractVerifierServer/messenger/repositories/MessageStatusRepository.kt
+++ Smart-Contract-Verifier-Server/src/main/kotlin/com/savannasolutions/SmartContractVerifierServer/messenger/repositories/MessageStatusRepository.kt
@@ -2,6 +2,7 @@ package com.savannasolutions.SmartContractVerifierServer.messenger.repositories
 
 import com.savannasolutions.SmartContractVerifierServer.messenger.models.MessageStatus
 import com.savannasolutions.SmartContractVerifierServer.messenger.models.Messages
+import com.savannasolutions.SmartContractVerifierServer.user.models.User
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 import java.util.*
@@ -9,4 +10,5 @@ import java.util.*
 @Repository
 interface MessageStatusRepository: JpaRepository<MessageStatus, UUID> {
     fun getAllByMessage(messages: Messages): List<MessageStatus>?= emptyList()
+    fun getAllByRecipient(recipient : User): List<MessageStatus>?= emptyList()
 }
