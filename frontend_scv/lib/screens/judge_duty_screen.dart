571561432ec8f99a2ca7f029090601f53a19dce2
Kevin <kevindupreez8@gmail.com>
2021-07-29 08:30:07 +0200

    Created judge duty screen

---
+++ frontend_scv/lib/screens/judge_duty_screen.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart' show kIsWeb;
+import '../widgets/app_drawer.dart';
+import '../widgets/contracts_grid.dart';
+
+class JudgeDutyScreen extends StatelessWidget {
+  static const routeName = '/judge-duty';
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Judge Duty'),
+      ),
+      drawer: AppDrawer(),
+      body: Center(
+        child: ContractsGrid(),
+      ),
+    );
+  }
+}
