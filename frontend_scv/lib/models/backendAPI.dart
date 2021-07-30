1471e0ca604ce87f29535da4e7284f0855376894
Ronan <u19288736@tuks.co.za>
2021-07-30 12:48:45 +0200

    Added retry dependency, added backendAPI.dart

---
+++ frontend_scv/lib/models/backendAPI.dart
@@ -0,0 +1,19 @@
+import 'dart:convert';
+import 'package:retry/retry.dart';
+import 'package:http/http.dart';
+import 'package:flutter/material.dart';
+
+
+class apiInteraction {
+
+  Future<Map> get(String url) {
+
+
+
+  }
+
+  Future<Map> post(String url, Map json) {
+
+  }
+
+}
