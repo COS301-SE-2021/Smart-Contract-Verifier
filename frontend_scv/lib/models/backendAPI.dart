d083d47723df2122e467ffff23f4a6cb1043a24e
Ronan <u19288736@tuks.co.za>
2021-07-30 13:09:22 +0200

    Added get and post functionality to new apiInteraction class

--- frontend_scv/lib/models/backendAPI.dart
+++ frontend_scv/lib/models/backendAPI.dart
@@ -2,18 +2,31 @@ import 'dart:convert';
 import 'package:retry/retry.dart';
 import 'package:http/http.dart';
 import 'package:flutter/material.dart';
-
+import 'dart:async';
+import 'dart:io';
 
 class apiInteraction {
 
-  Future<Map> get(String url) {
+  final String baseUrl = "https://localhost:8080/"; //Url where the backend is deployed
 
+  Future<Map<dynamic, dynamic>> get(String url) async{ //Pass in url extension
 
+    final response = await RetryOptions(maxAttempts: 5).retry(
+          () => get(baseUrl + url).timeout(Duration(seconds: 5)),
+      retryIf: (e) => e is SocketException || e is TimeoutException,
+    );
 
+    return response;
   }
 
-  Future<Map> post(String url, Map json) {
+  Future<Map<dynamic, dynamic>> post(String url, Map json) async{ //Pass in url extension and json body
 
+    final response = await RetryOptions(maxAttempts: 5).retry(
+          () => post(baseUrl + url, json).timeout(Duration(seconds: 5)),
+      retryIf: (e) => e is SocketException || e is TimeoutException,
+    );
+
+    return response;
   }
 
 }
