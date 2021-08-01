870522edc5785de91909dd971067a9abad07b03f
JasonMaritz <u19053292@tuks.co.za>
2021-08-01 23:13:50 +0200

    Added endpoints for the evidence subsystem

--- Smart-Contract-Verifier-Server/src/main/kotlin/com/savannasolutions/SmartContractVerifierServer/evidence/controllers/EvidenceController.kt
+++ Smart-Contract-Verifier-Server/src/main/kotlin/com/savannasolutions/SmartContractVerifierServer/evidence/controllers/EvidenceController.kt
@@ -1,8 +1,11 @@
 package com.savannasolutions.SmartContractVerifierServer.evidence.controllers
 
 import com.savannasolutions.SmartContractVerifierServer.evidence.interfaces.EvidenceFileSystem
+import com.savannasolutions.SmartContractVerifierServer.evidence.requests.*
 import com.savannasolutions.SmartContractVerifierServer.evidence.services.EvidenceService
 import org.springframework.beans.factory.annotation.Qualifier
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -10,5 +13,25 @@ import org.springframework.web.bind.annotation.RestController
 @RequestMapping("/evidence")
 class EvidenceController constructor(private val evidenceService: EvidenceService,
                                      @Qualifier("evidenceFileSystemJIMFSImplementation") val evidenceFileSystem: EvidenceFileSystem,) {
+
+    @PostMapping("/upload")
+    fun uploadEvidence(@RequestBody uploadEvidenceRequest: UploadEvidenceRequest) =
+        evidenceService.uploadEvidence(uploadEvidenceRequest)
+
+    @PostMapping("/link")
+    fun linkEvidence(@RequestBody linkEvidenceRequest: LinkEvidenceRequest) =
+        evidenceService.linkEvidence(linkEvidenceRequest)
+
+    @PostMapping("/fetch")
+    fun uploadEvidence(@RequestBody fetchEvidenceRequest: FetchEvidenceRequest) =
+        evidenceService.fetchEvidence(fetchEvidenceRequest)
+
+    @PostMapping("/get-agreement-evidence")
+    fun uploadEvidence(@RequestBody getAllEvidenceRequest: GetAllEvidenceRequest) =
+        evidenceService.getAllEvidence(getAllEvidenceRequest)
+
+    @PostMapping("/remove")
+    fun uploadEvidence(@RequestBody removeEvidenceRequest: RemoveEvidenceRequest) =
+        evidenceService.removeEvidence(removeEvidenceRequest)
 
 }
