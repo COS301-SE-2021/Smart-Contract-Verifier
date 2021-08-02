e682e6b162252382f4eb24c0bd4ee284814cdd5b
Cobus Redelinghuys <u19089296@tuks.co.za>
2021-08-02 14:56:32 +0200

    Added unit tests for getAllMessagesByAgreements with a parameterized function for tests

--- Smart-Contract-Verifier-Server/src/test/kotlin/com/savannasolutions/SmartContractVerifierServer/messenger/service/MessengerServiceTest.kt
+++ Smart-Contract-Verifier-Server/src/test/kotlin/com/savannasolutions/SmartContractVerifierServer/messenger/service/MessengerServiceTest.kt
@@ -1,11 +1,26 @@
 package com.savannasolutions.SmartContractVerifierServer.messenger.service
 
+import com.savannasolutions.SmartContractVerifierServer.common.ResponseStatus
+import com.savannasolutions.SmartContractVerifierServer.messenger.models.MessageStatus
+import com.savannasolutions.SmartContractVerifierServer.messenger.models.Messages
 import com.savannasolutions.SmartContractVerifierServer.messenger.repositories.MessageStatusRepository
 import com.savannasolutions.SmartContractVerifierServer.messenger.repositories.MessagesRepository
+import com.savannasolutions.SmartContractVerifierServer.messenger.requests.GetAllMessagesByAgreementRequest
+import com.savannasolutions.SmartContractVerifierServer.messenger.responses.GetAllMessagesByAgreementResponse
 import com.savannasolutions.SmartContractVerifierServer.messenger.services.MessengerService
+import com.savannasolutions.SmartContractVerifierServer.negotiation.models.Agreements
 import com.savannasolutions.SmartContractVerifierServer.negotiation.repositories.AgreementsRepository
+import com.savannasolutions.SmartContractVerifierServer.user.models.User
 import com.savannasolutions.SmartContractVerifierServer.user.repositories.UserRepository
+import org.junit.Test
 import org.mockito.kotlin.mock
+import org.mockito.kotlin.whenever
+import java.util.*
+import kotlin.collections.ArrayList
+import kotlin.test.assertEquals
+import kotlin.test.assertFalse
+import kotlin.test.assertNotNull
+import kotlin.test.assertTrue
 
 internal class MessengerServiceTest {
     private val messagesRepository: MessagesRepository = mock()
@@ -16,4 +31,123 @@ internal class MessengerServiceTest {
                                                     messageStatusRepository,
                                                     userRepository,
                                                     agreementsRepository)
+
+    private fun parameterizeGetAllMessagesByAgreement(userAddress: String,
+                                                                agreementID: UUID,
+                                                                userExist: Boolean,
+                                                                agreementExist: Boolean,
+                                                                messageList: List<Messages>? = null): GetAllMessagesByAgreementResponse
+    {
+        //given
+        val user = User(userAddress)
+        val otherUser = User("Other user")
+        var agreement = Agreements(ContractID = agreementID,
+                                    CreatedDate = Date(),
+                                    MovedToBlockChain = false,)
+        agreement = agreement.apply { users.add(user)}
+        agreement = agreement.apply { users.add(otherUser)}
+
+        //when
+
+        whenever(userRepository.existsById(userAddress)).thenReturn(userExist)
+        whenever(agreementsRepository.existsById(agreementID)).thenReturn(agreementExist)
+        whenever(agreementsRepository.getById(agreementID)).thenReturn(agreement)
+        whenever(messagesRepository.getAllByAgreements(agreement)).thenReturn(messageList)
+        if(messageList != null)
+        {
+            for(msg in messageList)
+            {
+                var msgStatus = MessageStatus(UUID.randomUUID(), Date())
+                msgStatus = msgStatus.apply { message = msg }
+                if(msg.sender == user)
+                    msgStatus = msgStatus.apply { recipient = otherUser }
+                else
+                    msgStatus = msgStatus.apply { recipient = user }
+                val msgStatusList = ArrayList<MessageStatus>()
+                msgStatusList.add(msgStatus)
+                whenever(messageStatusRepository.getAllByMessage(msg)).thenReturn(msgStatusList)
+            }
+        }
+
+        //then
+        return messengerService.getAllMessagesByAgreement(GetAllMessagesByAgreementRequest(agreementID,userAddress))
+    }
+
+    @Test
+    fun `Successful test for getAllMessagesByAgreement without messages`(){
+        //given
+
+        //when
+        val response = parameterizeGetAllMessagesByAgreement("userA",
+                                                                UUID.fromString("7b67f0f4-6433-4a72-b467-c6ddb9dd772a"),
+                                                                userExist = true,
+                                                                agreementExist = true,)
+
+        //then
+        assertEquals(response.status, ResponseStatus.SUCCESSFUL)
+        assertNotNull(response.messages)
+        assertTrue { response.messages!!.isEmpty()}
+    }
+
+    @Test
+    fun `Successful test for getAllMessagesByAgreement with messages`(){
+        //given
+        val agreement = Agreements(ContractID = UUID.fromString("7b67f0f4-6433-4a72-b467-c6ddb9dd772a"),
+            CreatedDate = Date(),
+            MovedToBlockChain = false,)
+
+        val messageList = ArrayList<Messages>()
+        var msg1 = Messages(UUID.fromString("3d63ca50-5584-460a-881a-24c215f0a5e2"),
+                            "Message 1", Date())
+        msg1 = msg1.apply { sender = User("7b67f0f4-6433-4a72-b467-c6ddb9dd772a") }
+        msg1 = msg1.apply { agreements = agreement }
+
+        var msg2 = Messages(UUID.fromString("013e7a05-c831-400c-a43d-507018db7b2c"),
+            "Message 2", Date())
+        msg2 = msg2.apply { sender = User("7b67f0f4-6433-4a72-b467-c6ddb9dd772a") }
+        msg2 = msg2.apply { agreements = agreement }
+
+        messageList.add(msg1)
+        messageList.add(msg2)
+
+        //when
+        val response = parameterizeGetAllMessagesByAgreement("userA",
+            UUID.fromString("7b67f0f4-6433-4a72-b467-c6ddb9dd772a"),
+            userExist = true,
+            agreementExist = true,
+            messageList)
+
+        //then
+        assertEquals(response.status, ResponseStatus.SUCCESSFUL)
+        assertNotNull(response.messages)
+        assertFalse{response.messages!!.isEmpty()}
+    }
+
+    @Test
+    fun `getAllMessagesByAgreement failed user does not exist`(){
+        //given
+
+        //when
+        val response = parameterizeGetAllMessagesByAgreement("userA",
+            UUID.fromString("7b67f0f4-6433-4a72-b467-c6ddb9dd772a"),
+            userExist = false,
+            agreementExist = true)
+
+        //then
+        assertEquals(response.status, ResponseStatus.FAILED)
+    }
+
+    @Test
+    fun `getAllMessagesByAgreement failed agreement does not exist`(){
+        //given
+
+        //when
+        val response = parameterizeGetAllMessagesByAgreement("userA",
+            UUID.fromString("7b67f0f4-6433-4a72-b467-c6ddb9dd772a"),
+            userExist = true,
+            agreementExist = false)
+
+        //then
+        assertEquals(response.status, ResponseStatus.FAILED)
+    }
 }
