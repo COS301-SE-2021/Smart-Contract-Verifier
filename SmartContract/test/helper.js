385325785631ce18217d59686f7f0da05572811d
Kaleb-Bruwer <u19064242@tuks.co.za>
2021-08-04 11:54:15 +0200

    Helper func for unit tests

---
+++ SmartContract/test/helper.js
@@ -0,0 +1,5 @@
+function giveJurorsCoins(token, source, destinations, amountEach){
+    for(var i=0; i<destinations.length; i++){
+        token.transfer(destinations[i], amountEach, {from:source});
+    }
+}
