256956f589981df7119350e9a494be688dd2b385
Cobus Redelinghuys <u19089296@tuks.co.za>
2021-08-06 18:17:08 +0200

    Added JSON annotation

--- Smart-Contract-Verifier-Server/src/main/kotlin/com/savannasolutions/SmartContractVerifierServer/messenger/responses/SetMessageAsReadResponse.kt
+++ Smart-Contract-Verifier-Server/src/main/kotlin/com/savannasolutions/SmartContractVerifierServer/messenger/responses/SetMessageAsReadResponse.kt
@@ -1,5 +1,6 @@
 package com.savannasolutions.SmartContractVerifierServer.messenger.responses
 
+import com.fasterxml.jackson.annotation.JsonProperty
 import com.savannasolutions.SmartContractVerifierServer.common.ResponseStatus
 
-data class SetMessageAsReadResponse(val status: ResponseStatus)
+data class SetMessageAsReadResponse(@JsonProperty("Status") val status: ResponseStatus)
