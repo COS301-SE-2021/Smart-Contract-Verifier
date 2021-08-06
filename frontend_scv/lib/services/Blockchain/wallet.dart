532cd20603bd293a19956d30795b427f0e22fb7b
Ronan <u19288736@tuks.co.za>
2021-08-06 13:34:14 +0200

    Added a method that checks if Metamask is available.

--- frontend_scv/lib/services/Blockchain/wallet.dart
+++ frontend_scv/lib/services/Blockchain/wallet.dart
@@ -12,23 +12,30 @@ class WalletInteraction {
   CredentialsWithKnownAddress metaCred;
   bool ready = false; //Basic check if the class can be used yet
 
+  //Temp string
   Future<void> metamaskConnect() async { //For this to work, a chrome session must be started out of debug mode. Copy the url to a normal tab
     final meta = window.ethereum;
-    if (meta == null) {
+    if (!metamaskAvailable()) {
       print ('Connection failed');
       throw Exception("Metamask is not available");
     }
 
     metaCred = await meta.requestAccount();
+
     Global.userAddress = metaCred.address.toString(); //Save the user address to globals
     ready = true;
     print ('Connection successful');
+
   }
 
   CredentialsWithKnownAddress getCredentials() {
 
     //Haven't tested this syntax;
     return ready ? metaCred : throw Exception('Metamask credentials not ready');
+  }
+
+  bool metamaskAvailable() { //Checks if Metamask is installed
+    return (window.ethereum != null);
   }
 
 
