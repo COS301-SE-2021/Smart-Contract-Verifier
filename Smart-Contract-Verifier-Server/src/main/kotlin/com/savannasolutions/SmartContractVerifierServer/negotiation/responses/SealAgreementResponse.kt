22a9b3749ab2d3756706134920bf99db6bdd0d1a
Cobus Redelinghuys <u19089296@tuks.co.za>
2021-08-06 17:35:16 +0200

    Updated response json format

--- Smart-Contract-Verifier-Server/src/main/kotlin/com/savannasolutions/SmartContractVerifierServer/negotiation/responses/SealAgreementResponse.kt
+++ Smart-Contract-Verifier-Server/src/main/kotlin/com/savannasolutions/SmartContractVerifierServer/negotiation/responses/SealAgreementResponse.kt
@@ -1,5 +1,6 @@
 package com.savannasolutions.SmartContractVerifierServer.negotiation.responses
 
+import com.fasterxml.jackson.annotation.JsonProperty
 import com.savannasolutions.SmartContractVerifierServer.common.ResponseStatus
 
-data class SealAgreementResponse(val status: Enum<ResponseStatus>,)
+data class SealAgreementResponse(@JsonProperty("Status") val status: Enum<ResponseStatus>,)
