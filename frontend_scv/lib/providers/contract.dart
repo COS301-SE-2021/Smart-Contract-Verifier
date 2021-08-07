1b887161612b192d73d38020d3d4f1a83a3d8e56
Ronan <u19288736@tuks.co.za>
2021-08-07 15:49:29 +0200

    Updated json constructor for Contract class to support conditions.

--- frontend_scv/lib/providers/contract.dart
+++ frontend_scv/lib/providers/contract.dart
@@ -1,25 +1,26 @@
 import 'dart:convert';
 import 'package:flutter/foundation.dart';
 import 'package:http/http.dart' as http;
+import 'package:unison/providers/condition.dart';
 import '../models/http_exception.dart';
 import 'package:flutter/material.dart';
 
 class Contract with ChangeNotifier {
-  final String contractId; //agreementID
-  final String durationId;
+  String contractId; //agreementID
+  String durationId;
   String paymentId;
-  final String partyA;
-  final String partyB;
-  final String createdDate;
+  String partyA;
+  String partyB;
+  String createdDate;
   String sealedDate;
   bool movedToBlockchain;
   List<dynamic> conditions = ['empty']; //TODO Handle empty/initial conditions
 
-  final String title;
-  final String description;
-  final double price;
-  final String imageUrl;
-  final String partyBId;
+  String title;
+  String description;
+  double price;
+  String imageUrl;
+  String partyBId;
   bool isFavorite;
 
 
@@ -51,23 +52,28 @@ class Contract with ChangeNotifier {
 
 
   //JSON constructor. Uses response from getAgreement. RC: Should be revised
-  Contract.fromJson(Map<String, dynamic> jsn)
-      : contractId = jsn['AgreementID'],
+  Contract.fromJson(Map<String, dynamic> jsn) {
+        contractId = jsn['AgreementID'];
         //duration = json['duration'], //Should be durationID?
-        durationId = '', //jsn['duration'],
-        partyA = jsn['PartyA']['publicWalletID'],
-        partyB = jsn['PartyB']['publicWalletID'],
-        createdDate = jsn['CreatedDate'],
-        sealedDate = jsn['SealedDate'],
+        durationId = ''; //jsn['duration'],
+        partyA = jsn['PartyA']['publicWalletID'];
+        partyB = jsn['PartyB']['publicWalletID'];
+        createdDate = jsn['CreatedDate'];
+        sealedDate = jsn['SealedDate'];
         //status = json['status'],
-        movedToBlockchain = jsn['MovedToBlockChain'],
-        description = jsn['AgreementDescription'],
-        imageUrl = jsn['AgreementImageURL'],
-        partyBId = '',
-        price = 0,
-        title = jsn['AgreementTitle'],
-        conditions = jsn['Conditions'];
+        movedToBlockchain = jsn['MovedToBlockChain'];
+        description = jsn['AgreementDescription'];
+        imageUrl = jsn['AgreementImageURL'];
+        partyBId = '';
+        price = 0;
+        title = jsn['AgreementTitle'];
+        contractId = '';
+        conditions = [];//jsn['Conditions'];
+        for (int i =0;i<jsn['Conditions'].length;i++) {
+          conditions.add(Condition.fromJson(jsn['Conditions'][i]));
+        }
 
+  }
   //To JSON. RC: Subject to the great field discussion
   Map<String, dynamic> toJson() => {
         'AgreementID': contractId,
