e763647aed4fd60d8dafb5b966132f0d073d7338
Ronan <u19288736@tuks.co.za>
2021-08-08 14:39:28 +0200

    Added message.dart, messageService.dart and a MessageService class for the future.

---
+++ frontend_scv/lib/providers/message.dart
@@ -0,0 +1,8 @@
+//This class represents a message sent between two parties.
+//This is a preliminary class, and should be expected to change
+
+class Message {
+
+
+
+}
---
+++ frontend_scv/lib/services/Server/messageService.dart
@@ -0,0 +1,19 @@
+//This class will be a service to be used by the messaging service.
+
+import 'backendAPI.dart';
+import '../../providers/message.dart';
+
+class MessageService {
+
+  ApiInteraction api = ApiInteraction();
+
+  Future<void> sendMessage(Message mess) async {
+    //TODO list:
+    //Get api endpoints
+    //Pass in relevant things
+    //Error handling
+
+  }
+
+
+}
