8978a8f71ba431138fc50287b3e5786bbba865ce
Kevin <kevindupreez8@gmail.com>
2021-08-08 06:26:40 +0200

    Removed dual/unnecessary provider initialisation and removed unnecessary filterbyuser parameter

--- frontend_scv/lib/widgets/contracts_grid.dart
+++ frontend_scv/lib/widgets/contracts_grid.dart
@@ -4,14 +4,16 @@ import '../providers/contracts.dart';
 import './contract_item.dart';
 
 class ContractsGrid extends StatelessWidget {
-  final bool showFavs;
-  ContractsGrid([this.showFavs]);
+  // final bool showFavs;
+  ContractsGrid();
   @override
   Widget build(BuildContext context) {
-    final contractsData = Provider.of<Contracts>(context);
-    final contracts =
-        showFavs ? contractsData.favoriteItems : contractsData.items;
-
+    //-> setting/getting contracts happens in the contracts_overview_screen
+    final contracts = Provider.of<Contracts>(context).items; //simply access
+    // the items here
+    print(
+        '\n\n_______________GRID contracts[0]__\n\n${contracts[0].toString()}\n'
+        '\n_________________\n\n');
     return ListView.builder(
       padding: const EdgeInsets.all(10.0),
       itemCount: contracts.length,
