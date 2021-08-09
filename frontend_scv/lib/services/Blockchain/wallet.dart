c630e713ab34a84667d40099bac1330196367ff3
Kevin <kevindupreez8@gmail.com>
2021-08-09 06:33:18 +0200

    Standardised formatting

--- frontend_scv/lib/services/Blockchain/wallet.dart
+++ frontend_scv/lib/services/Blockchain/wallet.dart
@@ -9,7 +9,6 @@ import '../../providers/global.dart';
 // import 'package:web3dart/web3dart.dart';
 
 class WalletInteraction {
-
   CredentialsWithKnownAddress metaCred;
   bool ready = false; //Basic check if the class can be used yet
   static final WalletInteraction wall = WalletInteraction._internal();
@@ -20,36 +19,37 @@ class WalletInteraction {
   }
 
   //Temp string
-  Future<void> metamaskConnect() async { //For this to work, a chrome session must be started out of debug mode. Copy the url to a normal tab
+  Future<void> metamaskConnect() async {
+    //For this to work, a chrome session must be started out of debug mode. Copy the url to a normal tab
     final meta = window.ethereum;
     if (!metamaskAvailable()) {
-      print ('Connection failed');
+      print('Connection failed');
       throw Exception("Metamask is not available");
     }
 
     metaCred = await meta.requestAccount();
 
-    Global.userAddress = metaCred.address.toString(); //Save the user address to globals
+    Global.userAddress =
+        metaCred.address.toString(); //Save the user address to globals
     ready = true;
 
     //print ('Connection successful');
-
   }
 
   CredentialsWithKnownAddress getCredentials() {
-
     //Haven't tested this syntax;
     return ready ? metaCred : throw Exception('Metamask credentials not ready');
   }
 
-  bool metamaskAvailable() { //Checks if Metamask is installed
+  bool metamaskAvailable() {
+    //Checks if Metamask is installed
     return (window.ethereum != null);
   }
 
-  void metamaskDisconnect() { //Disconnect from Metamask
+  void metamaskDisconnect() {
+    //Disconnect from Metamask
 
     metaCred = null;
     ready = false;
   }
-
 }
