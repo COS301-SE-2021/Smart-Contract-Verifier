8ca5ac81ce8dade5a26c64faa27db65e2eeb48a5
Ronan <u19288736@tuks.co.za>
2021-08-10 17:47:14 +0200

    Added additional functionality to MessageService.

--- frontend_scv/lib/services/Server/messageService.dart
+++ frontend_scv/lib/services/Server/messageService.dart
@@ -1,9 +1,7 @@
-//This class will be a service to be used by the messaging service.
+//This class will be a service to be used by the messaging interface.
 
 import 'dart:convert';
-
 import 'package:unison/providers/global.dart';
-
 import 'backendAPI.dart';
 import '../../providers/message.dart';
 
@@ -27,26 +25,57 @@ class MessageService {
   }
 
   Future<List<Message>> getMessages(String id) async { //Get all messages for the agreement id passed in
+    return await _getMessageHandler(id, true);
+  }
 
-    Map<String, dynamic> body = {'AgreementID' : id, 'RequestingUser' : Global.userAddress};
+  //Get messages either by agreement, or user id
+  Future<List<Message>> _getMessageHandler(String id, bool byAgreement) async { //Used by two other methods
+
+      Map<String, dynamic> body = byAgreement? {'AgreementID' : id, 'RequestingUser' : Global.userAddress} : {'RequestingUser' : id};
+      Map<String, String> response;
+
+      try {
+        String path = byAgreement? 'agreement' : 'user';
+        response = await _api.postData(_reqPath + 'get-all-messages-by-$path', body);
+
+        if (response['Status'] != "SUCCESSFUL")
+          throw Exception('Messages could not be retrieved');
+      } catch (err) {
+        print (err); //Handle exception
+        throw err;
+      }
+
+      List<Message> ret = [];
+      for (int i =0;i<response['Messages'].length;i++) {
+        ret.add(Message.fromJSON(jsonDecode(response['Messages'][i])));
+      }
+
+      return ret;
+  }
+
+
+  Future<void> setMessageRead(Message mes) async { //Let the backend know that a message has been read
+
+    Map<String, dynamic> body = {'MessageID' : mes.messageID, 'RecipientID' : Global.userAddress};
     Map<String, String> response;
 
     try {
-      response = await _api.postData(_reqPath + 'get-all-messages-by-agreement', body);
+      response = await _api.postData(_reqPath + 'set-message-as-read', body);
 
+      //RFC: Is error checking even necessary for an 'unimportant' operation?
       if (response['Status'] != "SUCCESSFUL")
-        throw Exception('Messages could not be retrieved');
-      } catch (err) {
-          print (err); //Handle exception
-          throw err;
-        }
-
-    List<Message> ret = [];
-    for (int i =0;i<response['Messages'].length;i++) {
-      ret.add(Message.fromJSON(jsonDecode(response['Messages'][i])));
+        throw Exception('Messages could not be set as read');
+    } catch (err) {
+      print (err); //Handle exception
+      throw err;
     }
 
-    return ret;
+  }
+
+  //The following methods are implemented for the sake of completeness, should they ever be needed.
+
+  Future<List<Message>> getAllMessages() async { //Get all messages for the current user, across all agreements.
+      return await _getMessageHandler(Global.userAddress, false);
   }
 
 
