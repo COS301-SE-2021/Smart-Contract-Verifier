3df4d3577342a20b1d24eec2ec622bee62de503b
Kaleb-Bruwer <u19064242@tuks.co.za>
2021-08-10 14:26:27 +0200

    Deployed addresses are written to file

--- SmartContract/migrations/2_deploy_contracts.js
+++ SmartContract/migrations/2_deploy_contracts.js
@@ -2,9 +2,21 @@ const Verifier = artifacts.require("Verifier");
 const UnisonToken = artifacts.require("UnisonToken");
 const RandomSource = artifacts.require("Utilities/RandomSource.sol");
 
+fs = require('fs');
+
 module.exports = async (deployer) => {
   await deployer.deploy(RandomSource);
   await deployer.deploy(UnisonToken);
   await deployer.deploy(Verifier, UnisonToken.address, RandomSource.address);
 
+
+  var obj =  [];
+  obj.push({name : "Verifier", address : Verifier.address});
+  obj.push({name : "UnisonToken", address : UnisonToken.address});
+
+  // This file will allow front end to know where the smart contracts have been deployed
+  fs.writeFile("../frontend_scv/assets/JSON/address.json", JSON.stringify(obj), function(err){
+    console.log(err);
+  })
+  console.log("Adress: ", Verifier.address);
 };
