6eff8df887b823f6918b6c46159d7bdc12796644
Kevin <kevindupreez8@gmail.com>
2021-08-11 19:55:11 +0200

    Optimised imports (token service)

--- frontend_scv/lib/services/Blockchain/tokenService.dart
+++ frontend_scv/lib/services/Blockchain/tokenService.dart
@@ -1,23 +1,22 @@
 //This file is the service used for interacting with the UnisonToken smart contract.
 
 import 'package:unison/models/global.dart';
+
 import 'smartContract.dart';
 
 class TokenService {
+  SmartContract _smC =
+      SmartContract("JSON/_src_UnisonToken_sol_UnisonToken.abi", 'UnisonToken');
 
-  SmartContract _smC = SmartContract("JSON/_src_UnisonToken_sol_UnisonToken.abi", 'UnisonToken');
-
-  Future<void> setStake() async { //Let the user stake an amount on UnisonToken
+  Future<void> setStake() async {
+    //Let the user stake an amount on UnisonToken
 
     try {
       await _smC.makeWriteCall(
           'approve', [Global.userAddress, 10000]); //User stakes 10 000 gwei
-    }
-    catch (e) {
-      print (e);
+    } catch (e) {
+      print(e);
       return;
     }
   }
-
-
 }
