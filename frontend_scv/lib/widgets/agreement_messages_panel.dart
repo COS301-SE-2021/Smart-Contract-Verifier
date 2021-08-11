dca6f78af26d2f8469342f6731289e3e4eaf1ee7
Kevin <kevindupreez8@gmail.com>
2021-08-11 14:18:41 +0200

    Added Message widgets logic (for current user and other party)

--- frontend_scv/lib/widgets/agreement_messages_panel.dart
+++ frontend_scv/lib/widgets/agreement_messages_panel.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:unison/models/global.dart';
 import 'package:unison/models/message.dart';
 import 'package:unison/services/Server/messageService.dart';
@@ -23,7 +24,80 @@ class AgreementMessagesPanel extends StatelessWidget {
               itemBuilder: (_, index) {
                 final message = _messages[index];
                 bool isCurrentUser = message.sender == Global.userAddress;
-                return Container();
+                return Container(
+                  margin: EdgeInsets.only(top: 10),
+                  child: Column(
+                    children: [
+                      Row(
+                        mainAxisAlignment: isCurrentUser
+                            ? MainAxisAlignment.end
+                            : MainAxisAlignment.start,
+                        crossAxisAlignment: CrossAxisAlignment.end,
+                        children: [
+                          if (!isCurrentUser)
+                            CircleAvatar(
+                              radius: 15,
+                              // backgroundImage: AssetImage(user.avatar),
+                              //TODO: use Jdicons here
+                            ),
+                          SizedBox(
+                            width: 10,
+                          ),
+                          Container(
+                            padding: EdgeInsets.all(10),
+                            constraints: BoxConstraints(
+                                maxWidth:
+                                    MediaQuery.of(context).size.width * 0.6),
+                            decoration: BoxDecoration(
+                                color: isCurrentUser
+                                    ? Colors.deepOrange
+                                    : Colors.grey[200],
+                                borderRadius: BorderRadius.only(
+                                  topLeft: Radius.circular(16),
+                                  topRight: Radius.circular(16),
+                                  bottomLeft:
+                                      Radius.circular(isCurrentUser ? 12 : 0),
+                                  bottomRight:
+                                      Radius.circular(isCurrentUser ? 0 : 12),
+                                )),
+                            child: Text(
+                              _messages[index].messageText,
+                              // TODO: style: MyTheme.bodyTextMessage.copyWith(
+                              //     color: isMe ? Colors.white : Colors.grey[800]),
+                            ),
+                          ),
+                        ],
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(top: 5),
+                        child: Row(
+                          mainAxisAlignment: isCurrentUser
+                              ? MainAxisAlignment.end
+                              : MainAxisAlignment.start,
+                          children: [
+                            if (!isCurrentUser)
+                              SizedBox(
+                                width: 40,
+                              ),
+                            Icon(
+                              Icons.done_all,
+                              size: 20,
+                              // color: MyTheme.bodyTextTime.color,
+                            ),
+                            SizedBox(
+                              width: 8,
+                            ),
+                            Text(
+                              DateFormat('yyyy-MM-dd – kk:mm')
+                                  .format(message.dateSent),
+                              // style: MyTheme.bodyTextTime,
+                            )
+                          ],
+                        ),
+                      )
+                    ],
+                  ),
+                );
               }),
     );
   }
