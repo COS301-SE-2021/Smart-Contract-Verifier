e009505125474e95ea7f5b2f3b8d5851bcef6307
Kevin <kevindupreez8@gmail.com>
2021-08-11 19:54:35 +0200

    Optimised imports (negotiation)

--- frontend_scv/lib/services/Server/negotiationService.dart
+++ frontend_scv/lib/services/Server/negotiationService.dart
@@ -2,128 +2,136 @@
 //This one deals with negotiation-related issues.
 
 import 'dart:async';
-import 'backendAPI.dart';
-import '../../models/contract.dart';
+
 import '../../models/condition.dart';
+import '../../models/contract.dart';
 import '../../models/global.dart';
+import 'backendAPI.dart';
 
 class NegotiationService {
-
   ApiInteraction _api = ApiInteraction();
   final String _reqPath = '/negotiation/';
 
- Future<dynamic> getNotifications(String party) async {
-
+  Future<dynamic> getNotifications(String party) async {
     return;
- }
+  }
 
- Future<void> saveAgreement (Contract agr) async { //Save initial version of agreement to backend
+  Future<void> saveAgreement(Contract agr) async {
+    //Save initial version of agreement to backend
     //TODO list:
-   //Add exception handling. Maybe change return type to String
+    //Add exception handling. Maybe change return type to String
 
-   Map<String, dynamic> response;
-   try {
-     response = await _api.postData(_reqPath + 'create-agreement', agr.toJson());
+    Map<String, dynamic> response;
+    try {
+      response =
+          await _api.postData(_reqPath + 'create-agreement', agr.toJson());
 
-     if (response['Status'] != 'SUCCESSFUL')
-       throw Exception('Agreement could not be saved');
-   } on Exception catch(e) {
+      if (response['Status'] != 'SUCCESSFUL')
+        throw Exception('Agreement could not be saved');
+    } on Exception catch (e) {
       //Handle exception
-     print (e);
-     throw e;
-   }
+      print(e);
+      throw e;
+    }
+  }
 
- }
+  Future<void> saveCondition(Condition cond) async {
+    //Save a condition associated with a contract
 
- Future<void> saveCondition(Condition cond) async { //Save a condition associated with a contract
+    Map<String, dynamic> response;
+    try {
+      response =
+          await _api.postData(_reqPath + 'create-condition', cond.toJson());
 
-   Map<String, dynamic> response;
-   try {
-     response = await _api.postData(_reqPath + 'create-condition', cond.toJson());
+      if (response['Status'] != 'SUCCESSFUL')
+        throw Exception('Condition could not be saved');
+    } on Exception catch (e) {
+      //Handle exception
+      print(e);
+      throw e;
+    }
+  }
 
-     if (response['Status'] != 'SUCCESSFUL')
-       throw Exception('Condition could not be saved');
-   } on Exception catch(e) {
-     //Handle exception
-     print (e);
-     throw e;
-   }
-
- }
-
-  void acceptCondition(String id) async { //Or condition object?
+  void acceptCondition(String id) async {
+    //Or condition object?
 
     await _handleCondition(id, true);
   }
 
   void rejectCondition(String id) async {
-
     await _handleCondition(id, false);
   }
 
-  Future<void> _handleCondition(String id, bool acc) async { //Either accept or reject condition
+  Future<void> _handleCondition(String id, bool acc) async {
+    //Either accept or reject condition
 
-   String path = acc ? 'accept-condition' : 'reject-condition';
+    String path = acc ? 'accept-condition' : 'reject-condition';
 
-   Map<String, dynamic> response;
-   try {
-     response = await _api.postData(_reqPath + path, {'ConditionID' : id});
+    Map<String, dynamic> response;
+    try {
+      response = await _api.postData(_reqPath + path, {'ConditionID': id});
 
-     if (response['Status'] != 'SUCCESSFUL')
-       throw Exception('Condition could not be ' + (acc ? 'accepted' : 'rejected'));
-   } on Exception catch(e) {
-     //Handle exception
-     print (e);
-     return;
-   }
- }
+      if (response['Status'] != 'SUCCESSFUL')
+        throw Exception(
+            'Condition could not be ' + (acc ? 'accepted' : 'rejected'));
+    } on Exception catch (e) {
+      //Handle exception
+      print(e);
+      return;
+    }
+  }
 
-  Future<void> setPayment(Contract con, double price) async { //Set the payment condition of an agreement.
+  Future<void> setPayment(Contract con, double price) async {
+    //Set the payment condition of an agreement.
 
     await _handlePayDuration(con, price, true);
   }
 
-  Future<void> setDuration(Contract con, double dur) async { //Set the duration condition of an agreement.
+  Future<void> setDuration(Contract con, double dur) async {
+    //Set the duration condition of an agreement.
 
     await _handlePayDuration(con, dur, false);
   }
 
+  Future<void> _handlePayDuration(Contract con, double val, bool price) async {
+    //Handles both price and duration
 
-  Future<void> _handlePayDuration(Contract con, double val, bool price) async { //Handles both price and duration
-
-    Map<String, dynamic> body = {'ProposedUser' : Global.userAddress, 'AgreementID' : con.contractId, (price? 'Payment':'Duration') : val};
+    Map<String, dynamic> body = {
+      'ProposedUser': Global.userAddress,
+      'AgreementID': con.contractId,
+      (price ? 'Payment' : 'Duration'): val
+    };
     Map<String, dynamic> response;
-    String path = price? 'payment' : 'duration';
+    String path = price ? 'payment' : 'duration';
 
     try {
       response = await _api.postData(_reqPath + 'set-$path-condition', body);
 
       if (response['Status'] != 'SUCCESSFUL')
-        throw Exception('${(price? 'Payment':'Duration')} could not be saved');
-    } on Exception catch(e) {
+        throw Exception(
+            '${(price ? 'Payment' : 'Duration')} could not be saved');
+    } on Exception catch (e) {
       //Handle exception
-      print (e);
+      print(e);
       throw e;
     }
-
   }
 
- Future<void> sealAgreement(Contract con) async { //Or pass in Contract?
-   //RFC: Should the blockchain be called immediately after the backend?
+  Future<void> sealAgreement(Contract con) async {
+    //Or pass in Contract?
+    //RFC: Should the blockchain be called immediately after the backend?
 
-   Map<String, dynamic> response;
-   try {
-     response = await _api.postData(_reqPath + 'seal-agreement', {'AgreementID' : con.contractId});
+    Map<String, dynamic> response;
+    try {
+      response = await _api.postData(
+          _reqPath + 'seal-agreement', {'AgreementID': con.contractId});
 
-     if (response['Status'] != 'SUCCESSFUL')
-       throw Exception('Agreement could not be sealed');
-   } on Exception catch(e) {
-     //Handle exception
-     print (e);
-     return;
-   }
-
- }
-
+      if (response['Status'] != 'SUCCESSFUL')
+        throw Exception('Agreement could not be sealed');
+    } on Exception catch (e) {
+      //Handle exception
+      print(e);
+      return;
+    }
+  }
 }
-
