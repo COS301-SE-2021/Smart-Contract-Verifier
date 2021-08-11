f6c8f4d8d5647ac5b5c6b7306e7f0615e309129c
Cobus Redelinghuys <u19089296@tuks.co.za>
2021-08-11 21:18:18 +0200

    Created Judge model

---
+++ Smart-Contract-Verifier-Server/src/main/kotlin/com/savannasolutions/SmartContractVerifierServer/judges/models/Judges.kt
@@ -0,0 +1,16 @@
+package com.savannasolutions.SmartContractVerifierServer.judges.models
+
+import com.savannasolutions.SmartContractVerifierServer.negotiation.models.Agreements
+import com.savannasolutions.SmartContractVerifierServer.user.models.User
+import java.util.*
+import javax.persistence.*
+
+@Entity
+data class Judges(@Id @GeneratedValue val JudgeAgreementID: UUID)
+{
+    @ManyToOne(fetch = FetchType.LAZY)
+    lateinit var judge : User
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    lateinit var agreement : Agreements
+}
