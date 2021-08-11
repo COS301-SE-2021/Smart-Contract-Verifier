7b864631055e4496fa4881afb1a46ab769d72ebd
JasonMaritz <u19053292@tuks.co.za>
2021-08-11 19:41:58 +0200

    Added SecurityConfig class to load values from the active properties file

---
+++ Smart-Contract-Verifier-Server/src/main/kotlin/com/savannasolutions/SmartContractVerifierServer/security/SecurityConfig.kt
@@ -0,0 +1,15 @@
+package com.savannasolutions.SmartContractVerifierServer.security
+
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.PropertySource
+
+@Configuration
+@PropertySource("classpath:application.properties")
+class SecurityConfig {
+    @Value("com.unison.SecretKey")
+    var secretKey: String = ""
+
+    @Value("com.unison.timeout")
+    var timeout: Long = 0
+}
