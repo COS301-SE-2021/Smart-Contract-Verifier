f5ce55cb807700d8f4e2f93c02a9bc50b0077e15
JasonMaritz <u19053292@tuks.co.za>
2021-08-11 12:07:51 +0200

    Remade AddUserRequest

--- Smart-Contract-Verifier-Server/src/main/kotlin/com/savannasolutions/SmartContractVerifierServer/user/requests/AddUserRequest.kt
+++ Smart-Contract-Verifier-Server/src/main/kotlin/com/savannasolutions/SmartContractVerifierServer/user/requests/AddUserRequest.kt
@@ -3,4 +3,4 @@ package com.savannasolutions.SmartContractVerifierServer.user.requests
 import com.fasterxml.jackson.annotation.JsonProperty
 
 data class AddUserRequest(@JsonProperty("WalletID") val WalletID: String,
-                            @JsonProperty("Alias") val Alias: String)
+                          @JsonProperty("Alias") val Alias: String,)
