4a6b3cafad09ba9a9fbddf6c34bba14d123715b2
JasonMaritz <u19053292@tuks.co.za>
2021-08-11 11:51:10 +0200

    Added stub for validating a user against his jwt claim

---
+++ Smart-Contract-Verifier-Server/src/main/kotlin/com/savannasolutions/SmartContractVerifierServer/security/services/JwtClaimManager.kt
@@ -0,0 +1,7 @@
+package com.savannasolutions.SmartContractVerifierServer.security.services
+
+class JwtClaimManager {
+    fun validateUserId(userId: String, jwtToken: String): Boolean {
+        return false
+    }
+}
