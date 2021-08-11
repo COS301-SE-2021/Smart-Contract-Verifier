f9f041e04500e326d2be2e0a8be55b39f2cc7a85
JasonMaritz <u19053292@tuks.co.za>
2021-08-11 23:50:48 +0200

    Added initial web3J filter setup and configuration class for contracts

--- Smart-Contract-Verifier-Server/src/main/kotlin/com/savannasolutions/SmartContractVerifierServer/judges/services/JudgesService.kt
+++ Smart-Contract-Verifier-Server/src/main/kotlin/com/savannasolutions/SmartContractVerifierServer/judges/services/JudgesService.kt
@@ -1,12 +1,31 @@
 package com.savannasolutions.SmartContractVerifierServer.judges.services
 
+import com.savannasolutions.SmartContractVerifierServer.judges.configurations.ContractConfig
 import com.savannasolutions.SmartContractVerifierServer.judges.repositories.JudgesRepository
 import com.savannasolutions.SmartContractVerifierServer.negotiation.repositories.AgreementsRepository
 import com.savannasolutions.SmartContractVerifierServer.user.repositories.UserRepository
 import org.springframework.stereotype.Service
+import org.web3j.protocol.Web3j
+import org.web3j.protocol.core.DefaultBlockParameterName
+import org.web3j.protocol.core.methods.request.EthFilter
+import org.web3j.protocol.http.HttpService
+import javax.annotation.PostConstruct
+import javax.servlet.http.HttpServlet
 
 @Service
 class JudgesService constructor(val judgesRepository: JudgesRepository,
                                 val agreementsRepository: AgreementsRepository,
-                                val userRepository: UserRepository,){
+                                val userRepository: UserRepository,
+                                private val contractConfig: ContractConfig,){
+
+    private val web3j: Web3j = Web3j.build(HttpService(contractConfig.nodeAddress))
+
+    @PostConstruct
+    fun initEventListener(){
+        var filter = EthFilter(DefaultBlockParameterName.EARLIEST,
+                               DefaultBlockParameterName.LATEST,
+                               contractConfig.contractId)
+
+    }
+
 }
