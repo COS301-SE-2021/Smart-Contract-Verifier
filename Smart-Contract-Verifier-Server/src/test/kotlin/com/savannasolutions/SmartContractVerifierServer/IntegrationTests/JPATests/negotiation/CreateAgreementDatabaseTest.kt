08f907faebceb5dd1e2997a255e8a9db55c083e2
Cobus Redelinghuys <u19089296@tuks.co.za>
2021-08-12 23:43:33 +0200

    Initial test for set up of integration test with db. Please note this test does not reflect what the tests in the file will actually look like

--- Smart-Contract-Verifier-Server/src/test/kotlin/com/savannasolutions/SmartContractVerifierServer/IntegrationTests/JPATests/negotiation/CreateAgreementDatabaseTest.kt
+++ Smart-Contract-Verifier-Server/src/test/kotlin/com/savannasolutions/SmartContractVerifierServer/IntegrationTests/JPATests/negotiation/CreateAgreementDatabaseTest.kt
@@ -1,4 +1,46 @@
 package com.savannasolutions.SmartContractVerifierServer.IntegrationTests.JPATests.negotiation
 
+import com.savannasolutions.SmartContractVerifierServer.negotiation.repositories.AgreementsRepository
+import com.savannasolutions.SmartContractVerifierServer.negotiation.repositories.ConditionsRepository
+import com.savannasolutions.SmartContractVerifierServer.user.models.User
+import com.savannasolutions.SmartContractVerifierServer.user.repositories.UserRepository
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.orm.jpa.AutoConfigureDataJpa
+import org.springframework.boot.test.context.SpringBootTest
+import kotlin.test.assertEquals
+
+@SpringBootTest
+@AutoConfigureDataJpa
 class CreateAgreementDatabaseTest {
+    @Autowired
+    lateinit var agreementsRepository: AgreementsRepository
+
+    @Autowired
+    lateinit var conditionsRepository: ConditionsRepository
+
+    @Autowired
+    lateinit var userRepository: UserRepository
+
+    @BeforeEach
+    fun beforeEach()
+    {
+        userRepository.save(User("integration test 2"))
+    }
+
+    @AfterEach
+    fun afterEach()
+    {
+        val user = userRepository.getById("integration test 2")
+        userRepository.delete(user)
+    }
+
+    @Test
+    fun `CreateAgreement successful`()
+    {
+        val user = userRepository.getById("integration test 2")
+        assertEquals(user.publicWalletID, "integration test 2")
+    }
 }
