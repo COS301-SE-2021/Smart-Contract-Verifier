728f8233589bb4ab3d1e90c8e57bcc2f9164131e
JasonMaritz <u19053292@tuks.co.za>
2021-08-13 22:31:47 +0200

    changed SealAgreementRequest to include blockchain ID

--- Smart-Contract-Verifier-Server/src/main/kotlin/com/savannasolutions/SmartContractVerifierServer/negotiation/requests/SealAgreementRequest.kt
+++ Smart-Contract-Verifier-Server/src/main/kotlin/com/savannasolutions/SmartContractVerifierServer/negotiation/requests/SealAgreementRequest.kt
@@ -3,4 +3,5 @@ package com.savannasolutions.SmartContractVerifierServer.negotiation.requests
 import com.fasterxml.jackson.annotation.JsonProperty
 import java.util.*
 
-data class SealAgreementRequest(@JsonProperty("AgreementID") val AgreementID: UUID,)
+data class SealAgreementRequest(@JsonProperty("AgreementID") val AgreementID: UUID,
+                                val index: Int)
