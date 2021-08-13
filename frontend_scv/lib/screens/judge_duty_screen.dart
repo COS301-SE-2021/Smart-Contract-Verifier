48668559488a490b5bca1e0ab9ddef2f0d0212be
Kevin <kevindupreez8@gmail.com>
2021-08-13 21:33:56 +0200

    Added check for if is judge

--- frontend_scv/lib/screens/judge_duty_screen.dart
+++ frontend_scv/lib/screens/judge_duty_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:unison/models/global.dart';
+import 'package:unison/services/Server/judgeService.dart';
 import 'package:unison/widgets/judge_active_cases_panel.dart';
 
 import '../providers/auth.dart';
@@ -11,33 +12,48 @@ class JudgeDutyScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Judge Duty'),
-      ),
-      drawer: AppDrawer(),
-      body: Column(
-        children: [
-          SizedBox(
-            height: 20,
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              SizedBox(
-                width: 20,
-              ),
-              Text(
-                'Current Assignments:',
-                style: TextStyle(
-                  fontSize: 16,
-                  decoration: TextDecoration.underline,
-                ),
-              ),
-            ],
-          ),
-          Expanded(child: JudgeActiveCasesPanel()),
-        ],
-      ),
-    );
+        appBar: AppBar(
+          title: Text('Judge Duty'),
+        ),
+        drawer: AppDrawer(),
+        body: Global.isJudge
+            ? Column(
+                children: [
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      SizedBox(
+                        width: 20,
+                      ),
+                      Text(
+                        'Current Assignments:',
+                        style: TextStyle(
+                          fontSize: 16,
+                          decoration: TextDecoration.underline,
+                        ),
+                      ),
+                    ],
+                  ),
+                  Expanded(child: JudgeActiveCasesPanel()),
+                ],
+              )
+            : Column(
+                children: [
+                  Text('You are currently not a judge.'),
+                  TextButton(
+                      onPressed: () async {
+                        // JudgeService jS = JudgeService();
+                        // await jS.makeUserJudge();
+                        Global.isJudge = true;
+                        Navigator.of(context).pushNamed(
+                          JudgeDutyScreen.routeName,
+                        );
+                      },
+                      child: Text('Become a Judge'))
+                ],
+              ));
   }
 }
