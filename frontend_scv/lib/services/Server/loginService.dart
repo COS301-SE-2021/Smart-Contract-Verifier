a30f411ecf7c2063179f0c7bed07d0cbb5fe01f7
Ronan <u19288736@tuks.co.za>
2021-08-13 13:04:25 +0200

    Added method that attempts to add a user to the db. This is meant to be used at login.

--- frontend_scv/lib/services/Server/loginService.dart
+++ frontend_scv/lib/services/Server/loginService.dart
@@ -8,11 +8,23 @@ class LoginService {
   //Implement login flow when server is ready.
     // - Signing with Metamask etc.
 
-  ApiInteraction api = ApiInteraction();
+  ApiInteraction _api = ApiInteraction();
 
   Future<void> refreshToken() async { //Starts the login procedure to get a token to use in backend api requests
 
     Global.apiToken = ''; //Will eventually be used in all following requests to backend.
+
+  }
+
+  Future<void> tryAddUser() async { //This attempts to add a new user to the DB. Failure means the user already exists
+
+    try {
+      await _api.postData('/user/add-user', {'WalletID': Global.userAddress, 'Alias' : 'Nothing'});
+
+    } on Exception catch (e) {
+      print(e);
+      throw(e);
+    }
 
   }
 
