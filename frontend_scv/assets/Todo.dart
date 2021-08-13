88ffb909696a081b7e615c703fb9ade0de28ccce
Kevin <kevindupreez8@gmail.com>
2021-08-13 22:56:38 +0200

    Action plan for tomorrow set in TODO, not in stone

--- frontend_scv/assets/Todo.dart
+++ frontend_scv/assets/Todo.dart
@@ -1,19 +1,15 @@
 // Dashboard screen
-//// Involved Agreements (as party A or party B):
-/*
- * -> List of Agreement objects DONE
-*/
-//// Notifications (for Agreement activity):
+//// Notifications (for Agreement activity): Surprised Pikachu
 /*
  * -> List of Notification objects
 */
 
-// New Agreement Screen
+// New Agreement Screen : Touch ups
 //// Save a new/empty Agreement object
 /*
  * -> New Agreement object save to server
- *    * How do we ensure the other party is on the server already?
- *
+ *    * How do we ensure the other party is on the server already? (ASSUME
+ * for now)
 */
 
 // Agreement Details Screen
@@ -23,21 +19,11 @@
  * -> Get Agreement condition objects from server DONE
  * -> Insert Agreement conditions into a list and make it part of the
  * agreement object DONE
- -> Messaging & Evidence
-
+ -> Messaging & Evidence (half)
  *
 */
-//// Save a new Condition
-/*
- * -> Save a new condition object to server
-*/
-//// Update a Condition status
-/*
- * -> Save an update to a condition object to server
-*/
-
 // Judge Duty Screen
-//// Involved Agreements (where user is a jury member):
+//// Involved Agreements (where user is a jury member): (GET LEGIT)
 /*
  * -> List of Agreement objects
 */
@@ -53,12 +39,9 @@
 * the app) for logout functionality
 *
 * Generic API Handler (Hairdu example)
-* Factory JSON methods
-* Update/fix backend to handle 'Title' of condition
 *
 * 0xf808ee4efb1b80ebd803f02b8f99fcc4a5a65709 (Main Account)
 * 0x351277a110948cb5be266c53ea85737e28ac35bf (Secondary Account)
-*
 *
 * Wednesday Night:
 * - Special Conditions:
@@ -72,26 +55,25 @@
 *   - enable the seal agreement button
 *   If both parties 'Seal' -> move to blockchain
 *
-*   {
-*
-*     ''
-*   }
-*
 *   If one party requests seal and another
-*
-*
-*
-*
-*
-*
+**
 *
 * - Check if user exists
 *    -> PartyB in create agreement screen
 *    -> First Time Login (check user is in db)
 * - Show no Agreements message if no agreements
+
 *
-*
-* - Judge Screen
+* TOMORROW:
+* ---------------
+* Jury chat
+* Jury Voting
+* Seal the deal
+* ---------------
+* LATER:
+* Evidence
+* Notifications
+* ---------------
 *
 *
 *
