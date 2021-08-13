9ce8a7ca3cf5f8685c3fb89eefb5b71cb78846de
Kevin <kevindupreez8@gmail.com>
2021-08-13 21:06:39 +0200

    Judge Duty screen is now able to call and populate

---
+++ frontend_scv/lib/screens/view_assignment_screen.dart
@@ -0,0 +1,316 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:unison/models/condition.dart';
+import 'package:unison/models/global.dart';
+import 'package:unison/screens/messaging_screen.dart';
+import 'package:unison/services/Server/negotiationService.dart';
+
+import '../models/contracts.dart';
+import '../widgets/contract_conditions_panel.dart';
+import '../widgets/contract_detail_info_panel.dart';
+
+class ViewAssignmentScreen extends StatefulWidget {
+  static const routeName = '/view-assignment';
+
+  @override
+  _ViewAssignmentScreenState createState() => _ViewAssignmentScreenState();
+}
+
+enum ConditionType { Normal, Payment, Duration }
+
+class _ViewAssignmentScreenState extends State<ViewAssignmentScreen> {
+  // final _conditionTitleController = TextEditingController();
+  // final _conditionDescriptionController = TextEditingController();
+  // final _paymentConditionAmountController = TextEditingController();
+  // final _durationConditionAmountController = TextEditingController();
+  var _isLoading = false;
+  // var _isInit = true;
+
+  NegotiationService negotiationService = NegotiationService();
+
+  GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+  @override
+  void initState() {
+    // print('InitState()');
+    super.initState();
+  }
+
+  // Future<void> _saveForm(String cId, ConditionType type) async {
+  //   final isValid = _formKey.currentState.validate();
+  //
+  //   Condition newCondition = Condition(
+  //     title: '',
+  //     proposedBy: Global.userAddress,
+  //     description: '',
+  //     agreementId: cId,
+  //   );
+  //
+  //   if (type == ConditionType.Normal) {
+  //     newCondition = Condition(
+  //       title: _conditionTitleController.text,
+  //       proposedBy: Global.userAddress,
+  //       description: _conditionDescriptionController.text,
+  //       agreementId: cId,
+  //     );
+  //   } else {}
+  //   if (!isValid) return;
+  //   _formKey.currentState.save();
+  //   //^^^^saves the form -> executes the 'onSaved' of each input
+  //   setState(() {
+  //     _isLoading = true;
+  //   });
+  //
+  //   try {
+  //     //Save to DB:
+  //     if (type == ConditionType.Payment) {
+  //       await negotiationService.setPayment(
+  //           cId, double.parse(_paymentConditionAmountController.text));
+  //     } else if (type == ConditionType.Duration) {
+  //       await negotiationService.setDuration(
+  //           cId, double.parse(_durationConditionAmountController.text));
+  //     } else {
+  //       await negotiationService.saveCondition(newCondition);
+  //     }
+  //     print('new condition saved');
+  //   } catch (error) {
+  //     await showDialog(
+  //       context: context,
+  //       builder: (ctx) => AlertDialog(
+  //         title: Text('An error occurred!'),
+  //         content: Text('Something went wrong.'),
+  //         actions: <Widget>[
+  //           TextButton(
+  //             child: Text('Okay'),
+  //             onPressed: () {
+  //               Navigator.of(ctx).pop();
+  //             },
+  //           )
+  //         ],
+  //       ),
+  //     );
+  //   }
+  //   setState(() {
+  //     _isLoading = false;
+  //     Provider.of<Contracts>(context, listen: false).fetchAndSetContracts();
+  //   });
+  //   Navigator.of(context).pop();
+  // }
+
+  // Future<void> _newConditionDialog(String contId) async {
+  //   return await showDialog(
+  //       context: context,
+  //       builder: (context) {
+  //         return AlertDialog(
+  //           title: Text('Add New Condition'),
+  //           content: Form(
+  //             key: _formKey,
+  //             child: Column(
+  //               mainAxisSize: MainAxisSize.min,
+  //               children: [
+  //                 TextFormField(
+  //                   validator: (value) {
+  //                     if (value.isEmpty) {
+  //                       return 'Please enter a title.';
+  //                     } else {
+  //                       return null;
+  //                     }
+  //                   },
+  //                   decoration: InputDecoration(labelText: 'Title'),
+  //                   controller: _conditionTitleController,
+  //                 ),
+  //                 TextFormField(
+  //                   decoration: InputDecoration(labelText: 'Description'),
+  //                   maxLines: 3,
+  //                   keyboardType: TextInputType.multiline,
+  //                   controller: _conditionDescriptionController,
+  //                   validator: (value) {
+  //                     if (value.isEmpty) return 'Please enter a description.';
+  //                     if (value.length < 8)
+  //                       return 'Please enter at least 8 '
+  //                           'characters for the description.';
+  //                     return null;
+  //                   },
+  //                 ),
+  //               ],
+  //             ),
+  //           ),
+  //           actions: <Widget>[
+  //             TextButton(
+  //               child: Text('Discard'),
+  //               onPressed: () {
+  //                 _conditionTitleController.clear();
+  //                 _conditionDescriptionController.clear();
+  //                 Navigator.of(context).pop();
+  //               },
+  //             ),
+  //             TextButton(
+  //               child: Text('Add'),
+  //               onPressed: () {
+  //                 _saveForm(contId, ConditionType.Normal);
+  //               },
+  //             ),
+  //           ],
+  //         );
+  //       });
+  // }
+  //
+  // bool isNumeric(String s) {
+  //   if (s == null) {
+  //     return false;
+  //   }
+  //   return double.tryParse(s) != null;
+  // }
+  //
+  // Future<void> _newSpecialConditionDialog(
+  //     String contId, ConditionType type) async {
+  //   return await showDialog(
+  //       context: context,
+  //       builder: (context) {
+  //         return AlertDialog(
+  //           title: type == ConditionType.Payment
+  //               ? Text('Add New Payment '
+  //               'Condition')
+  //               : Text('Add New Duration '
+  //               'Condition'),
+  //           content: Form(
+  //             key: _formKey,
+  //             child: Column(
+  //               mainAxisSize: MainAxisSize.min,
+  //               children: [
+  //                 TextFormField(
+  //                   decoration: type == ConditionType.Payment
+  //                       ? InputDecoration(labelText: 'Enter UNT amount')
+  //                       : InputDecoration(
+  //                       labelText: 'Enter Duration (in seconds)'
+  //                           ' from the seal time'),
+  //                   keyboardType: TextInputType.number,
+  //                   controller: type == ConditionType.Payment
+  //                       ? _paymentConditionAmountController
+  //                       : _durationConditionAmountController,
+  //                   validator: (value) {
+  //                     if (value.isEmpty) return 'Please enter an amount.';
+  //                     if (!isNumeric(value)) return 'Please enter a double.';
+  //                     return null;
+  //                   },
+  //                 ),
+  //               ],
+  //             ),
+  //           ),
+  //           actions: <Widget>[
+  //             TextButton(
+  //               child: Text('Discard'),
+  //               onPressed: () {
+  //                 _paymentConditionAmountController.clear();
+  //                 _durationConditionAmountController.clear();
+  //                 Navigator.of(context).pop();
+  //               },
+  //             ),
+  //             TextButton(
+  //               child: Text('Add'),
+  //               onPressed: () {
+  //                 type == ConditionType.Payment
+  //                     ? _saveForm(contId, ConditionType.Payment)
+  //                     : _saveForm(contId, ConditionType.Duration);
+  //               },
+  //             ),
+  //           ],
+  //         );
+  //       });
+  // }
+
+  Widget build(BuildContext context) {
+    final contractId = ModalRoute.of(context).settings.arguments as String;
+    final loadedContract =
+        Provider.of<Contracts>(context, listen: true).findById(contractId);
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(loadedContract.title),
+      ),
+      body: Column(
+        children: [
+          ContractDetailInfoPanel(loadedContract),
+          Container(
+            padding: EdgeInsets.symmetric(
+              vertical: 10,
+              horizontal: 8,
+            ),
+            // alignment: Alignment.centerLeft,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  'Agreement Conditions:',
+                  style: TextStyle(
+                    fontSize: 16,
+                  ),
+                ),
+                // TextButton(
+                //   onPressed: () async {
+                //     await _newConditionDialog(contractId);
+                //   },
+                //   child: Row(
+                //     children: [
+                //       Icon(Icons.add),
+                //       Text('Add New Condition'),
+                //     ],
+                //   ),
+                // ),
+                // TextButton(
+                //   onPressed: () async {
+                //     await _newSpecialConditionDialog(
+                //         contractId, ConditionType.Payment);
+                //   },
+                //   child: Row(
+                //     children: [
+                //       Icon(Icons.add),
+                //       Text('Add New Payment Condition'),
+                //     ],
+                //   ),
+                // ),
+                // TextButton(
+                //   onPressed: () async {
+                //     await _newSpecialConditionDialog(
+                //         contractId, ConditionType.Duration);
+                //   },
+                //   child: Row(
+                //     children: [
+                //       Icon(Icons.add),
+                //       Text('Add New Duration Condition'),
+                //     ],
+                //   ),
+                // ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: _isLoading
+                ? Center(
+                    child: CircularProgressIndicator(),
+                  )
+                : ContractConditionsPanel(loadedContract),
+            flex: 6,
+          ),
+          Expanded(
+            flex: 1,
+            child: Container(
+                // TODO: More Information or actions here?
+                ),
+          )
+        ],
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: () {
+          Navigator.of(context).pushNamed(
+            MessagingScreen.routeName,
+            arguments: loadedContract.contractId,
+          );
+        },
+        label: Text('Agreement Chat'),
+        icon: Icon(Icons.chat),
+      ),
+    );
+  }
+}
--- frontend_scv/lib/widgets/judge_active_cases_panel.dart
+++ frontend_scv/lib/widgets/judge_active_cases_panel.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:unison/models/global.dart';
+import 'package:unison/services/Server/commonService.dart';
 import 'package:unison/services/Server/judgeService.dart';
+import 'package:unison/widgets/judge_active_contract_item.dart';
 
 class JudgeActiveCasesPanel extends StatefulWidget {
   @override
@@ -8,7 +10,6 @@ class JudgeActiveCasesPanel extends StatefulWidget {
 }
 
 class _JudgeActiveCasesPanelState extends State<JudgeActiveCasesPanel> {
-  final _controller = ScrollController();
   @override
   void didChangeDependencies() {
     super.didChangeDependencies();
@@ -17,99 +18,25 @@ class _JudgeActiveCasesPanelState extends State<JudgeActiveCasesPanel> {
 
   @override
   Widget build(BuildContext context) {
-    JudgeService judgeService = JudgeService();
-
+    // JudgeService judgeService = JudgeService();
+    CommonService commonMock = CommonService();
     return FutureBuilder(
-        future: judgeService.getInvolvedAgreements(Global.userAddress),
+        // future: judgeService.getInvolvedAgreements(Global.userAddress),
+        future: commonMock.getInvolvedAgreements(Global.userAddress),
         builder: (context, agreementsSnapshot) {
           return agreementsSnapshot.connectionState != ConnectionState.done
               ? CircularProgressIndicator()
               : agreementsSnapshot.data.length == 0
                   ? Text('No Assignments')
-                  : Text(agreementsSnapshot.data.toString());
-
-          // ListView.builder(
-          //     controller: _controller,
-          //     padding: const EdgeInsets.all(10.0),
-          //     itemCount: messagesSnapshot.data.length,
-          //     // reverse: true,
-          //     // shrinkWrap: true,
-          //     itemBuilder: (BuildContext context, int index) {
-          //       final message = messagesSnapshot.data[index];
-          //       bool isCurrentUser = message.sender == Global.userAddress;
-          //       return Container(
-          //         margin: EdgeInsets.only(top: 10),
-          //         child: Column(
-          //           children: [
-          //             Row(
-          //               mainAxisAlignment: isCurrentUser
-          //                   ? MainAxisAlignment.end
-          //                   : MainAxisAlignment.start,
-          //               crossAxisAlignment: CrossAxisAlignment.end,
-          //               children: [
-          //                 if (!isCurrentUser)
-          //                   CircleAvatar(
-          //                     radius: 15,
-          //                     // backgroundImage: AssetImage(user.avatar),
-          //                   ),
-          //                 SizedBox(
-          //                   width: 10,
-          //                 ),
-          //                 Container(
-          //                   padding: EdgeInsets.all(10),
-          //                   constraints: BoxConstraints(
-          //                       maxWidth:
-          //                           MediaQuery.of(context).size.width *
-          //                               0.6),
-          //                   decoration: BoxDecoration(
-          //                       color: isCurrentUser
-          //                           ? Colors.deepOrange
-          //                           : Colors.cyan,
-          //                       borderRadius: BorderRadius.only(
-          //                         topLeft: Radius.circular(16),
-          //                         topRight: Radius.circular(16),
-          //                         bottomLeft: Radius.circular(
-          //                             isCurrentUser ? 12 : 0),
-          //                         bottomRight: Radius.circular(
-          //                             isCurrentUser ? 0 : 12),
-          //                       )),
-          //                   child: Text(
-          //                     messagesSnapshot.data[index].messageText,
-          //                     // style: MyTheme.bodyTextMessage.copyWith(
-          //                     //     color: isCurrentUser ? Colors.white : Colors.grey[800]),
-          //                   ),
-          //                 ),
-          //               ],
-          //             ),
-          //             Padding(
-          //               padding: const EdgeInsets.only(top: 5),
-          //               child: Row(
-          //                 mainAxisAlignment: isCurrentUser
-          //                     ? MainAxisAlignment.end
-          //                     : MainAxisAlignment.start,
-          //                 children: [
-          //                   if (!isCurrentUser)
-          //                     SizedBox(
-          //                       width: 40,
-          //                     ),
-          //                   SizedBox(
-          //                     width: 8,
-          //                   ),
-          //                   Text(
-          //                     DateFormat('yyyy-MM-dd hh:mm')
-          //                         .format(message.dateSent),
-          //                     style: TextStyle(
-          //                       fontSize: 10,
-          //                     ),
-          //                   )
-          //                 ],
-          //               ),
-          //             )
-          //           ],
-          //         ),
-          //       );
-          //     },
-          //   );
+                  // : Text(agreementsSnapshot.data.toString());
+                  : ListView.builder(
+                      padding: const EdgeInsets.all(10.0),
+                      itemCount: agreementsSnapshot.data.length,
+                      itemBuilder: (BuildContext context, int index) {
+                        final agreement = agreementsSnapshot.data[index];
+                        return JudgeActiveContractItem(agreement);
+                      },
+                    );
         });
   }
 }
