f9130b5fc3e2e405a69576da9fb5aeeb93d09809
JasonMaritz <u19053292@tuks.co.za>
2021-08-13 12:07:25 +0200

    Added createAgreement and juryAssigned event parameter lists to config for event listener creation

--- Smart-Contract-Verifier-Server/src/main/kotlin/com/savannasolutions/SmartContractVerifierServer/judges/configurations/ContractConfig.kt
+++ Smart-Contract-Verifier-Server/src/main/kotlin/com/savannasolutions/SmartContractVerifierServer/judges/configurations/ContractConfig.kt
@@ -2,8 +2,25 @@ package com.savannasolutions.SmartContractVerifierServer.judges.configurations
 
 import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.boot.context.properties.ConstructorBinding
+import org.web3j.abi.TypeReference
+import org.web3j.abi.datatypes.*
+import java.util.*
+import kotlin.collections.ArrayList
 
 
 @ConfigurationProperties("com.unison")
 @ConstructorBinding
-data class ContractConfig(var nodeAddress: String, var contractId: String,)
+data class ContractConfig(var nodeAddress: String, var contractId: String,){
+
+    var creationList = listOf<TypeReference<*>>(
+        object : TypeReference<Address>(false) {},
+        object : TypeReference<Address>(false) {},
+        object : TypeReference<Uint>(false) {},
+        object : TypeReference<Utf8String>(false) {},
+    )
+
+    var juryList = listOf<TypeReference<*>>(
+        object : TypeReference<Uint>(false) {},
+        object : TypeReference<DynamicArray<Address>>(false) {},
+    )
+}
