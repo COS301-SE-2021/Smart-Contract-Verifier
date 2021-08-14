9b9509abe1543421675305b1b2c75fa269114f11
Ronan <u19288736@tuks.co.za>
2021-08-14 14:34:34 +0200

    Updated comment.

--- frontend_scv/lib/services/Blockchain/unisonService.dart
+++ frontend_scv/lib/services/Blockchain/unisonService.dart
@@ -114,6 +114,7 @@ class UnisonService {
   }
 
   //Pay the platform fee for an agreement
+  //Someone (anyone) has to pay the platform fee after the agreement is accepted, and that will make it active.
   Future<void> payPlatformFee(BigInt id) async {
 
     await _smC.makeWriteCall('payPlatformFee', [id]);
