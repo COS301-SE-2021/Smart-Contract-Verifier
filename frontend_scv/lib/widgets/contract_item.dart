8d238489d0d5bb29ffe979c0dd9b5747a37bf838
Kevin <kevindupreez8@gmail.com>
2021-08-14 21:08:25 +0200

    Updated Jdenticons

--- frontend_scv/lib/widgets/contract_item.dart
+++ frontend_scv/lib/widgets/contract_item.dart
@@ -11,7 +11,26 @@ class ContractItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final contract = Provider.of<Contract>(context);
-    final String rawSvg = Jdenticon.toSvg(contract.contractId);
+    final String rawSvg = Jdenticon.toSvg(
+      contract.contractId,
+      colorSaturation: 1.0,
+      grayscaleSaturation: 1.0,
+      colorLightnessMinValue: 0.40,
+      colorLightnessMaxValue: 0.80,
+      grayscaleLightnessMinValue: 0.30,
+      grayscaleLightnessMaxValue: 0.90,
+      backColor: '#ff7800',
+      hues: [205],
+    );
+
+    /*
+    * Hues = new HueCollection { { 191, HueUnit.Degrees } },
+            BackColor = Color.FromRgba(255, 130, 0, 255),
+            ColorLightness = Range.Create(0.29f, 0.60f),
+            GrayscaleLightness = Range.Create(0.53f, 0.86f),
+            ColorSaturation = 1.00f,
+            GrayscaleSaturation = 1.00f
+    * */
 
     return GestureDetector(
       onTap: () {
