ec227d941effd52ffe2be422d6335766efb1a9d8
Kaleb-Bruwer <u19064242@tuks.co.za>
2021-08-26 13:17:56 +0200

    Added more unit tests

--- SmartContract/test/FeeContract.test.js
+++ SmartContract/test/FeeContract.test.js
@@ -24,15 +24,13 @@ async function testRatio(feeC, users, jurors){
     var platformFee = await feeC.getPlatformFee();
     platformFee = BigInt(platformFee);
 
-    console.log(platformFee);
     var expectedFee = calcNewFee(oldPlatformFee, users,jurors);
-    console.log(expectedFee);
 
     assert(platformFee == expectedFee, "Invalid Platform fee update");
 }
 
 contract('FeeContract', (accounts) =>{
-    describe("FeeContract unit tests", async () =>{
+    describe("FeeContract users change", async () =>{
         var feeC;
 
         before(async ()=>{
@@ -65,7 +63,7 @@ contract('FeeContract', (accounts) =>{
         })
     })
 
-    describe("FeeContract unit tests 2", async () =>{
+    describe("FeeContract jurors change", async () =>{
         var feeC;
 
         before(async ()=>{
@@ -76,5 +74,35 @@ contract('FeeContract', (accounts) =>{
         it("Increased jurors", async() =>{
             await testRatio(feeC, 25, 6);
         })
+
+        it("Decreased jurors", async() =>{
+            await testRatio(feeC, 25, 4);
+        })
     })
+
+    describe("FeeContract large values", async () =>{
+        var feeC;
+
+        before(async ()=>{
+            // accounts[0] is owner
+            feeC = await FeeContract.new(accounts[0]);
+        })
+
+        it("Standard", async() =>{
+            await testRatio(feeC, 5005, 1001);
+        })
+
+        it("Decreased jurors", async() =>{
+            await testRatio(feeC, 5005, 950);
+        })
+
+        it("Increased jurors", async() =>{
+            await testRatio(feeC, 5005, 1500);
+        })
+
+        it("Increased users", async() =>{
+            await testRatio(feeC, 10005, 1001);
+        })
+    })
+
 })
