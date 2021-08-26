d917a1eba2507f67aea121e3963ef019913b9036
Cobus Redelinghuys <u19089296@tuks.co.za>
2021-08-27 01:39:06 +0200

    Refactored file name. Updated unit tests to new controller

--- Smart-Contract-Verifier-Server/src/test/kotlin/com/savannasolutions/SmartContractVerifierServer/UnitTests/user/user/RetrieveUserAgreementsUnitTests.kt
+++ Smart-Contract-Verifier-Server/src/test/kotlin/com/savannasolutions/SmartContractVerifierServer/UnitTests/user/user/RetrieveUserAgreementsUnitTests.kt
@@ -1,4 +1,4 @@
-package com.savannasolutions.SmartContractVerifierServer.user.services
+package com.savannasolutions.SmartContractVerifierServer.UnitTests.user.user
 
 import com.savannasolutions.SmartContractVerifierServer.common.ResponseStatus
 import com.savannasolutions.SmartContractVerifierServer.negotiation.models.Agreements
@@ -6,52 +6,35 @@ import com.savannasolutions.SmartContractVerifierServer.negotiation.repositories
 import com.savannasolutions.SmartContractVerifierServer.negotiation.repositories.ConditionsRepository
 import com.savannasolutions.SmartContractVerifierServer.user.models.User
 import com.savannasolutions.SmartContractVerifierServer.user.repositories.UserRepository
-import com.savannasolutions.SmartContractVerifierServer.user.requests.RetrieveUserAgreementsRequest
-import com.savannasolutions.SmartContractVerifierServer.security.requests.UserExistsRequest
-import com.savannasolutions.SmartContractVerifierServer.security.services.SecurityService
+import com.savannasolutions.SmartContractVerifierServer.user.responses.RetrieveUserAgreementsResponse
+import com.savannasolutions.SmartContractVerifierServer.user.services.UserService
 import org.junit.Test
 import org.mockito.kotlin.mock
 import org.mockito.kotlin.whenever
 import java.util.*
 import kotlin.collections.ArrayList
 import kotlin.test.assertEquals
-import kotlin.test.assertFalse
 import kotlin.test.assertNotNull
 import kotlin.test.assertTrue
 
-internal class UserServiceTest {
+internal class RetrieveUserAgreementsUnitTests {
     private val userRepository : UserRepository = mock()
     private val agreementsRepository : AgreementsRepository = mock()
     private val conditionsRepository : ConditionsRepository = mock()
     private val userService = UserService(userRepository,agreementsRepository, conditionsRepository)
 
-    @Test
-    fun `RetrieveUserAgreements successful without agreement`()
+    private var userA = User("0x743Fb032c0bE976e1178d8157f911a9e825d9E23", "TestA")
+
+    private fun parameterizedRetrieveUserAgreements(userID: String,
+                                                    userExists: Boolean) : RetrieveUserAgreementsResponse
     {
         //Given
-        val user = User("0x743Fb032c0bE976e1178d8157f911a9e825d9E23", "TestA")
-        whenever(userRepository.existsById(user.publicWalletID)).thenReturn(true)
-        whenever(userRepository.getById(user.publicWalletID)).thenReturn(user)
-
-
-        //When
-        val response = userService.retrieveUserAgreements(RetrieveUserAgreementsRequest(user.publicWalletID))
-
-        //Then
-        assertEquals(response.status, ResponseStatus.SUCCESSFUL)
-        assertEquals(response.Agreements!!, emptyList())
-    }
-
-    @Test
-    fun `RetrieveUserAgreements successful with an agreement`()
-    {
-        //Given
-        var userA = User("0x743Fb032c0bE976e1178d8157f911a9e825d9E23", "TestA")
+        val tempUser = User(userID)
         val userB = User("0x37Ec9a8aBFa094b24054422564e68B08aF3114B4", "TestB")
 
         var agreement = Agreements(UUID.fromString("6e28cc77-d2e2-4221-abd7-7a6d0e84dbd3"),
-                CreatedDate = Date(),
-                MovedToBlockChain = true)
+            CreatedDate = Date(),
+            MovedToBlockChain = true)
 
         agreement = agreement.apply { users.add(userA) }
         agreement = agreement.apply { users.add(userB)}
@@ -68,15 +51,40 @@ internal class UserServiceTest {
         userList.add(userA)
         userList.add(userB)
 
-        whenever(userRepository.existsById(userA.publicWalletID)).thenReturn(true)
-        whenever(userRepository.getById(userA.publicWalletID)).thenReturn(userA)
-        whenever(agreementsRepository.getAllByUsersContaining(userA)).thenReturn(list)
+        //when
+        whenever(userRepository.existsById(userID)).thenReturn(userExists)
+        whenever(userRepository.getById(userID)).thenReturn(tempUser)
+        whenever(agreementsRepository.getAllByUsersContaining(tempUser)).thenReturn(list)
         whenever(conditionsRepository.getAllByContract(agreement)).thenReturn(agreement.conditions)
         whenever(userRepository.getUsersByAgreementsContaining(agreement)).thenReturn(userList)
 
+        //then
+        return userService.retrieveUserAgreements(userID)
+    }
+
+    @Test
+    fun `RetrieveUserAgreements successful without agreement`()
+    {
+        //Given
+        val user = User("0x743Fb032c0bE976e1178d8157f911a9e825d9E23", "TestA")
+        whenever(userRepository.existsById(user.publicWalletID)).thenReturn(true)
+        whenever(userRepository.getById(user.publicWalletID)).thenReturn(user)
+
 
         //When
-        val response = userService.retrieveUserAgreements(RetrieveUserAgreementsRequest(userA.publicWalletID))
+        val response = userService.retrieveUserAgreements(user.publicWalletID)
+
+        //Then
+        assertEquals(response.status, ResponseStatus.SUCCESSFUL)
+        assertEquals(response.Agreements!!, emptyList())
+    }
+
+    @Test
+    fun `RetrieveUserAgreements successful with an agreement`()
+    {
+
+        //When
+        val response = parameterizedRetrieveUserAgreements(userA.publicWalletID, true)
 
         //Then
         assertEquals(response.status, ResponseStatus.SUCCESSFUL)
@@ -88,14 +96,9 @@ internal class UserServiceTest {
     fun `RetrieveUserAgreements userid is empty`()
     {
         //Given
-        val userA = User("0x743Fb032c0bE976e1178d8157f911a9e825d9E23", "TestA")
-
-        whenever(userRepository.existsById(userA.publicWalletID)).thenReturn(true)
-        whenever(userRepository.getById(userA.publicWalletID)).thenReturn(userA)
-
 
         //When
-        val response = userService.retrieveUserAgreements(RetrieveUserAgreementsRequest(""))
+        val response = parameterizedRetrieveUserAgreements("", true)
 
         //Then
         assertEquals(response.status, ResponseStatus.FAILED)
@@ -105,13 +108,9 @@ internal class UserServiceTest {
     fun `RetrieveUserAgreements does not exist`()
     {
         //Given
-        val userA = User("0x743Fb032c0bE976e1178d8157f911a9e825d9E23", "TestA")
-
-        whenever(userRepository.existsById(userA.publicWalletID)).thenReturn(false)
-
 
         //When
-        val response = userService.retrieveUserAgreements(RetrieveUserAgreementsRequest(userA.publicWalletID))
+        val response = parameterizedRetrieveUserAgreements(userA.publicWalletID, false)
 
         //Then
         assertEquals(response.status, ResponseStatus.FAILED)
