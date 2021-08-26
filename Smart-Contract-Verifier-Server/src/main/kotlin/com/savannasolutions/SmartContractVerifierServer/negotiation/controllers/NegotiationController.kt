7c325503d2ec2524b28e5ae812f766be4742c406
JasonMaritz <u19053292@tuks.co.za>
2021-08-26 11:39:08 +0200

    Rewrote NegotiationController.kt to use new path variables

--- Smart-Contract-Verifier-Server/src/main/kotlin/com/savannasolutions/SmartContractVerifierServer/negotiation/controllers/NegotiationController.kt
+++ Smart-Contract-Verifier-Server/src/main/kotlin/com/savannasolutions/SmartContractVerifierServer/negotiation/controllers/NegotiationController.kt
@@ -3,55 +3,58 @@ package com.savannasolutions.SmartContractVerifierServer.negotiation.controllers
 import com.savannasolutions.SmartContractVerifierServer.negotiation.requests.*
 import com.savannasolutions.SmartContractVerifierServer.negotiation.services.NegotiationService
 import org.springframework.web.bind.annotation.*
+import java.util.*
 
 @CrossOrigin
 @RestController
-class NegotiationController constructor(private val negotiationService: NegotiationService) {
+class NegotiationController constructor(private val negotiationService: NegotiationService,) {
 
     @GetMapping("/user/{userId}/agreement/{agreementId}")
-    fun getAgreementDetails(@RequestBody getAgreementDetailsRequest: GetAgreementDetailsRequest) =
-        negotiationService.getAgreementDetails(getAgreementDetailsRequest)
+    fun getAgreementDetails(@PathVariable userId:String, @PathVariable agreementId: UUID,) =
+        negotiationService.getAgreementDetails(userId, agreementId)
 
     @GetMapping("/user/{userId}/agreement/{agreementId}/condition")
-    fun getAllConditions(@RequestBody getAllConditionsRequest: GetAllConditionsRequest) =
-        negotiationService.getAllConditions(getAllConditionsRequest)
+    fun getAllConditions(@PathVariable userId: String, @PathVariable agreementId: UUID,) =
+        negotiationService.getAllConditions(userId, agreementId)
 
     @PutMapping("/user/{userId}/agreement/{agreementId}/condition/{conditionId}/accept")
-    fun acceptCondition(@RequestBody acceptConditionRequest: AcceptConditionRequest) =
-        negotiationService.acceptCondition(acceptConditionRequest)
+    fun acceptCondition(@PathVariable userId: String, @PathVariable agreementId: UUID, @PathVariable conditionId: UUID,) =
+        negotiationService.acceptCondition(userId, agreementId, conditionId)
 
     @PostMapping("/user/{userId}/agreement/{agreementId}/condition/{conditionId}/reject")
-    fun rejectCondition(@RequestBody rejectConditionRequest: RejectConditionRequest) =
-        negotiationService.rejectCondition(rejectConditionRequest)
+    fun rejectCondition(@PathVariable userId: String, @PathVariable agreementId: UUID, @PathVariable conditionId: UUID,) =
+        negotiationService.rejectCondition(userId, agreementId, conditionId)
 
     @GetMapping("/hello")
     fun hello() = "HELLO!"
 
     @PostMapping("/user/{userId}/agreement/{agreementId}/condition")
-    fun createCondition(@RequestBody createConditionRequest: CreateConditionRequest) =
-        negotiationService.createCondition(createConditionRequest)
+    fun createCondition(@PathVariable userId: String,
+                        @PathVariable agreementId: UUID,
+                        @RequestBody createConditionRequest: CreateConditionRequest,) =
+        negotiationService.createCondition(userId, agreementId, createConditionRequest)
 
     @PostMapping("/user/{userId}/agreement")
-    fun createAgreement(@RequestBody createAgreementRequest: CreateAgreementRequest) =
-        negotiationService.createAgreement(createAgreementRequest)
-
-    @PutMapping("/user/{userId}/agreement/{agreementId}")
-    fun sealAgreement(@RequestBody sealAgreementRequest: SealAgreementRequest) =
-        negotiationService.sealAgreement(sealAgreementRequest)
+    fun createAgreement(@PathVariable userId: String, @RequestBody createAgreementRequest: CreateAgreementRequest,) =
+        negotiationService.createAgreement(userId, createAgreementRequest)
 
     @GetMapping("/user/{userId}/agreement/{agreementId}/condition/{conditionId}")
-    fun getConditionDetails(@RequestBody getConditionDetailsRequest: GetConditionDetailsRequest) =
-        negotiationService.getConditionDetails(getConditionDetailsRequest)
+    fun getConditionDetails(@PathVariable userId: String, agreementId: UUID, conditionId: UUID,) =
+        negotiationService.getConditionDetails(userId, agreementId, conditionId)
 
     @PostMapping("/user/{userId}/agreement/{agreementId}/condition/payment")
-    fun setPaymentCondition(@RequestBody setPaymentConditionRequest: SetPaymentConditionRequest) =
-        negotiationService.setPaymentCondition(setPaymentConditionRequest)
+    fun setPaymentCondition(@PathVariable userId: String,
+                            @PathVariable agreementId: UUID,
+                            @RequestBody setDurationConditionRequest: SetPaymentConditionRequest,) =
+        negotiationService.setPaymentCondition(userId, agreementId, setPaymentConditionRequest)
 
     @PostMapping("/user/{userId}/agreement/{agreementId}/condition/duration")
-    fun setPaymentCondition(@RequestBody setDurationConditionRequest: SetDurationConditionRequest) =
-        negotiationService.setDurationCondition(setDurationConditionRequest)
+    fun setPaymentCondition(@PathVariable userId: String,
+                            @PathVariable agreementId: UUID,
+                            @RequestBody setDurationConditionRequest: SetDurationConditionRequest,) =
+        negotiationService.setDurationCondition(userId, agreementId, setDurationConditionRequest)
 
     @GetMapping("/judge/{userId}/agreement")
-    fun getJudgingAgreements(@RequestBody getJudgingAgreementsRequest: GetJudgingAgreementsRequest) =
-        negotiationService.getJudgingAgreements(getJudgingAgreementsRequest)
+    fun getJudgingAgreements(@PathVariable userId: String,) =
+        negotiationService.getJudgingAgreements(userId)
 }
--- Smart-Contract-Verifier-Server/src/main/kotlin/com/savannasolutions/SmartContractVerifierServer/negotiation/requests/SetDurationConditionRequest.kt
+++ Smart-Contract-Verifier-Server/src/main/kotlin/com/savannasolutions/SmartContractVerifierServer/negotiation/requests/SetDurationConditionRequest.kt
@@ -4,6 +4,4 @@ import com.fasterxml.jackson.annotation.JsonProperty
 import java.time.Duration
 import java.util.*
 
-data class SetDurationConditionRequest(@JsonProperty("ProposedUser") val PreposedUser: String,
-                                       @JsonProperty("AgreementID") val AgreementID: UUID,
-                                       @JsonProperty("Duration") val Duration: Duration,)
+data class SetDurationConditionRequest(@JsonProperty("Duration") val Duration: Duration,)
