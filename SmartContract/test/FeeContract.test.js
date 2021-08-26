d9c18eb6843b5e832cc16d08c3ed0abd2966c3b0
Kaleb-Bruwer <u19064242@tuks.co.za>
2021-08-26 12:48:00 +0200

    Unit test

--- SmartContract/test/FeeContract.test.js
+++ SmartContract/test/FeeContract.test.js
@@ -4,6 +4,17 @@ const { assert } = require('chai');
 const truffleAssert = require('truffle-assertions');
 require('chai').use(require('chai-as-promised')).should()
 
+function calcNewFee(oldFee, users, jurors){
+    // oldFee * ratio / targetRatio
+
+    result = oldFee;
+    result *= BigInt(users);
+    result /= BigInt(jurors);
+    result /= BigInt(5); //target ratio
+
+    return result;
+}
+
 contract('FeeContract', (accounts) =>{
     describe("FeeContract unit tests", async () =>{
         var feeC;
@@ -24,7 +35,7 @@ contract('FeeContract', (accounts) =>{
             assert(stakingAmount == 10000, "issue with getStakingAmount");
         })
 
-        it("users to jurors ratio is correct", async() =>{
+        it("users ratio is correct", async() =>{
             var oldPlatformFee = await feeC.getPlatformFee();
             oldPlatformFee = BigInt(oldPlatformFee);
 
@@ -35,6 +46,23 @@ contract('FeeContract', (accounts) =>{
 
             assert(platformFee == oldPlatformFee, "Platform fee changed when ratio was correct");
 
+        })
+
+        it("users ratio is over", async() =>{
+            var oldPlatformFee = await feeC.getPlatformFee();
+            oldPlatformFee = BigInt(oldPlatformFee);
+
+            await feeC.updatePlatformFee(26, 5);
+
+            var platformFee = await feeC.getPlatformFee();
+            platformFee = BigInt(platformFee);
+
+            console.log(platformFee);
+
+            var expectedFee = calcNewFee(oldPlatformFee, 26,5);
+            console.log(expectedFee);
+
+            assert(platformFee == expectedFee, "Platform fee changed when ratio was correct");
         })
     })
 
