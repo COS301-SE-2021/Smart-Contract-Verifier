96cff9cc2cba91b239b95855c91ef09f9c457209
JasonMaritz <u19053292@tuks.co.za>
2021-08-26 12:05:06 +0200

    Removed or altered unused request objects

--- Smart-Contract-Verifier-Server/src/main/kotlin/com/savannasolutions/SmartContractVerifierServer/security/requests/LoginRequest.kt
+++ Smart-Contract-Verifier-Server/src/main/kotlin/com/savannasolutions/SmartContractVerifierServer/security/requests/LoginRequest.kt
@@ -2,5 +2,4 @@ package com.savannasolutions.SmartContractVerifierServer.security.requests
 
 import com.fasterxml.jackson.annotation.JsonProperty
 
-data class LoginRequest(@JsonProperty("UserId") val userId: String,
-                        @JsonProperty("SignedNonce") val signedNonce: String,)
+data class LoginRequest(@JsonProperty("SignedNonce") val signedNonce: String,)
