73e73106ddc2b517c61a48c87e888df7e9687b70
Kaleb-Bruwer <u19064242@tuks.co.za>
2021-08-26 11:01:28 +0200

    update platform fee functionality in FeeContract

--- SmartContract/src/FeeContract.sol
+++ SmartContract/src/FeeContract.sol
@@ -7,6 +7,11 @@ pragma solidity ^0.8.0;
 contract FeeContract{
     address owner;
 
+    uint stakingAmount = 10000;
+
+    uint platformFee = 1000000000;
+    uint constant targetRatio = 5000; //ratio of agreements to jurors multiplied by 1000
+
     modifier onlyOwner(){
         require(msg.sender == owner);
         _;
@@ -16,5 +21,27 @@ contract FeeContract{
         owner = _owner;
     }
 
+    function getPlatformFee() public view returns(uint){
+        return platformFee;
+    }
+
+    function getStakingAmount() public view returns(uint){
+        return stakingAmount;
+    }
+
+    function updatePlatformFee(uint numActive, uint numJurors) public onlyOwner(){
+
+        // multiplied by 1000 an extra time so that the 1000's don't cancel out
+
+        uint totalRatio = (1000 * 1000 * numActive) / numJurors;
+
+        uint error = totalRatio / targetRatio;
+
+        // error is now a fraction where 1000 represents 1, so platform fee must be divided by 1000
+        // to correct for that
+        platformFee *= error;
+        platformFee /= 1000;
+
+    }
 
 }
