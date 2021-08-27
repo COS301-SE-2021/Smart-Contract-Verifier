d787f39cc74ade40fc08be2e3c917689f00c4f3d
Cobus Redelinghuys <u19089296@tuks.co.za>
2021-08-27 20:50:09 +0200

    Updated to use ApiResponse object

--- Smart-Contract-Verifier-Server/src/main/kotlin/com/savannasolutions/SmartContractVerifierServer/common/responseErrorMessages/commonResponseErrorMessages.kt
+++ Smart-Contract-Verifier-Server/src/main/kotlin/com/savannasolutions/SmartContractVerifierServer/common/responseErrorMessages/commonResponseErrorMessages.kt
@@ -6,5 +6,9 @@ class commonResponseErrorMessages {
         const val agreementDoesNotExist = "Agreement does not exist in database"
         const val messageDoesNotExist = "Message does not exist in database"
         const val userNotPartOfAgreement = "User not part of the provided agreement"
+        const val conditionDoesNotExist = "Condition does not exist in database"
+        const val conditionIsNotPending = "Condition is not pending"
+        const val titleIsEmpty = "Title is empty"
+        const val descriptionEmpty = "Description is empty"
     }
 }
