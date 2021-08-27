deb72a0acb08387d81f7d8076505912bd6ebcb20
Cobus Redelinghuys <u19089296@tuks.co.za>
2021-08-27 20:17:06 +0200

    Updated commonResponseErrorMessages

--- Smart-Contract-Verifier-Server/src/main/kotlin/com/savannasolutions/SmartContractVerifierServer/common/responseErrorMessages/commonResponseErrorMessages.kt
+++ Smart-Contract-Verifier-Server/src/main/kotlin/com/savannasolutions/SmartContractVerifierServer/common/responseErrorMessages/commonResponseErrorMessages.kt
@@ -4,5 +4,7 @@ class commonResponseErrorMessages {
     companion object{
         const val userDoesNotExist = "User does not exist in database"
         const val agreementDoesNotExist = "Agreement does not exist in database"
+        const val messageDoesNotExist = "Message does not exist in database"
+        const val userNotPartOfAgreement = "User not part of the provided agreement"
     }
 }
