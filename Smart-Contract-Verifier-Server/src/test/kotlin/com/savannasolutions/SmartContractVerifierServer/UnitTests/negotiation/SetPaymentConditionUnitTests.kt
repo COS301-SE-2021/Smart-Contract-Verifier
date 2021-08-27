19151dd4bd087ff59665ad7d5e64b8378ded2126
Cobus Redelinghuys <u19089296@tuks.co.za>
2021-08-27 23:04:02 +0200

    Updated to use ApiResponse object for test

--- Smart-Contract-Verifier-Server/src/test/kotlin/com/savannasolutions/SmartContractVerifierServer/UnitTests/negotiation/SetPaymentConditionUnitTests.kt
+++ Smart-Contract-Verifier-Server/src/test/kotlin/com/savannasolutions/SmartContractVerifierServer/UnitTests/negotiation/SetPaymentConditionUnitTests.kt
@@ -1,5 +1,6 @@
 package com.savannasolutions.SmartContractVerifierServer.UnitTests.negotiation
 
+import com.savannasolutions.SmartContractVerifierServer.common.commonDataObjects.ApiResponse
 import com.savannasolutions.SmartContractVerifierServer.common.commonDataObjects.ResponseStatus
 import com.savannasolutions.SmartContractVerifierServer.contracts.repositories.JudgesRepository
 import com.savannasolutions.SmartContractVerifierServer.negotiation.models.Agreements
@@ -34,7 +35,7 @@ internal class SetPaymentConditionUnitTests {
     private fun parameterizedSetPaymentCondition(userID: String,
                                                     paymentAmount: Double,
                                                     agreementExists: Boolean,
-                                                    propUser: String): SetPaymentConditionResponse
+                                                    propUser: String): ApiResponse<SetPaymentConditionResponse>
     {
         //given
         var mockAgreement = Agreements(ContractID = UUID.fromString("7fa870d3-2119-4b41-8062-46e2d5136937"),
