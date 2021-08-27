e73f2d174b0dda99498d5dd708ae30a61d3b57d3
JasonMaritz <u19053292@tuks.co.za>
2021-08-27 19:49:44 +0200

    Finished link evidence upload

--- Smart-Contract-Verifier-Server/src/main/kotlin/com/savannasolutions/SmartContractVerifierServer/evidence/services/EvidenceService.kt
+++ Smart-Contract-Verifier-Server/src/main/kotlin/com/savannasolutions/SmartContractVerifierServer/evidence/services/EvidenceService.kt
@@ -3,7 +3,12 @@ package com.savannasolutions.SmartContractVerifierServer.evidence.services
 import com.savannasolutions.SmartContractVerifierServer.common.ResponseStatus
 import com.savannasolutions.SmartContractVerifierServer.evidence.configuration.EvidenceConfig
 import com.savannasolutions.SmartContractVerifierServer.evidence.interfaces.EvidenceFileSystem
+import com.savannasolutions.SmartContractVerifierServer.evidence.models.Evidence
+import com.savannasolutions.SmartContractVerifierServer.evidence.models.EvidenceType
+import com.savannasolutions.SmartContractVerifierServer.evidence.models.LinkedEvidence
 import com.savannasolutions.SmartContractVerifierServer.evidence.repositories.EvidenceRepository
+import com.savannasolutions.SmartContractVerifierServer.evidence.repositories.LinkedEvidenceRepository
+import com.savannasolutions.SmartContractVerifierServer.evidence.repositories.UploadedEvidenceRepository
 import com.savannasolutions.SmartContractVerifierServer.evidence.requests.*
 import com.savannasolutions.SmartContractVerifierServer.evidence.responses.*
 import com.savannasolutions.SmartContractVerifierServer.negotiation.repositories.AgreementsRepository
@@ -15,9 +20,11 @@ import javax.annotation.PostConstruct
 
 @Service
 @EnableConfigurationProperties(EvidenceConfig::class)
-class EvidenceService constructor(val agreementsRespository: AgreementsRepository,
+class EvidenceService constructor(val agreementsRepository: AgreementsRepository,
                                   val userRepository: UserRepository,
                                   val evidenceRepository: EvidenceRepository,
+                                  val linkedEvidenceRepository: LinkedEvidenceRepository,
+                                  val uploadedEvidenceRepository: UploadedEvidenceRepository,
                                   val evidenceConfig: EvidenceConfig,) {
     lateinit var fileSystem: EvidenceFileSystem
 
@@ -25,13 +32,14 @@ class EvidenceService constructor(val agreementsRespository: AgreementsRepositor
     fun initialise() {
         fileSystem = evidenceConfig.filesystem
     }
+
     fun uploadEvidence(userId: String, agreementId: UUID, uploadEvidenceRequest: UploadEvidenceRequest): UploadEvidenceResponse? = null
 
     fun linkEvidence(userId: String, agreementId: UUID, linkEvidenceRequest: LinkEvidenceRequest): LinkEvidenceResponse {
-        if(!agreementsRespository.existsById(agreementId))
+        if(!agreementsRepository.existsById(agreementId))
             return LinkEvidenceResponse(ResponseStatus.FAILED)
 
-        var agreement = agreementsRespository.getById(agreementId)
+        val agreement = agreementsRepository.getById(agreementId)
 
         if(!userRepository.existsById(userId))
             return LinkEvidenceResponse(ResponseStatus.FAILED)
@@ -41,7 +49,13 @@ class EvidenceService constructor(val agreementsRespository: AgreementsRepositor
         if(!agreement.users.contains(user))
             return LinkEvidenceResponse(ResponseStatus.FAILED)
 
-        //insert link to table
+        val nEvidence = Evidence("", EvidenceType.LINKED)
+        val linkedEvidence = LinkedEvidence(UUID.fromString("6612469d-ffd8-4126-8c5b-9e5873aaf8f3"),
+                                            linkEvidenceRequest.url,)
+        nEvidence.evidenceUrl = linkedEvidence
+
+        evidenceRepository.save(nEvidence)
+        linkedEvidenceRepository.save(linkedEvidence)
 
         return LinkEvidenceResponse(ResponseStatus.SUCCESSFUL)
     }
