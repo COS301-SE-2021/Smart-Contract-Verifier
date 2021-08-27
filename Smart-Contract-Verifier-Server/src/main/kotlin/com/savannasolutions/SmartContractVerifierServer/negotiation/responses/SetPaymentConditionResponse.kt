77f0b88682cc2ffd6e05335aab0708b61a09802c
Cobus Redelinghuys <u19089296@tuks.co.za>
2021-08-27 21:40:35 +0200

    Updated to use ApiResponse object

--- Smart-Contract-Verifier-Server/src/main/kotlin/com/savannasolutions/SmartContractVerifierServer/negotiation/responses/SetPaymentConditionResponse.kt
+++ Smart-Contract-Verifier-Server/src/main/kotlin/com/savannasolutions/SmartContractVerifierServer/negotiation/responses/SetPaymentConditionResponse.kt
@@ -4,5 +4,4 @@ import com.fasterxml.jackson.annotation.JsonProperty
 import com.savannasolutions.SmartContractVerifierServer.common.commonDataObjects.ResponseStatus
 import java.util.*
 
-data class SetPaymentConditionResponse(@JsonProperty("ConditionID") val conditionID: UUID?,
-                                       @JsonProperty("Status") val  status: Enum<ResponseStatus>,)
+data class SetPaymentConditionResponse(@JsonProperty("ConditionID") val conditionID: UUID?,)
