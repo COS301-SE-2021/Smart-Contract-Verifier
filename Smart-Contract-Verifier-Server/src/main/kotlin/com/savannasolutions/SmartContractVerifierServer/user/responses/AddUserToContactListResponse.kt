eb5494e7d0ca0fb4149694b4bdf8418bb4a20a92
Cobus Redelinghuys <u19089296@tuks.co.za>
2021-08-27 22:13:05 +0200

    Updated to use ApiResponse object

--- Smart-Contract-Verifier-Server/src/main/kotlin/com/savannasolutions/SmartContractVerifierServer/user/responses/AddUserToContactListResponse.kt
+++
@@ -1,6 +0,0 @@
-package com.savannasolutions.SmartContractVerifierServer.user.responses
-
-import com.fasterxml.jackson.annotation.JsonProperty
-import com.savannasolutions.SmartContractVerifierServer.common.commonDataObjects.ResponseStatus
-
-data class AddUserToContactListResponse(@JsonProperty("Status") val status: ResponseStatus)
