dd407b397485230160151185eb6bef256803777f
Cobus Redelinghuys <u19089296@tuks.co.za>
2021-08-27 21:19:17 +0200

    Updated to use ApiResponse object

--- Smart-Contract-Verifier-Server/src/main/kotlin/com/savannasolutions/SmartContractVerifierServer/negotiation/responses/GetAgreementDetailsResponse.kt
+++ Smart-Contract-Verifier-Server/src/main/kotlin/com/savannasolutions/SmartContractVerifierServer/negotiation/responses/GetAgreementDetailsResponse.kt
@@ -4,6 +4,5 @@ import com.fasterxml.jackson.annotation.JsonProperty
 import com.savannasolutions.SmartContractVerifierServer.common.commonDataObjects.AgreementResponse
 import com.savannasolutions.SmartContractVerifierServer.common.commonDataObjects.ResponseStatus
 
-data class GetAgreementDetailsResponse(@JsonProperty("AgreementResponse") val agreementResponse : AgreementResponse?=null,
-                                       @JsonProperty("Status") val status: Enum<ResponseStatus>,)
+data class GetAgreementDetailsResponse(@JsonProperty("AgreementResponse") val agreementResponse : AgreementResponse?=null,)
 
