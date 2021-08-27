9d8e6d275027e963902ac28d0c46e35edae9bc78
Cobus Redelinghuys <u19089296@tuks.co.za>
2021-08-27 19:52:13 +0200

    Added commonResponseErrorMessages for apiResponseObject

---
+++ Smart-Contract-Verifier-Server/src/main/kotlin/com/savannasolutions/SmartContractVerifierServer/common/responseErrorMessages/commonResponseErrorMessages.kt
@@ -0,0 +1,8 @@
+package com.savannasolutions.SmartContractVerifierServer.common.responseErrorMessages
+
+class commonResponseErrorMessages {
+    companion object{
+        const val userDoesNotExist = "User does not exist in database"
+        const val agreementDoesNotExist = "Agreement does not exist in database"
+    }
+}
