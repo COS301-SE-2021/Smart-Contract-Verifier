7992f5c5c3eb7ddd4fc0daf7d0c418306a94952b
JasonMaritz <u19053292@tuks.co.za>
2021-08-27 23:46:13 +0200

    added getAllByContract query to EvidenceRepository.kt

--- Smart-Contract-Verifier-Server/src/main/kotlin/com/savannasolutions/SmartContractVerifierServer/evidence/repositories/EvidenceRepository.kt
+++ Smart-Contract-Verifier-Server/src/main/kotlin/com/savannasolutions/SmartContractVerifierServer/evidence/repositories/EvidenceRepository.kt
@@ -1,8 +1,11 @@
 package com.savannasolutions.SmartContractVerifierServer.evidence.repositories
 
 import com.savannasolutions.SmartContractVerifierServer.evidence.models.Evidence
+import com.savannasolutions.SmartContractVerifierServer.negotiation.models.Agreements
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 
 @Repository
-interface EvidenceRepository:JpaRepository<Evidence, String>
+interface EvidenceRepository:JpaRepository<Evidence, String>{
+    fun getAllByContract(agreements: Agreements) : List<Evidence>
+}
