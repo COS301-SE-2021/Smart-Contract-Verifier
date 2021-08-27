a47c4250767816d84a1013098c161aad2a7d05ed
Cobus Redelinghuys <u19089296@tuks.co.za>
2021-08-27 19:56:22 +0200

    Updated GetAllMessagesByUserResponse to use new apiResponse object

--- Smart-Contract-Verifier-Server/src/main/kotlin/com/savannasolutions/SmartContractVerifierServer/messenger/responses/GetAllMessagesByUserResponse.kt
+++ Smart-Contract-Verifier-Server/src/main/kotlin/com/savannasolutions/SmartContractVerifierServer/messenger/responses/GetAllMessagesByUserResponse.kt
@@ -4,6 +4,4 @@ import com.fasterxml.jackson.annotation.JsonProperty
 import com.savannasolutions.SmartContractVerifierServer.common.commonDataObjects.MessageResponse
 import com.savannasolutions.SmartContractVerifierServer.common.commonDataObjects.ResponseStatus
 
-data class GetAllMessagesByUserResponse(@JsonProperty("Messages") val messages: List<MessageResponse>?=null,
-                                        @JsonProperty("Status") val status: ResponseStatus
-)
+data class GetAllMessagesByUserResponse(@JsonProperty("Messages") val messages: List<MessageResponse>?=null)
