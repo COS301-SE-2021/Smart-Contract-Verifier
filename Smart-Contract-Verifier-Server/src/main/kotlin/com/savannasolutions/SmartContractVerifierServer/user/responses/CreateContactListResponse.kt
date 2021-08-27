658ea835a00c1b8d73454fe824240a70a368c23c
Cobus Redelinghuys <u19089296@tuks.co.za>
2021-08-27 22:20:57 +0200

    Updated to use ApiResponse object

--- Smart-Contract-Verifier-Server/src/main/kotlin/com/savannasolutions/SmartContractVerifierServer/user/responses/CreateContactListResponse.kt
+++ Smart-Contract-Verifier-Server/src/main/kotlin/com/savannasolutions/SmartContractVerifierServer/user/responses/CreateContactListResponse.kt
@@ -4,6 +4,4 @@ import com.fasterxml.jackson.annotation.JsonProperty
 import com.savannasolutions.SmartContractVerifierServer.common.commonDataObjects.ResponseStatus
 import java.util.*
 
-data class CreateContactListResponse(@JsonProperty("ContactListID") val ContactListID:UUID? = null,
-                                     @JsonProperty("Status") val status: ResponseStatus
-)
+data class CreateContactListResponse(@JsonProperty("ContactListID") val ContactListID:UUID? = null,)
