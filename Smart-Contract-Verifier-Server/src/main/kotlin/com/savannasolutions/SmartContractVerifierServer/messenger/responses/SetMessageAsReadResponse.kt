f384f7c0e92331f8deb80fd4bc9ffd179b481e03
Cobus Redelinghuys <u19089296@tuks.co.za>
2021-08-27 20:17:21 +0200

    Removed redundant response

--- Smart-Contract-Verifier-Server/src/main/kotlin/com/savannasolutions/SmartContractVerifierServer/messenger/responses/SetMessageAsReadResponse.kt
+++
@@ -1,6 +0,0 @@
-package com.savannasolutions.SmartContractVerifierServer.messenger.responses
-
-import com.fasterxml.jackson.annotation.JsonProperty
-import com.savannasolutions.SmartContractVerifierServer.common.commonDataObjects.ResponseStatus
-
-data class SetMessageAsReadResponse(@JsonProperty("Status") val status: ResponseStatus)
