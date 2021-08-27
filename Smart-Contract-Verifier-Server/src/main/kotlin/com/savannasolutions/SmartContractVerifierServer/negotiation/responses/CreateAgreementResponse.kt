161e75005c3c711242c52491c3a1f9de3518a4ec
Cobus Redelinghuys <u19089296@tuks.co.za>
2021-08-27 21:03:37 +0200

    Updated to use ApiResponse object

--- Smart-Contract-Verifier-Server/src/main/kotlin/com/savannasolutions/SmartContractVerifierServer/negotiation/responses/CreateAgreementResponse.kt
+++ Smart-Contract-Verifier-Server/src/main/kotlin/com/savannasolutions/SmartContractVerifierServer/negotiation/responses/CreateAgreementResponse.kt
@@ -4,5 +4,4 @@ import com.fasterxml.jackson.annotation.JsonProperty
 import com.savannasolutions.SmartContractVerifierServer.common.commonDataObjects.ResponseStatus
 import java.util.*
 
-data class CreateAgreementResponse(@JsonProperty("AgreementID") val agreementID: UUID? = null,
-                                   @JsonProperty("Status") val  status: Enum<ResponseStatus>,)
+data class CreateAgreementResponse(@JsonProperty("AgreementID") val agreementID: UUID? = null,)
