9b69702d3eed0f9cf8c69637f8c2ac83ed104475
Kevin <kevindupreez8@gmail.com>
2021-08-30 00:08:05 +0200

    Complete rework of view contract screen - focus on SoC - removed redundant code and variables

--- frontend_scv/lib/screens/view_contract_screen.dart
+++ frontend_scv/lib/screens/view_contract_screen.dart
@@ -1,13 +1,13 @@
+import 'package:awesome_loader/awesome_loader.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import 'package:unison/models/condition.dart';
-import 'package:unison/models/global.dart';
-import 'package:unison/screens/messaging_screen.dart';
-import 'package:unison/services/Server/negotiationService.dart';
+import 'package:unison/models/contract.dart';
+import 'package:unison/services/Server/commonService.dart';
+import 'package:unison/widgets/contract_condition_actions_panel.dart';
+import 'package:unison/widgets/contract_conditions_panel.dart';
+import 'package:unison/widgets/contract_detail_info_panel.dart';
+import 'package:unison/widgets/funky_text_widget.dart';
 
-import '../models/contracts.dart';
-import '../widgets/contract_conditions_panel.dart';
-import '../widgets/contract_detail_info_panel.dart';
+import 'messaging_screen.dart';
 
 class ViewContractScreen extends StatefulWidget {
   static const routeName = '/view-contract';
@@ -16,307 +16,73 @@ class ViewContractScreen extends StatefulWidget {
   _ViewContractScreenState createState() => _ViewContractScreenState();
 }
 
-enum ConditionType { Normal, Payment, Duration }
-
 class _ViewContractScreenState extends State<ViewContractScreen> {
-  final _conditionTitleController = TextEditingController();
-  final _conditionDescriptionController = TextEditingController();
-  final _paymentConditionAmountController = TextEditingController();
-  final _durationConditionAmountController = TextEditingController();
-  var _isLoading = false;
-  // var _isInit = true;
-
-  NegotiationService negotiationService = NegotiationService();
-
-  GlobalKey<FormState> _formKey = GlobalKey<FormState>();
-
   @override
   void initState() {
-    // print('InitState()');
     super.initState();
   }
 
-  Future<void> _saveForm(String cId, ConditionType type) async {
-    final isValid = _formKey.currentState.validate();
+  void _reloadView() {
+    setState(() {});
+  }
 
-    Condition newCondition = Condition(
-      title: '',
-      proposedBy: Global.userAddress,
-      description: '',
-      agreementId: cId,
-    );
-
-    if (type == ConditionType.Normal) {
-      newCondition = Condition(
-        title: _conditionTitleController.text,
-        proposedBy: Global.userAddress,
-        description: _conditionDescriptionController.text,
-        agreementId: cId,
-      );
-    } else {}
-    if (!isValid) return;
-    _formKey.currentState.save();
-    //^^^^saves the form -> executes the 'onSaved' of each input
-    setState(() {
-      _isLoading = true;
-    });
-
+  Future<Contract> fetchAgreement(agreementId) async {
+    CommonService commonService = CommonService();
+    await Future.delayed(Duration(milliseconds: 500));
     try {
-      //Save to DB:
-      if (type == ConditionType.Payment) {
-        await negotiationService.setPayment(
-          cId,
-          Global.userAddress, //TODO: change this to Party Input
-          double.parse(_paymentConditionAmountController.text),
-        );
-      } else if (type == ConditionType.Duration) {
-        await negotiationService.setDuration(
-          cId,
-          (60 * 60 * double.parse(_durationConditionAmountController.text)),
-        ); //Updated to ask for hours, send through in seconds
-      } else {
-        await negotiationService.saveCondition(newCondition);
-      }
-      print('new condition saved');
+      Contract loaded = await commonService.getAgreement(agreementId);
+      return loaded;
     } catch (error) {
-      await showDialog(
-        context: context,
-        builder: (ctx) => AlertDialog(
-          title: Text('An error occurred!'),
-          content: Text('Something went wrong.'),
-          actions: <Widget>[
-            TextButton(
-              child: Text('Okay'),
-              onPressed: () {
-                Navigator.of(ctx).pop();
-              },
-            )
-          ],
-        ),
-      );
+      print(error);
+      throw (error);
     }
-    setState(() {
-      _isLoading = false;
-      Provider.of<Contracts>(context, listen: false).fetchAndSetContracts();
-    });
-    Navigator.of(context).pop();
-  }
-
-  Future<void> _newConditionDialog(String contId) async {
-    return await showDialog(
-        context: context,
-        builder: (context) {
-          return AlertDialog(
-            title: Text('Add New Condition'),
-            content: Form(
-              key: _formKey,
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  TextFormField(
-                    validator: (value) {
-                      if (value.isEmpty) {
-                        return 'Please enter a title.';
-                      } else {
-                        return null;
-                      }
-                    },
-                    decoration: InputDecoration(labelText: 'Title'),
-                    controller: _conditionTitleController,
-                  ),
-                  TextFormField(
-                    decoration: InputDecoration(labelText: 'Description'),
-                    maxLines: 3,
-                    keyboardType: TextInputType.multiline,
-                    controller: _conditionDescriptionController,
-                    validator: (value) {
-                      if (value.isEmpty) return 'Please enter a description.';
-                      if (value.length < 8)
-                        return 'Please enter at least 8 '
-                            'characters for the description.';
-                      return null;
-                    },
-                  ),
-                ],
-              ),
-            ),
-            actions: <Widget>[
-              TextButton(
-                child: Text('Discard'),
-                onPressed: () {
-                  _conditionTitleController.clear();
-                  _conditionDescriptionController.clear();
-                  Navigator.of(context).pop();
-                },
-              ),
-              TextButton(
-                child: Text('Add'),
-                onPressed: () {
-                  _saveForm(contId, ConditionType.Normal);
-                },
-              ),
-            ],
-          );
-        });
-  }
-
-  bool isNumeric(String s) {
-    if (s == null) {
-      return false;
-    }
-    return double.tryParse(s) != null;
-  }
-
-  Future<void> _newSpecialConditionDialog(
-      String contId, ConditionType type) async {
-    return await showDialog(
-        context: context,
-        builder: (context) {
-          return AlertDialog(
-            title: type == ConditionType.Payment
-                ? Text('Add New Payment '
-                    'Condition')
-                : Text('Add New Duration '
-                    'Condition'),
-            content: Form(
-              key: _formKey,
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  TextFormField(
-                    decoration: type == ConditionType.Payment
-                        ? InputDecoration(labelText: 'Enter UNT amount')
-                        : InputDecoration(
-                            labelText: 'Enter Duration (in hours)'
-                                ' from the seal time'),
-                    keyboardType: TextInputType.number,
-                    controller: type == ConditionType.Payment
-                        ? _paymentConditionAmountController
-                        : _durationConditionAmountController,
-                    validator: (value) {
-                      if (value.isEmpty) return 'Please enter an amount.';
-                      if (!isNumeric(value)) return 'Please enter a double.';
-                      return null;
-                    },
-                  ),
-                ],
-              ),
-            ),
-            actions: <Widget>[
-              TextButton(
-                child: Text('Discard'),
-                onPressed: () {
-                  _paymentConditionAmountController.clear();
-                  _durationConditionAmountController.clear();
-                  Navigator.of(context).pop();
-                },
-              ),
-              TextButton(
-                child: Text('Add'),
-                onPressed: () {
-                  type == ConditionType.Payment
-                      ? _saveForm(contId, ConditionType.Payment)
-                      : _saveForm(contId, ConditionType.Duration);
-                },
-              ),
-            ],
-          );
-        });
   }
 
   Widget build(BuildContext context) {
-    final contractId = ModalRoute.of(context).settings.arguments as String;
-    final loadedContract =
-        Provider.of<Contracts>(context, listen: true).findById(contractId);
-
+    final agreementId = ModalRoute.of(context).settings.arguments as String;
     return Scaffold(
-      appBar: AppBar(
-        title: Text(loadedContract.title),
-      ),
+      appBar: AppBar(title: FunkyText('View Agreement')),
       body: Padding(
-        padding: const EdgeInsets.all(15.0),
-        child: Column(
-          children: [
-            ContractDetailInfoPanel(loadedContract),
-            Container(
-              padding: EdgeInsets.symmetric(
-                vertical: 10,
-                horizontal: 8,
-              ),
-              // alignment: Alignment.centerLeft,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text(
-                    'Agreement Conditions:',
-                    style: TextStyle(
-                      fontSize: 16,
-                    ),
-                  ),
-                  if (!loadedContract.movedToBlockchain)
-                    TextButton(
-                      onPressed: () async {
-                        await _newConditionDialog(contractId);
-                      },
-                      child: Row(
-                        children: [
-                          Icon(Icons.add),
-                          Text('Add New Condition'),
-                        ],
+          padding: const EdgeInsets.all(15.0),
+          child: FutureBuilder(
+            future: fetchAgreement(agreementId),
+            builder: (context, agreementSnap) {
+              if (agreementSnap.connectionState == ConnectionState.done) {
+                return Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    ContractDetailInfoPanel(agreementSnap.data),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text(
+                        'Agreement Conditions:',
+                        style: TextStyle(
+                          fontSize: 16,
+                        ),
                       ),
                     ),
-                  if (!loadedContract.movedToBlockchain)
-                    TextButton(
-                      onPressed: () async {
-                        await _newSpecialConditionDialog(
-                            contractId, ConditionType.Payment);
-                      },
-                      child: Row(
-                        children: [
-                          Icon(Icons.add),
-                          Text('Add New Payment Condition'),
-                        ],
-                      ),
-                    ),
-                  if (!loadedContract.movedToBlockchain)
-                    TextButton(
-                      onPressed: () async {
-                        await _newSpecialConditionDialog(
-                            contractId, ConditionType.Duration);
-                      },
-                      child: Row(
-                        children: [
-                          Icon(Icons.add),
-                          Text('Add New Duration Condition'),
-                        ],
-                      ),
-                    ),
-                ],
-              ),
-            ),
-            Expanded(
-              child: _isLoading
-                  ? Center(
-                      child: CircularProgressIndicator(),
+                    if (!agreementSnap.data.movedToBlockchain)
+                      ContractConditionActionsPanel(agreementId, _reloadView),
+                    ContractConditionsPanel(agreementSnap.data, _reloadView),
+                    SizedBox(
+                      height: MediaQuery.of(context).size.height * 0.1,
                     )
-                  : ContractConditionsPanel(loadedContract),
-              flex: 6,
-            ),
-            Expanded(
-              flex: 1,
-              child: Container(
-                  // TODO: More Information or actions here?
-                  ),
-            )
-          ],
-        ),
-      ),
+                  ],
+                );
+              }
+              return Center(
+                child: AwesomeLoader(
+                  loaderType: AwesomeLoader.AwesomeLoader4,
+                ),
+              );
+            },
+          )),
       floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
       floatingActionButton: FloatingActionButton.extended(
         onPressed: () {
           Navigator.of(context).pushNamed(
             MessagingScreen.routeName,
-            arguments: loadedContract.contractId,
+            arguments: agreementId,
           );
         },
         label: Text('Agreement Chat'),
