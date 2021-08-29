51853a46c2ceea351662d37b87262750aa4f8785
JasonMaritz <u19053292@tuks.co.za>
2021-08-29 23:30:11 +0200

    Upcased Json names in request objects

--- Smart-Contract-Verifier-Server/src/main/kotlin/com/savannasolutions/SmartContractVerifierServer/evidence/requests/LinkEvidenceRequest.kt
+++ Smart-Contract-Verifier-Server/src/main/kotlin/com/savannasolutions/SmartContractVerifierServer/evidence/requests/LinkEvidenceRequest.kt
@@ -3,4 +3,4 @@ package com.savannasolutions.SmartContractVerifierServer.evidence.requests
 import com.fasterxml.jackson.annotation.JsonProperty
 import java.util.*
 
-data class LinkEvidenceRequest(@JsonProperty("evidenceUrl") val url: String,)
+data class LinkEvidenceRequest(@JsonProperty("EvidenceUrl") val url: String,)
--- Smart-Contract-Verifier-Server/src/main/kotlin/com/savannasolutions/SmartContractVerifierServer/evidence/requests/UploadEvidenceRequest.kt
+++ Smart-Contract-Verifier-Server/src/main/kotlin/com/savannasolutions/SmartContractVerifierServer/evidence/requests/UploadEvidenceRequest.kt
@@ -4,4 +4,4 @@ import com.fasterxml.jackson.annotation.JsonProperty
 import org.springframework.web.multipart.MultipartFile
 import java.util.*
 
-data class UploadEvidenceRequest(@JsonProperty("uploadFile") val fileToUpload: MultipartFile,)
+data class UploadEvidenceRequest(@JsonProperty("UploadFile") val fileToUpload: MultipartFile,)
