8ddaa62f853959270b6b0bd92667fa330edcbc11
Kevin <kevindupreez8@gmail.com>
2021-08-30 09:39:21 +0200

    General cleanup of code for edit_contract_screen.dart

--- frontend_scv/lib/screens/edit_contract_screen.dart
+++ frontend_scv/lib/screens/edit_contract_screen.dart
@@ -7,8 +7,6 @@ import '../models/contracts.dart';
 
 class EditContractScreen extends StatefulWidget {
   static const routeName = '/edit-contract';
-
-  //USE STATEFUL for Form input
   @override
   _EditContractScreenState createState() => _EditContractScreenState();
 }
@@ -18,12 +16,7 @@ class _EditContractScreenState extends State<EditContractScreen> {
   final _priceFocusNode = FocusNode();
   final _partyBIdFocusNode = FocusNode();
   final _descriptionFocusNode = FocusNode();
-  // final _imageUrlFocusNode = FocusNode();
-  // final _imageUrlController = TextEditingController();
-  //Global Key - used to interact with a widget within your code - very rare
-  //Normally used for forms:
-  final _form = GlobalKey<FormState>(); //essentially 'hooks' into forms state
-  //use this Product and update it when saveForm is called
+  final _form = GlobalKey<FormState>();
   var _editedContract = Contract(
     contractId: null,
     title: '',
@@ -49,7 +42,6 @@ class _EditContractScreenState extends State<EditContractScreen> {
   };
   @override
   void initState() {
-    // _imageUrlFocusNode.addListener(_updateImageUrl);
     super.initState();
   }
 
@@ -66,40 +58,21 @@ class _EditContractScreenState extends State<EditContractScreen> {
           'price': _editedContract.price.toString(),
           'partyBId': _editedContract.partyB,
         };
-        // _imageUrlController.text = _editedContract.imageUrl;
       }
-    } //
+    }
     _isInit = false;
     super.didChangeDependencies();
   }
 
-  // void _updateImageUrl() {
-  //   if (!_imageUrlFocusNode.hasFocus) {
-  //     if ((!_imageUrlController.text.startsWith('http') &&
-  //             !_imageUrlController.text.startsWith('https')) ||
-  //         (!_imageUrlController.text.endsWith('.png') &&
-  //             !_imageUrlController.text.endsWith('.jpg') &&
-  //             !_imageUrlController.text.endsWith('.jpeg'))) {
-  //       return;
-  //     }
-  //     setState(() {});
-  //   }
-  // }
-
   Future<void> _saveForm() async {
-    //validates inputs on the form -> executes the 'validator' of each input:
-    //return True if no errors i.e. no strings, or False if errors
     final isValid = _form.currentState.validate();
     if (!isValid) return;
     _form.currentState.save();
-    //^^^^saves the form -> executes the 'onSaved' of each input
     setState(() {
       _isLoading = true;
     });
 
     if (_editedContract.contractId != null) {
-      //editing
-      //can wrap in try catch here
       await Provider.of<Contracts>(context, listen: false)
           .updateContract(_editedContract.contractId, _editedContract);
     } else {
@@ -132,12 +105,9 @@ class _EditContractScreenState extends State<EditContractScreen> {
 
   @override
   void dispose() {
-    // _imageUrlFocusNode.removeListener(_updateImageUrl);
     _priceFocusNode.dispose();
     _partyBIdFocusNode.dispose();
     _descriptionFocusNode.dispose();
-    // _imageUrlController.dispose();
-    // _imageUrlFocusNode.dispose();
     super.dispose();
   }
 
@@ -185,10 +155,6 @@ class _EditContractScreenState extends State<EditContractScreen> {
                         );
                       },
                       validator: (value) {
-                        //Used to validate the value in the edit field
-                        //returning null -> input is correct
-                        //returning text -> 'This  is wrong' (something is wrong)
-                        // -> message for user
                         if (value.isEmpty) return 'Please provide a value.';
                         return null;
                       },
@@ -197,25 +163,17 @@ class _EditContractScreenState extends State<EditContractScreen> {
                       initialValue: _initValues['partyBId'],
                       decoration: InputDecoration(
                         labelText: 'Party B Address',
-                        // errorText:
                       ),
                       focusNode: _partyBIdFocusNode,
                       textInputAction: TextInputAction
                           .next, //bottom right button in soft keyboard
                       onFieldSubmitted: (_) {
-                        //not neccessary
                         FocusScope.of(context).requestFocus(_priceFocusNode);
                       },
                       onSaved: (value) {
                         _editedContract = Contract(
-                            //because the values of a Product
-                            // are final - we need to create a new Product (we cannot
-                            // simply edit them) and then replace the existing one
                             contractId: _editedContract.contractId,
-                            title:
-                                _editedContract.title, //This is the value that
-                            // needs to be
-                            // updated
+                            title: _editedContract.title,
                             description: _editedContract.description,
                             price: _editedContract.price,
                             imageUrl: _editedContract.imageUrl,
@@ -224,10 +182,6 @@ class _EditContractScreenState extends State<EditContractScreen> {
                             conditions: []);
                       },
                       validator: (value) {
-                        //Used to validate the value in the edit field
-                        //returning null -> input is correct
-                        //returning text -> 'This  is wrong' (something is wrong)
-                        // -> message for user
                         if (value.isEmpty) return 'Please provide a value.';
                         return null;
                       },
@@ -257,64 +211,6 @@ class _EditContractScreenState extends State<EditContractScreen> {
                         return null;
                       },
                     ),
-                    // Row(
-                    //   crossAxisAlignment: CrossAxisAlignment.end,
-                    //   children: [
-                    //     Container(
-                    //       width: 100,
-                    //       height: 100,
-                    //       margin: EdgeInsets.only(top: 8, right: 10),
-                    //       decoration: BoxDecoration(
-                    //           border: Border.all(
-                    //         color: Colors.grey,
-                    //       )),
-                    //       child: _imageUrlController.text.isEmpty
-                    //           ? Center(child: Text('Enter a URL'))
-                    //           : FittedBox(
-                    //               fit: BoxFit.cover,
-                    //               child:
-                    //                   Image.network(_imageUrlController.text),
-                    //             ),
-                    //     ),
-                    //     Expanded(
-                    //       child: TextFormField(
-                    //         decoration: InputDecoration(labelText: 'Image URL'),
-                    //         keyboardType: TextInputType.url,
-                    //         textInputAction: TextInputAction.done,
-                    //         controller: _imageUrlController,
-                    //         focusNode: _imageUrlFocusNode,
-                    //         onFieldSubmitted: (_) {
-                    //           _saveForm();
-                    //         },
-                    //         onSaved: (value) {
-                    //           _editedContract = Contract(
-                    //               contractId: _editedContract.contractId,
-                    //               title: _editedContract.title,
-                    //               description: _editedContract.description,
-                    //               price: _editedContract.price,
-                    //               imageUrl: value,
-                    //               partyB: _editedContract.partyBId,
-                    //               conditions: []);
-                    //         },
-                    //         validator: (value) {
-                    //           // if (value.isEmpty) {
-                    //           //   return 'Please enter an image URL.';
-                    //           // }
-                    //           // if (!value.startsWith('http') &&
-                    //           //     !value.startsWith('https')) {
-                    //           //   return 'Please enter a valid URL.';
-                    //           // }
-                    //           // if (!value.endsWith('.png') &&
-                    //           //     !value.endsWith('.jpg') &&
-                    //           //     !value.endsWith('.jpeg')) {
-                    //           //   return 'Please enter a valid image URL.';
-                    //           // }
-                    //           return null;
-                    //         },
-                    //       ),
-                    //     )
-                    //   ],
-                    // ),
                   ],
                 ),
               ),
