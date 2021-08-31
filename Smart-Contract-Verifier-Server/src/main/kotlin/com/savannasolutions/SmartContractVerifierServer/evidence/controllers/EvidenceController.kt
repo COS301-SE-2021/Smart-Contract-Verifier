b5b2e02221d94569d6d5c8f60ee61b27a79e8c5c
JasonMaritz <u19053292@tuks.co.za>
2021-08-31 23:47:41 +0200

    Added response logic to download files

--- Smart-Contract-Verifier-Server/src/main/kotlin/com/savannasolutions/SmartContractVerifierServer/evidence/controllers/EvidenceController.kt
+++ Smart-Contract-Verifier-Server/src/main/kotlin/com/savannasolutions/SmartContractVerifierServer/evidence/controllers/EvidenceController.kt
@@ -1,17 +1,21 @@
 package com.savannasolutions.SmartContractVerifierServer.evidence.controllers
 
-import com.google.common.net.MediaType
 import com.savannasolutions.SmartContractVerifierServer.evidence.requests.*
 import com.savannasolutions.SmartContractVerifierServer.evidence.services.EvidenceService
-import org.springframework.http.MediaType.MULTIPART_FORM_DATA_VALUE
+import org.springframework.http.MediaType
 import org.springframework.web.bind.annotation.*
 import org.springframework.web.multipart.MultipartFile
+import java.io.BufferedOutputStream
+import java.io.FileInputStream
+import java.lang.RuntimeException
 import java.util.*
+import javax.servlet.http.HttpServletResponse
 
 @RestController
 class EvidenceController constructor(private val evidenceService: EvidenceService,) {
 
-    @PostMapping("/user/{userId}/agreement/{agreementId}/evidence/upload", consumes = [org.springframework.http.MediaType.MULTIPART_FORM_DATA_VALUE])
+    @PostMapping("/user/{userId}/agreement/{agreementId}/evidence/upload",
+        consumes = [org.springframework.http.MediaType.MULTIPART_FORM_DATA_VALUE])
     fun uploadEvidence(@PathVariable userId: String,
                        @PathVariable agreementId: UUID,
                        @RequestParam uploadEvidence: MultipartFile,) =
@@ -29,9 +33,38 @@ class EvidenceController constructor(private val evidenceService: EvidenceServic
                          @PathVariable evidenceHash: String,) =
         evidenceService.fetchEvidence(userId, agreementId, evidenceHash)
 
-    @GetMapping("/user/{userId}/agreement/{agreementId}/evidence/{evidenceHash}/download")
-    fun downloadEvidence(@PathVariable userId: String, @PathVariable agreementId: UUID, @PathVariable evidenceHash: String,){
-
+    @GetMapping("/user/{userId}/agreement/{agreementId}/evidence/{evidenceHash}/download",
+        produces = [MediaType.APPLICATION_OCTET_STREAM_VALUE])
+    @ResponseBody
+    fun  downloadEvidence(response: HttpServletResponse,
+                         @PathVariable userId: String,
+                         @PathVariable agreementId: UUID,
+                         @PathVariable evidenceHash: String,){
+        val fileResp = evidenceService.downloadEvidence(userId, agreementId, evidenceHash)
+        if(fileResp.file != null) {
+            response.contentType = fileResp.mimeType
+            response.setHeader("Content-Disposition", "attachment; filename=" + fileResp.file?.name)
+            response.setHeader("Content-Transfer-Encoding", "binary")
+            try {
+                val outputStream = BufferedOutputStream(response.outputStream)
+                val inputStream = FileInputStream(fileResp.file)
+                var len: Int
+                val buffer: ByteArray = ByteArray(1024)
+                var reading = true
+                while (reading){
+                    len=inputStream.read(buffer)
+                    if(len > 0) {
+                        outputStream.write(buffer, 0, len)
+                    }else{
+                        reading = false
+                    }
+                }
+                outputStream.close()
+                response.flushBuffer()
+            } catch (e: Exception) {
+                throw RuntimeException(e)
+            }
+        }
     }
 
     @GetMapping("/user/{userId}/agreement/{agreementId}/evidence/")
