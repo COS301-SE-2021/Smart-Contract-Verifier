92e19a6f17c15da6ebd026a5aababde01081fce8
Kevin <kevindupreez8@gmail.com>
2021-09-02 00:20:20 +0200

    Extracted message input panel with logic

--- frontend_scv/lib/screens/messaging_screen.dart
+++ frontend_scv/lib/screens/messaging_screen.dart
@@ -3,6 +3,7 @@ import 'package:unison/models/global.dart';
 import 'package:unison/models/message.dart';
 import 'package:unison/services/Server/messageService.dart';
 import 'package:unison/widgets/funky_text_widget.dart';
+import 'package:unison/widgets/message_input_panel.dart';
 import 'package:unison/widgets/messages_panel.dart';
 
 class MessagingScreen extends StatefulWidget {
@@ -12,53 +13,11 @@ class MessagingScreen extends StatefulWidget {
 }
 
 class _MessagingScreenState extends State<MessagingScreen> {
-  GlobalKey<FormState> _formKey = GlobalKey<FormState>();
-  final _messageTextController = TextEditingController();
   void initState() {
     super.initState();
   }
 
   MessageService messageService = MessageService();
-
-  Future<void> _saveForm(String agreementId) async {
-    final isValid = _formKey.currentState.validate();
-
-    Message newMessage = Message(
-      _messageTextController.text,
-      agreementId,
-    );
-
-    if (!isValid) return;
-    _formKey.currentState.save();
-    //^^^^saves the form -> executes the 'onSaved' of each input
-    // setState(() {});
-
-    try {
-      //Save to DB:
-      await messageService.sendMessage(newMessage);
-      print('new message sent');
-    } catch (error) {
-      await showDialog(
-        context: context,
-        builder: (ctx) => AlertDialog(
-          title: Text('An error occurred!'),
-          content: Text('Something went wrong.'),
-          actions: <Widget>[
-            TextButton(
-              child: Text('Okay'),
-              onPressed: () {
-                Navigator.of(ctx).pop();
-              },
-            )
-          ],
-        ),
-      );
-    }
-    // setState(() {
-    //   super.setState(() {});
-    // });
-  }
-
   @override
   Widget build(BuildContext context) {
     final args = ModalRoute.of(context).settings.arguments as Map;
@@ -74,10 +33,6 @@ class _MessagingScreenState extends State<MessagingScreen> {
       body: Column(
         children: [
           Card(
-            // : EdgeInsets.symmetric(
-            //   vertical: MediaQuery.of(context).size.height * 0.1,
-            //   horizontal: MediaQuery.of(context).size.width * 0.1,
-            // ),
             color: Color.fromRGBO(43, 45, 60, 0.8),
             child: Row(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
@@ -151,59 +106,7 @@ class _MessagingScreenState extends State<MessagingScreen> {
           ),
           Expanded(
             flex: 1,
-            child: Container(
-              padding: EdgeInsets.symmetric(horizontal: 20),
-              // color: Colors.white,
-              height: 100,
-              child: Row(
-                children: [
-                  Expanded(
-                    child: Container(
-                      padding: EdgeInsets.symmetric(horizontal: 14),
-                      height: 60,
-                      decoration: BoxDecoration(
-                        border: Border.all(
-                          color: Colors.deepOrange,
-                        ),
-                        borderRadius: BorderRadius.circular(10.0),
-                      ),
-                      child: Row(
-                        children: [
-                          SizedBox(
-                            width: 10,
-                          ),
-                          Expanded(
-                            child: Form(
-                              key: _formKey,
-                              child: TextFormField(
-                                maxLines: 1,
-                                keyboardType: TextInputType.text,
-                                controller: _messageTextController,
-                                validator: (value) {
-                                  if (value.isEmpty)
-                                    return 'Please enter a message.';
-                                  return null;
-                                },
-                                onFieldSubmitted: (value) {
-                                  _saveForm(agreementId);
-                                },
-                              ),
-                            ),
-                          ),
-                          IconButton(
-                            onPressed: () => _saveForm(agreementId),
-                            icon: Icon(Icons.send_outlined),
-                          )
-                        ],
-                      ),
-                    ),
-                  ),
-                  SizedBox(
-                    width: 10,
-                  ),
-                ],
-              ),
-            ),
+            child: MessageInputPanel(agreementId),
           ),
         ],
       ),
