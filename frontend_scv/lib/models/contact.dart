12ba0320d5bc4ab2556a2475a20697a29c0be977
Ronan <u19288736@tuks.co.za>
2021-09-02 13:08:46 +0200

    Updated misspelling of Contact, and added contactList.dart.

--- frontend_scv/lib/models/contact.dart
+++ frontend_scv/lib/models/contact.dart
@@ -1,9 +1,15 @@
 //This class models a contact.
 //A contact has a wallet address and a name.
+//Be aware of confusion with Contract in terms of naming.
 
-class Contract {
+class Contact {
 
   String address;
-  String name;
+  String alias;
+
+  Contact.fromJSON(Map<String, dynamic> jsn) {
+
+    //address =
+  }
 
 }
---
+++ frontend_scv/lib/models/contactList.dart
@@ -0,0 +1,7 @@
+//The user can separate contacts into dedicated lists,
+//this class thus holds a list of contacts.
+
+class ContactList {
+
+
+}
