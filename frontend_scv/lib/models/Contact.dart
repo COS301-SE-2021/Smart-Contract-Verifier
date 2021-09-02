d6c4021f086e42757ba69857353c67cacec86695
Ronan <u19288736@tuks.co.za>
2021-09-02 12:47:43 +0200

    Added Contact.dart and contactService.dart.

---
+++ frontend_scv/lib/models/Contact.dart
@@ -0,0 +1,9 @@
+//This class models a contact.
+//A contact has a wallet address and a name.
+
+class Contract {
+
+  String address;
+  String name;
+
+}
---
+++ frontend_scv/lib/services/Server/contactService.dart
@@ -0,0 +1,9 @@
+//This class is used to facilitate the operation of the contact list.
+
+import 'package:unison/services/Server/backendAPI.dart';
+
+class ContactService {
+
+  ApiInteraction _api = ApiInteraction();
+
+}
