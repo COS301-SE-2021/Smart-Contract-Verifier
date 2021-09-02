df1b12e5a55df12a8b24f0850359a9f3d138b88a
Kevin <kevindupreez8@gmail.com>
2021-09-02 22:42:58 +0200

    All Agreement States are accounted for and render the correct actions

--- frontend_scv/lib/widgets/contract_action_area.dart
+++ frontend_scv/lib/widgets/contract_action_area.dart
@@ -2,9 +2,9 @@ import 'package:awesome_loader/awesome_loader.dart';
 import 'package:flutter/material.dart';
 import 'package:unison/models/blockchainAgreement.dart';
 import 'package:unison/models/contract.dart';
-import 'package:unison/models/global.dart';
 import 'package:unison/services/Blockchain/unisonService.dart';
 import 'package:unison/services/Server/commonService.dart';
+import 'package:unison/services/Server/judgeService.dart';
 import 'package:unison/services/Server/negotiationService.dart';
 import 'package:unison/widgets/waiting_for_blockchain_text.dart';
 
@@ -30,6 +30,7 @@ class ContractActionArea extends StatefulWidget {
 class _ContractActionAreaState extends State<ContractActionArea> {
   bool _isLoading = false;
   bool _disabled = false;
+  JudgeService _judgeService;
 
   showLoaderDialog(BuildContext context) {
     AlertDialog alert = AlertDialog(
@@ -159,6 +160,122 @@ class _ContractActionAreaState extends State<ContractActionArea> {
             );
           }
           return Text('Awaiting acceptance from other party');
+        }
+        //CurrentState >= ACCEPTED:
+        if (currentState == AgreementState.ACCEPTED) {
+          //Pay Platform Fee?
+          return TextButton(
+            onPressed: () async {
+              showLoaderDialog(context);
+              try {
+                print('Pay Platform Fee');
+                await widget._unisonService.payPlatformFee(
+                  widget._agreement.blockchainId,
+                );
+                Navigator.of(context).pop();
+                setState(() {});
+              } catch (error) {
+                //TODO: make dialog throw handler in parent
+                Navigator.of(context).pop();
+                setState(() {});
+              }
+            },
+            child: Text('Pay Platform Fee'),
+          );
+        }
+        if (currentState == AgreementState.ACTIVE) {
+          //Check Duration of BCAgreement:
+          var expiryDate = DateTime.fromMillisecondsSinceEpoch(
+              _loadedBCAgreement.resTime.toInt() * 1000);
+          if (expiryDate.compareTo(DateTime.now()) < 0) {
+            // if(party has already voted): TODO
+            // return Text('Show Resolution options');
+            return Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                TextButton(
+                  onPressed: () async {
+                    showLoaderDialog(context);
+                    try {
+                      print('Conclude Agreement');
+                      await widget._unisonService.agreementFulfilled(
+                        widget._agreement,
+                        true,
+                      );
+                      Navigator.of(context).pop();
+                      setState(() {});
+                    } catch (error) {
+                      //TODO: make dialog throw handler in parent
+                      Navigator.of(context).pop();
+                      setState(() {});
+                    }
+                  },
+                  child: Text('Conclude Agreement'),
+                ),
+                SizedBox(
+                  width: 20,
+                ),
+                TextButton(
+                  onPressed: () async {
+                    showLoaderDialog(context);
+                    try {
+                      print('Dispute Agreement');
+                      await widget._unisonService.agreementFulfilled(
+                        widget._agreement,
+                        false,
+                      );
+                      Navigator.of(context).pop();
+                      setState(() {});
+                    } catch (error) {
+                      //TODO: make dialog throw handler in parent
+                      Navigator.of(context).pop();
+                      setState(() {});
+                    }
+                  },
+                  child: Text('Dispute Agreement'),
+                ),
+              ],
+            );
+          } else {
+            var difference = expiryDate.difference(DateTime.now());
+            Duration duration = Duration(seconds: difference.inSeconds);
+            String sDuration =
+                "Hours: ${duration.inHours} Minutes: ${duration.inMinutes.remainder(60)}"; //:${(duration.inSeconds.remainder(60))}
+            // for later
+            return Text('Concludes in ' + sDuration);
+          }
+          //if expired/past due
+          //show Resolution Options
+          //voted or not
+
+        }
+
+        if (currentState == AgreementState.CLOSED) {
+          return Text('Agreement Concluded');
+        }
+        print(currentState.toString());
+
+        if (currentState == AgreementState.CONTESTED) {
+          _judgeService = JudgeService();
+          return FutureBuilder(
+            future: _judgeService.getJury(widget._agreement.blockchainId),
+            builder: (context, jurySnapshot) {
+              if (jurySnapshot.connectionState == ConnectionState.done) {
+                var juryConclusion = DateTime.fromMillisecondsSinceEpoch(
+                    jurySnapshot.data.resTime.toInt() * 1000);
+                var timeDifference = juryConclusion.difference(DateTime.now());
+                Duration duration = Duration(seconds: timeDifference.inSeconds);
+                String sJuryConclusion =
+                    "${duration.inHours} Hours and ${duration.inMinutes.remainder(60)} Minutes";
+                return Text('Jury voting concludes in: ' + sJuryConclusion);
+              }
+              return AwesomeLoader(
+                loaderType: AwesomeLoader.AwesomeLoader4,
+                color: Colors.teal,
+              );
+            },
+          );
         }
 
         return Text(currentState.toString());
