51479a7fad2c4032e674e04d815565177201c496
Ronan <u19288736@tuks.co.za>
2021-09-03 18:23:20 +0200

    Fixed issues due to strange merge.

--- frontend_scv/lib/services/Server/backendAPI.dart
+++ frontend_scv/lib/services/Server/backendAPI.dart
@@ -18,7 +18,7 @@ class ApiInteraction {
   final String baseUrl =
       "http://localhost:8080"; //Url where the backend is deployed
   static final ApiInteraction api =
-      ApiInteraction._internal(); //Only instance of the class
+  ApiInteraction._internal(); //Only instance of the class
 
   ///Factory constructor to return instance;
   factory ApiInteraction() {
@@ -44,7 +44,53 @@ class ApiInteraction {
     return _baseRequest(url, ReqType.PUT, jsn);
   }
 
-
-    }
+  ///Delete data from the server
+  Future<ApiResponse> deleteData(String url, [Map<dynamic, dynamic> jsn]) async {
+    return _baseRequest(url, ReqType.DELETE, jsn);
   }
 
+  //An attempt at re-organising the services.
+  Future<ApiResponse> _baseRequest(String url, ReqType method, [Map<dynamic, dynamic> jsn]) async {
+    jsn ??= {}; //If null, make empty
+
+    Function toCall = get;
+    //Cool function pointers
+    switch (method) {
+      case ReqType.GET : {toCall = get;
+      break;}
+      case ReqType.POST : {toCall = post;
+      break;}
+      case ReqType.PUT : {toCall = put;
+      break;}
+      case ReqType.DELETE : {toCall = delete;
+      break; }
+    }
+
+    print ('Here 1');
+    var headers = {
+      'Content-Type': 'application/json; charset=UTF-8',
+    };
+    var response;
+    try {
+      response = await RetryOptions(maxAttempts: 5).retry(
+            () => (method == ReqType.GET) ? toCall(Uri.parse(baseUrl + url),
+            headers: headers) :toCall(Uri.parse(baseUrl + url),
+          headers: headers, body: jsonEncode(jsn),)
+            .timeout(Duration(seconds: 2)),
+        retryIf: (e) => e is SocketException || e is TimeoutException,
+      );
+
+    } on Exception catch (e) {
+      return ApiResponse.fromError(
+          'Could not connect to backend'); //Could be expanded in the future
+    }
+
+    if (response.statusCode != 200)
+      return ApiResponse.fromError(
+          'An error occurred while making the request. The server responded with status code ' +
+              response.statusCode.toString()); //Failed http request
+
+    print ('Ended');
+    return ApiResponse.fromJSON(jsonDecode(response.body));
+  }
+}
