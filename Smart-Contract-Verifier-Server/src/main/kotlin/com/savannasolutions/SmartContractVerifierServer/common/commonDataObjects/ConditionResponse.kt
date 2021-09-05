908e005d19236ad89217819af0dc8a6978824543
Cobus Redelinghuys <u19089296@tuks.co.za>
2021-09-05 21:28:52 +0200

    Added autogenerated documentation companion object

--- Smart-Contract-Verifier-Server/src/main/kotlin/com/savannasolutions/SmartContractVerifierServer/common/commonDataObjects/ConditionResponse.kt
+++ Smart-Contract-Verifier-Server/src/main/kotlin/com/savannasolutions/SmartContractVerifierServer/common/commonDataObjects/ConditionResponse.kt
@@ -2,7 +2,11 @@ package com.savannasolutions.SmartContractVerifierServer.common.commonDataObject
 
 import com.fasterxml.jackson.annotation.JsonProperty
 import com.savannasolutions.SmartContractVerifierServer.negotiation.models.ConditionStatus
+import org.springframework.restdocs.payload.FieldDescriptor
+import org.springframework.restdocs.payload.PayloadDocumentation
+import java.io.FileDescriptor
 import java.util.*
+import kotlin.collections.ArrayList
 
 data class ConditionResponse(@JsonProperty("ConditionID") val conditionID: UUID,
                              @JsonProperty("ConditionDescription") val conditionDescription: String? = null,
@@ -11,3 +15,20 @@ data class ConditionResponse(@JsonProperty("ConditionID") val conditionID: UUID,
                              @JsonProperty("AgreementID") val agreementID : UUID? = null,
                              @JsonProperty("ConditionStatus") val conditionStatus: Enum<ConditionStatus>? = null,
                              @JsonProperty("ConditionTitle") val conditionTitle: String? = null,)
+{
+    companion object{
+        fun response(Path: String): ArrayList<FieldDescriptor>
+        {
+            val fieldDescriptorResponse = ArrayList<FieldDescriptor>()
+            fieldDescriptorResponse.add(PayloadDocumentation.fieldWithPath("$Path.ConditionID").description("This is the unique ID for the condition").type("UUID"))
+            fieldDescriptorResponse.add(PayloadDocumentation.fieldWithPath("$Path.ConditionDescription").description("This is the description for the condition").type("String"))
+            fieldDescriptorResponse.add(PayloadDocumentation.subsectionWithPath("$Path.ProposingUser").description("This is the public wallet address for the user who proposed the condition"))
+            fieldDescriptorResponse.addAll(UserResponse.response("$Path.ProposingUser"))
+            fieldDescriptorResponse.add(PayloadDocumentation.fieldWithPath("$Path.AgreementID").description("This is the unique id for the agreement this condition is attached to").type("UUID"))
+            fieldDescriptorResponse.add(PayloadDocumentation.fieldWithPath("$Path.ConditionStatus").description("This is the status of the condition. It can be PENDING, ACCEPTED or REJECTED").type("ENUM"))
+            fieldDescriptorResponse.add(PayloadDocumentation.fieldWithPath("$Path.ConditionTitle").description("This is the title for the condition").type("String"))
+            return fieldDescriptorResponse
+
+        }
+    }
+}
