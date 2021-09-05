6de29453dc80b5dd9c3673dc2519670126976bf3
Cobus Redelinghuys <u19089296@tuks.co.za>
2021-09-05 18:01:40 +0200

    Added autogenerated documentation companion object

--- Smart-Contract-Verifier-Server/src/main/kotlin/com/savannasolutions/SmartContractVerifierServer/negotiation/requests/CreateConditionRequest.kt
+++ Smart-Contract-Verifier-Server/src/main/kotlin/com/savannasolutions/SmartContractVerifierServer/negotiation/requests/CreateConditionRequest.kt
@@ -1,7 +1,20 @@
 package com.savannasolutions.SmartContractVerifierServer.negotiation.requests
 
 import com.fasterxml.jackson.annotation.JsonProperty
+import org.springframework.restdocs.payload.FieldDescriptor
+import org.springframework.restdocs.payload.PayloadDocumentation
 import java.util.*
+import kotlin.collections.ArrayList
 
 data class CreateConditionRequest(@JsonProperty("ConditionTitle") val Title : String,
                                   @JsonProperty("ConditionDescription") val ConditionDescription: String,)
+{
+    companion object{
+        fun request(): ArrayList<FieldDescriptor>{
+            val fieldDescriptorRequests = ArrayList<FieldDescriptor>()
+            fieldDescriptorRequests.add(PayloadDocumentation.fieldWithPath("ConditionTitle").description("This is the title of the condition").type("String"))
+            fieldDescriptorRequests.add(PayloadDocumentation.fieldWithPath("ConditionDescription").description("This is the description of the condition").type("String"))
+            return fieldDescriptorRequests
+        }
+    }
+}
