423c979e09992fb543693401d1f761a69d83516d
Cobus Redelinghuys <u19089296@tuks.co.za>
2021-09-05 22:04:46 +0200

    Added autogenerated documentation companion object

--- Smart-Contract-Verifier-Server/src/main/kotlin/com/savannasolutions/SmartContractVerifierServer/negotiation/responses/GetAllConditionsResponse.kt
+++ Smart-Contract-Verifier-Server/src/main/kotlin/com/savannasolutions/SmartContractVerifierServer/negotiation/responses/GetAllConditionsResponse.kt
@@ -3,5 +3,23 @@ package com.savannasolutions.SmartContractVerifierServer.negotiation.responses
 import com.fasterxml.jackson.annotation.JsonProperty
 import com.savannasolutions.SmartContractVerifierServer.common.commonDataObjects.ConditionResponse
 import com.savannasolutions.SmartContractVerifierServer.common.commonDataObjects.ResponseStatus
+import org.springframework.restdocs.payload.FieldDescriptor
+import org.springframework.restdocs.payload.PayloadDocumentation
 
 data class GetAllConditionsResponse(@JsonProperty("Conditions") val conditions: List<ConditionResponse>? = null,)
+{
+    companion object{
+        fun response(): ArrayList<FieldDescriptor>{
+            val fieldDescriptorResponse = ArrayList<FieldDescriptor>()
+            fieldDescriptorResponse.add(PayloadDocumentation.subsectionWithPath("ResponseObject.Conditions[]").description("This is the list of conditions"))
+            fieldDescriptorResponse.addAll(ConditionResponse.response("ResponseObject.Conditions[]"))
+            return fieldDescriptorResponse
+        }
+
+        fun responseEmpty(): ArrayList<FieldDescriptor>{
+            val fieldDescriptorResponse = ArrayList<FieldDescriptor>()
+            fieldDescriptorResponse.add(PayloadDocumentation.subsectionWithPath("ResponseObject.Conditions[]").description("This is the list of conditions"))
+            return fieldDescriptorResponse
+        }
+    }
+}
