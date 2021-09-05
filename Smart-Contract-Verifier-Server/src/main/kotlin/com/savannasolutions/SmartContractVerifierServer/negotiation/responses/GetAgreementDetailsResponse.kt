294eb07978bd8aa291dc8b16102007c886e371ae
Cobus Redelinghuys <u19089296@tuks.co.za>
2021-09-05 21:29:01 +0200

    Added autogenerated documentation companion object

--- Smart-Contract-Verifier-Server/src/main/kotlin/com/savannasolutions/SmartContractVerifierServer/negotiation/responses/GetAgreementDetailsResponse.kt
+++ Smart-Contract-Verifier-Server/src/main/kotlin/com/savannasolutions/SmartContractVerifierServer/negotiation/responses/GetAgreementDetailsResponse.kt
@@ -3,6 +3,19 @@ package com.savannasolutions.SmartContractVerifierServer.negotiation.responses
 import com.fasterxml.jackson.annotation.JsonProperty
 import com.savannasolutions.SmartContractVerifierServer.common.commonDataObjects.AgreementResponse
 import com.savannasolutions.SmartContractVerifierServer.common.commonDataObjects.ResponseStatus
+import org.springframework.restdocs.payload.FieldDescriptor
+import org.springframework.restdocs.payload.PayloadDocumentation
 
 data class GetAgreementDetailsResponse(@JsonProperty("AgreementResponse") val agreementResponse : AgreementResponse?=null,)
+{
+    companion object{
+        fun response(): ArrayList<FieldDescriptor>
+        {
+            val fieldDescriptorResponse = ArrayList<FieldDescriptor>()
+            fieldDescriptorResponse.add(PayloadDocumentation.subsectionWithPath("ResponseObject.AgreementResponse").description("This is the agreement details"))
+            fieldDescriptorResponse.addAll(AgreementResponse.response("ResponseObject.AgreementResponse"))
+            return fieldDescriptorResponse
+        }
+    }
+}
 
