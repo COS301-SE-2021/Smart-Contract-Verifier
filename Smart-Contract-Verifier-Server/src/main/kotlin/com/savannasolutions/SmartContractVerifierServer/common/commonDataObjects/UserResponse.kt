84f4f4f4b6df5d47ffdea8abcde83e443eb6fe31
Cobus Redelinghuys <u19089296@tuks.co.za>
2021-09-05 21:29:10 +0200

    Added autogenerated documentation companion object

--- Smart-Contract-Verifier-Server/src/main/kotlin/com/savannasolutions/SmartContractVerifierServer/common/commonDataObjects/UserResponse.kt
+++ Smart-Contract-Verifier-Server/src/main/kotlin/com/savannasolutions/SmartContractVerifierServer/common/commonDataObjects/UserResponse.kt
@@ -1,5 +1,16 @@
 package com.savannasolutions.SmartContractVerifierServer.common.commonDataObjects
 
 import com.fasterxml.jackson.annotation.JsonProperty
+import org.springframework.restdocs.payload.FieldDescriptor
+import org.springframework.restdocs.payload.PayloadDocumentation
 
 data class UserResponse(@JsonProperty("PublicWalletID") val PublicWalletID: String)
+{
+    companion object{
+        fun response(Path:String): ArrayList<FieldDescriptor>{
+            val fieldDescriptorResponse = ArrayList<FieldDescriptor>()
+            fieldDescriptorResponse.add(PayloadDocumentation.fieldWithPath("$Path.publicWalletID").description("This is the public wallet of the user"))
+            return fieldDescriptorResponse
+        }
+    }
+}
