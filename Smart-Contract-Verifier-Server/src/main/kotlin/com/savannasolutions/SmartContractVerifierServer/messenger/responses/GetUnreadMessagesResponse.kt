056f4b090ea251664ea31d5bc8baaf37cdfabfac
Cobus Redelinghuys <u19089296@tuks.co.za>
2021-09-05 15:28:24 +0200

    Added companion object for autogenerated documentation

--- Smart-Contract-Verifier-Server/src/main/kotlin/com/savannasolutions/SmartContractVerifierServer/messenger/responses/GetUnreadMessagesResponse.kt
+++ Smart-Contract-Verifier-Server/src/main/kotlin/com/savannasolutions/SmartContractVerifierServer/messenger/responses/GetUnreadMessagesResponse.kt
@@ -1,5 +1,25 @@
 package com.savannasolutions.SmartContractVerifierServer.messenger.responses
 
+import com.savannasolutions.SmartContractVerifierServer.common.commonDataObjects.MessageResponse
 import com.savannasolutions.SmartContractVerifierServer.common.commonDataObjects.MessagesByAgreementResponse
+import org.springframework.restdocs.payload.FieldDescriptor
+import org.springframework.restdocs.payload.PayloadDocumentation
 
 data class GetUnreadMessagesResponse(val MessageAgreementList :List<MessagesByAgreementResponse>?= emptyList())
+{
+    companion object{
+        fun response(): ArrayList<FieldDescriptor>{
+            val fieldDescriptorResponse = ArrayList<FieldDescriptor>()
+            fieldDescriptorResponse.add(PayloadDocumentation.subsectionWithPath("ResponseObject.messageAgreementList[]").description("These are the details of the messages attached to the agreement").optional())
+            fieldDescriptorResponse.addAll(MessagesByAgreementResponse.response())
+            return fieldDescriptorResponse
+        }
+
+        fun emptyResponse(): ArrayList<FieldDescriptor>{
+            val fieldDescriptorResponse = ArrayList<FieldDescriptor>()
+            fieldDescriptorResponse.add(PayloadDocumentation.subsectionWithPath("ResponseObject.messageAgreementList[]").description("These are the details of the messages attached to the agreement").optional())
+            return fieldDescriptorResponse
+        }
+    }
+}
+
