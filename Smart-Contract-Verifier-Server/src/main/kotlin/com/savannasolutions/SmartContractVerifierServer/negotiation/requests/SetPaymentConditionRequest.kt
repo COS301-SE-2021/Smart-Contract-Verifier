cedb1cb372a78970ad46a3a4324d92ab91767f0a
Cobus Redelinghuys <u19089296@tuks.co.za>
2021-09-06 21:51:22 +0200

    Added autogenerated documentation companion object

--- Smart-Contract-Verifier-Server/src/main/kotlin/com/savannasolutions/SmartContractVerifierServer/negotiation/requests/SetPaymentConditionRequest.kt
+++ Smart-Contract-Verifier-Server/src/main/kotlin/com/savannasolutions/SmartContractVerifierServer/negotiation/requests/SetPaymentConditionRequest.kt
@@ -1,7 +1,19 @@
 package com.savannasolutions.SmartContractVerifierServer.negotiation.requests
 
 import com.fasterxml.jackson.annotation.JsonProperty
+import org.springframework.restdocs.payload.FieldDescriptor
+import org.springframework.restdocs.payload.PayloadDocumentation
 import java.util.*
 
 data class SetPaymentConditionRequest(@JsonProperty("Payment") val Payment: Double,
                                       @JsonProperty("PayingUser") val PayingUser: String,)
+{
+    companion object{
+        fun request(): ArrayList<FieldDescriptor>{
+            val fieldDescriptorRequest = ArrayList<FieldDescriptor>()
+            fieldDescriptorRequest.add(PayloadDocumentation.fieldWithPath("Payment").description("This is the amount needed to be paid").type("Double"))
+            fieldDescriptorRequest.add(PayloadDocumentation.fieldWithPath("PayingUser").description("This is the user public wallet address who will be paying").type("String"))
+            return fieldDescriptorRequest
+        }
+    }
+}
