514e4cbbcde56907081d98503bc6cc73e80d6bba
Cobus Redelinghuys <u19089296@tuks.co.za>
2021-09-07 00:24:29 +0200

    Added autogenerated documentation companion object

--- Smart-Contract-Verifier-Server/src/main/kotlin/com/savannasolutions/SmartContractVerifierServer/user/responses/RetrieveUserAgreementsResponse.kt
+++ Smart-Contract-Verifier-Server/src/main/kotlin/com/savannasolutions/SmartContractVerifierServer/user/responses/RetrieveUserAgreementsResponse.kt
@@ -3,5 +3,27 @@ package com.savannasolutions.SmartContractVerifierServer.user.responses
 import com.fasterxml.jackson.annotation.JsonProperty
 import com.savannasolutions.SmartContractVerifierServer.common.commonDataObjects.AgreementResponse
 import com.savannasolutions.SmartContractVerifierServer.common.commonDataObjects.ResponseStatus
+import org.springframework.restdocs.payload.FieldDescriptor
+import org.springframework.restdocs.payload.PayloadDocumentation
+import java.io.FileDescriptor
 
 data class RetrieveUserAgreementsResponse(@JsonProperty("Agreements") val Agreements: List<AgreementResponse>? = null,)
+{
+    companion object{
+        fun response(): ArrayList<FieldDescriptor>
+        {
+            val fieldDescriptorResponse = ArrayList<FieldDescriptor>()
+            fieldDescriptorResponse.add(PayloadDocumentation.subsectionWithPath("ResponseObject.agreements[]").description("This is the list of agreements the user currently has"))
+            fieldDescriptorResponse.addAll(AgreementResponse.response("ResponseObject.agreements[]"))
+            return fieldDescriptorResponse
+        }
+
+        fun responseDurationPaymentEmpty(): ArrayList<FieldDescriptor>
+        {
+            val fieldDescriptorResponse = ArrayList<FieldDescriptor>()
+            fieldDescriptorResponse.add(PayloadDocumentation.subsectionWithPath("ResponseObject.agreements[]").description("This is the list of agreements the user currently has"))
+            fieldDescriptorResponse.addAll(AgreementResponse.responseDurationPaymentEmpty("ResponseObject.agreements[]"))
+            return fieldDescriptorResponse
+        }
+    }
+}
