6b45f95bffff6df4de6d93e8218a1bc14332e3a2
Cobus Redelinghuys <u19089296@tuks.co.za>
2021-09-07 00:07:17 +0200

    Added autogenerated documentation companion object

--- Smart-Contract-Verifier-Server/src/main/kotlin/com/savannasolutions/SmartContractVerifierServer/user/responses/RetrieveUserContactListResponse.kt
+++ Smart-Contract-Verifier-Server/src/main/kotlin/com/savannasolutions/SmartContractVerifierServer/user/responses/RetrieveUserContactListResponse.kt
@@ -3,5 +3,23 @@ package com.savannasolutions.SmartContractVerifierServer.user.responses
 import com.fasterxml.jackson.annotation.JsonProperty
 import com.savannasolutions.SmartContractVerifierServer.common.commonDataObjects.ContactListIDContactListNameResponse
 import com.savannasolutions.SmartContractVerifierServer.common.commonDataObjects.ResponseStatus
+import org.springframework.restdocs.payload.FieldDescriptor
+import org.springframework.restdocs.payload.PayloadDocumentation
 
 data class RetrieveUserContactListResponse(@JsonProperty("ContactListInfo") val ContactListInfo: List<ContactListIDContactListNameResponse>?= null,)
+{
+    companion object{
+        fun response(): ArrayList<FieldDescriptor>{
+            val fieldDescriptorResponse = ArrayList<FieldDescriptor>()
+            fieldDescriptorResponse.add(PayloadDocumentation.subsectionWithPath("ResponseObject.contactListInfo[]").description("This is the list of contact list information"))
+            fieldDescriptorResponse.addAll(ContactListIDContactListNameResponse.response("ResponseObject.contactListInfo[]"))
+            return fieldDescriptorResponse
+        }
+
+        fun responseEmpty(): ArrayList<FieldDescriptor>{
+            val fieldDescriptorResponse = ArrayList<FieldDescriptor>()
+            fieldDescriptorResponse.add(PayloadDocumentation.subsectionWithPath("ResponseObject.contactListInfo[]").description("This is the list of contact list information"))
+            return fieldDescriptorResponse
+        }
+    }
+}
