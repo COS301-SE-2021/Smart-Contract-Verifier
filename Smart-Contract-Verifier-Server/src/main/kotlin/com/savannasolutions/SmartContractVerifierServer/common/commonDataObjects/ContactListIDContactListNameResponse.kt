75b5c56a4c3c00f455b157174bc8c9cf5c76559b
Cobus Redelinghuys <u19089296@tuks.co.za>
2021-09-07 00:07:09 +0200

    Added autogenerated documentation companion object

--- Smart-Contract-Verifier-Server/src/main/kotlin/com/savannasolutions/SmartContractVerifierServer/common/commonDataObjects/ContactListIDContactListNameResponse.kt
+++ Smart-Contract-Verifier-Server/src/main/kotlin/com/savannasolutions/SmartContractVerifierServer/common/commonDataObjects/ContactListIDContactListNameResponse.kt
@@ -1,7 +1,21 @@
 package com.savannasolutions.SmartContractVerifierServer.common.commonDataObjects
 
 import com.fasterxml.jackson.annotation.JsonProperty
+import org.springframework.restdocs.payload.FieldDescriptor
+import org.springframework.restdocs.payload.PayloadDocumentation
 import java.util.*
+import kotlin.collections.ArrayList
 
 data class ContactListIDContactListNameResponse(@JsonProperty("ContactListName") val contactListName : String,
                                                 @JsonProperty("ContactListID") val contactListID: UUID)
+{
+    companion object{
+        fun response(Path: String): ArrayList<FieldDescriptor>
+        {
+            val fieldDescriptorResponse = ArrayList<FieldDescriptor>()
+            fieldDescriptorResponse.add(PayloadDocumentation.fieldWithPath("$Path.ContactListName").description("This is the name of the contact list").type("String"))
+            fieldDescriptorResponse.add(PayloadDocumentation.fieldWithPath("$Path.ContactListID").description("This is the unique ID of the contact list").type("UUID"))
+            return fieldDescriptorResponse
+        }
+    }
+}
