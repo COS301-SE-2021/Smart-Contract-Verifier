801b4a05011bfa68ca561c1426ae11564ff93487
Cobus Redelinghuys <u19089296@tuks.co.za>
2021-09-07 00:24:24 +0200

    Added autogenerated documentation companion object

--- Smart-Contract-Verifier-Server/src/main/kotlin/com/savannasolutions/SmartContractVerifierServer/common/commonDataObjects/AgreementResponse.kt
+++ Smart-Contract-Verifier-Server/src/main/kotlin/com/savannasolutions/SmartContractVerifierServer/common/commonDataObjects/AgreementResponse.kt
@@ -44,5 +44,24 @@ data class AgreementResponse(@JsonProperty("AgreementID") val agreementID: UUID,
             fieldDescriptorResponses.add(PayloadDocumentation.fieldWithPath("$Path.BlockchainID").description("This is the blockchain id for when the agreement has been moved to the blockchain").type("Long Int").optional())
             return fieldDescriptorResponses
         }
+
+        fun responseDurationPaymentEmpty(Path: String): ArrayList<FieldDescriptor>{
+
+            val fieldDescriptorResponses = ArrayList<FieldDescriptor>()
+            fieldDescriptorResponses.add(PayloadDocumentation.fieldWithPath("$Path.AgreementID").description("This is the unique ID of the agreement").type("UUID"))
+            fieldDescriptorResponses.add(PayloadDocumentation.fieldWithPath("$Path.AgreementTitle").description("This is the title of the agreement").type("String"))
+            fieldDescriptorResponses.add(PayloadDocumentation.fieldWithPath("$Path.AgreementDescription").description("This is the description of the agreement").type("String"))
+            fieldDescriptorResponses.add(PayloadDocumentation.subsectionWithPath("$Path.PartyA").description("This is the public wallet id of party A"))
+            fieldDescriptorResponses.addAll(UserResponse.response("$Path.PartyA"))
+            fieldDescriptorResponses.add(PayloadDocumentation.subsectionWithPath("$Path.PartyB").description("This is the public wallet id of party B"))
+            fieldDescriptorResponses.add(PayloadDocumentation.fieldWithPath("$Path.CreatedDate").description("This is the date that the agreement was initially proposed").type("Date").optional())
+            fieldDescriptorResponses.add(PayloadDocumentation.fieldWithPath("$Path.SealedDate").description("This is the date that the agreement was sealed on the backend").type("Date").optional())
+            fieldDescriptorResponses.add(PayloadDocumentation.fieldWithPath("$Path.MovedToBlockChain").description("This flag indicates if the agreement has been moved to the blockchain").type("Boolean").optional())
+            fieldDescriptorResponses.add(PayloadDocumentation.subsectionWithPath("$Path.Conditions[]").description("This is the possibly empty list of conditions for the agreement"))
+            fieldDescriptorResponses.addAll(ConditionResponse.response("$Path.Conditions[]"))
+            fieldDescriptorResponses.add(PayloadDocumentation.fieldWithPath("$Path.AgreementImageURL").description("This is the url to the image associated with the agreement").type("String").optional())
+            fieldDescriptorResponses.add(PayloadDocumentation.fieldWithPath("$Path.BlockchainID").description("This is the blockchain id for when the agreement has been moved to the blockchain").type("Long Int").optional())
+            return fieldDescriptorResponses
+        }
     }
 }
