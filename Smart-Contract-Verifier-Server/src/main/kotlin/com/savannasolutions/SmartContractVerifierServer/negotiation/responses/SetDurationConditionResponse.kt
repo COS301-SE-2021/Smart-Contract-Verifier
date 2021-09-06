ff1e4314b7dfbd95799bfb27c37ca68ef4acbcd5
Cobus Redelinghuys <u19089296@tuks.co.za>
2021-09-06 19:27:59 +0200

    Added autogenerated documentation companion object

--- Smart-Contract-Verifier-Server/src/main/kotlin/com/savannasolutions/SmartContractVerifierServer/negotiation/responses/SetDurationConditionResponse.kt
+++ Smart-Contract-Verifier-Server/src/main/kotlin/com/savannasolutions/SmartContractVerifierServer/negotiation/responses/SetDurationConditionResponse.kt
@@ -2,6 +2,17 @@ package com.savannasolutions.SmartContractVerifierServer.negotiation.responses
 
 import com.fasterxml.jackson.annotation.JsonProperty
 import com.savannasolutions.SmartContractVerifierServer.common.commonDataObjects.ResponseStatus
+import org.springframework.restdocs.payload.FieldDescriptor
+import org.springframework.restdocs.payload.PayloadDocumentation
 import java.util.*
 
 data class SetDurationConditionResponse(@JsonProperty("ConditionID") val conditionID: UUID? = null,)
+{
+    companion object{
+        fun response(): ArrayList<FieldDescriptor>{
+            val fieldDescriptorResponse = ArrayList<FieldDescriptor>()
+            fieldDescriptorResponse.add(PayloadDocumentation.fieldWithPath("ResponseObject.ConditionID").description("This is the unique id of the newly created duration condition").type("UUID"))
+            return fieldDescriptorResponse
+        }
+    }
+}
