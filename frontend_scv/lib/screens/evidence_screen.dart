e6ff2a45df01431734af238a0c81698d80296a8e
Kevin <kevindupreez8@gmail.com>
2021-09-07 21:49:16 +0200

    Updated Evidence screen to contain an evidence uploading panel

--- frontend_scv/lib/screens/evidence_screen.dart
+++ frontend_scv/lib/screens/evidence_screen.dart
@@ -1,10 +1,9 @@
+import 'dart:typed_data';
+
 import 'package:flutter/material.dart';
-import 'package:unison/models/global.dart';
-import 'package:unison/models/message.dart';
-import 'package:unison/services/Server/messageService.dart';
-import 'package:unison/widgets/messaging/message_input_panel.dart';
-import 'package:unison/widgets/messaging/messages_panel.dart';
+import 'package:unison/widgets/agreement/evidence_upload_panel.dart';
 import 'package:unison/widgets/miscellaneous/funky_text_widget.dart';
+import 'package:mime/mime.dart';
 
 class EvidenceScreen extends StatefulWidget {
   @override
@@ -13,30 +12,19 @@ class EvidenceScreen extends StatefulWidget {
 }
 
 class _MessagingScreenState extends State<EvidenceScreen> {
-  void initState() {
-    super.initState();
-  }
-
-  MessageService messageService = MessageService();
   @override
   Widget build(BuildContext context) {
-    print('Message Screen Build: Curr: ${Global.userAddress}');
-
     final args = ModalRoute.of(context).settings.arguments as Map;
     final agreementId = args['agreementId'];
     final partyA = args['partyA'];
     final partyB = args['partyB'];
-
-    print('PA: $partyA\nPB: $partyB');
 
     return Scaffold(
       appBar: AppBar(
         title: FunkyText('Evidence for Agreement ' + agreementId),
       ),
       // drawer: AppDrawer(),
-      body: Center(
-        child: Text('Evidence Screen Goes Here'),
-      ),
+      body: EvidenceUploadPanel(),
     );
   }
 }
