d74ae072ffa5e28969a3ca24f07d3298ef44c587
Kevin <kevindupreez8@gmail.com>
2021-09-07 11:26:19 +0200

    Formatting & Refactored widgets to new directory

--- frontend_scv/lib/screens/view_contact_screen.dart
+++ frontend_scv/lib/screens/view_contact_screen.dart
@@ -2,7 +2,7 @@
 //
 import 'package:flutter/material.dart';
 import 'package:unison/services/Server/contactService.dart';
-import 'package:unison/widgets/contact_item.dart';
+import 'package:unison/widgets/contact/contact_item.dart';
 
 class ViewContactScreen extends StatefulWidget {
   static const routeName = '/view-contact';
@@ -12,7 +12,6 @@ class ViewContactScreen extends StatefulWidget {
 }
 
 class _ViewContactScreenState extends State<ViewContactScreen> {
-
   final TextEditingController _conAddrController = TextEditingController();
   final TextEditingController _conNameController = TextEditingController();
   final ContactService cs = ContactService();
@@ -24,54 +23,60 @@ class _ViewContactScreenState extends State<ViewContactScreen> {
     super.initState();
   }
 
-
-
   Future<void> _createDial() async {
+    await showDialog(
+        context: context,
+        builder: (context) {
+          _conNameController.clear();
+          _conAddrController.clear();
+          return AlertDialog(
+            content: Container(
+              child: Column(mainAxisSize: MainAxisSize.min, children: [
+                TextFormField(
+                  validator: (value) {
+                    if (value.isEmpty) {
+                      return 'Name:';
+                    } else {
+                      return null;
+                    }
+                  },
+                  decoration: InputDecoration(labelText: 'Enter Contact Name:'),
+                  controller: _conNameController,
+                ),
+                TextFormField(
+                  validator: (value) {
+                    if (value.isEmpty) {
+                      return 'Address:';
+                    } else {
+                      return null;
+                    }
+                  },
+                  decoration:
+                      InputDecoration(labelText: 'Enter Wallet Address:'),
+                  controller: _conAddrController,
+                ),
+              ]),
+            ),
+            title: Text('Add Contact'),
+            actions: [
+              TextButton(
+                child: Text('Cancel'),
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+              ),
+              TextButton(
+                child: Text('Save'),
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+              ),
+            ],
+          );
+        });
 
-    await showDialog(context: context, builder: (context) {
-
-      _conNameController.clear();
-      _conAddrController.clear();
-      return AlertDialog(content: Container(child: Column(mainAxisSize: MainAxisSize.min, children: [
-        TextFormField(
-        validator: (value) {
-          if (value.isEmpty) {
-            return 'Name:';
-          } else {
-            return null;
-          }
-        },
-        decoration: InputDecoration(labelText: 'Enter Contact Name:'),
-        controller: _conNameController,
-      ),
-        TextFormField(
-          validator: (value) {
-            if (value.isEmpty) {
-              return 'Address:';
-            } else {
-              return null;
-            }
-          },
-          decoration: InputDecoration(labelText: 'Enter Wallet Address:'),
-          controller: _conAddrController,
-        ),
-      ] ),
-      ), title: Text('Add Contact'), actions: [TextButton(
-        child: Text('Cancel'),
-        onPressed: () {
-          Navigator.of(context).pop();
-        },
-      ),
-        TextButton(
-          child: Text('Save'),
-          onPressed: () {
-            Navigator.of(context).pop();
-          },
-        ),
-      ],);
-    });
-
-    await cs.addUser(_conAddrController.text, _conNameController.text, id); //Get from above
+    await cs.addUser(
+        _conAddrController.text, _conNameController.text, id); //Get from above
   }
 
   Widget build(BuildContext context) {
@@ -80,10 +85,10 @@ class _ViewContactScreenState extends State<ViewContactScreen> {
     name = args[1];
 
     return Scaffold(
-      appBar: AppBar(
-        title: Text(name),
-      ),
-      body: Column(
+        appBar: AppBar(
+          title: Text(name),
+        ),
+        body: Column(
           children: [
             TextButton(
               style: TextButton.styleFrom(
@@ -100,25 +105,27 @@ class _ViewContactScreenState extends State<ViewContactScreen> {
                 });
               },
             ),
-            FutureBuilder(future: cs.getContacts(id),builder: (context, AsyncSnapshot snap) {
-            if (snap.connectionState != ConnectionState.done) {
-              return CircularProgressIndicator();
-            }
-            if (snap.data == null)
-              return Text('Big problem');
+            FutureBuilder(
+                future: cs.getContacts(id),
+                builder: (context, AsyncSnapshot snap) {
+                  if (snap.connectionState != ConnectionState.done) {
+                    return CircularProgressIndicator();
+                  }
+                  if (snap.data == null) return Text('Big problem');
 
-            List<Widget> ch = [];
-            print (snap.data);
-            for (var i in snap.data) {
-              //  ch.add(Column(children: [Text(i.name), Text(i.id)]));
-              ch.add(SizedBox(height: 10));
-              ch.add(ContactItem(i, id));
-            }
-            return ListView(children: ch, shrinkWrap: true,);
-          }),
-        ],
-      )
-    );
+                  List<Widget> ch = [];
+                  print(snap.data);
+                  for (var i in snap.data) {
+                    //  ch.add(Column(children: [Text(i.name), Text(i.id)]));
+                    ch.add(SizedBox(height: 10));
+                    ch.add(ContactItem(i, id));
+                  }
+                  return ListView(
+                    children: ch,
+                    shrinkWrap: true,
+                  );
+                }),
+          ],
+        ));
   }
 }
-
