bf66b8ef355073f4c687397ae6f4dbfce8d0f3cf
Ronan <u19288736@tuks.co.za>
2021-09-07 19:09:20 +0200

    Added Evidence class, and the evidence service now makes use of it.

---
+++ frontend_scv/lib/models/evidence.dart
@@ -0,0 +1,11 @@
+//This models an item of evidence that a user may send.
+//An evidence, if you will.
+
+class Evidence {
+
+  //The party that owns the evidence
+  String owningParty;
+
+  //More members may be implemented soon
+
+}
--- frontend_scv/lib/services/Server/evidenceService.dart
+++ frontend_scv/lib/services/Server/evidenceService.dart
@@ -2,19 +2,20 @@
 //Evidence must be retrieved and stored on both the backend and possible blockchain.
 //A second, contemporary service may be added in blockchain services.
 
+import 'package:unison/models/evidence.dart';
 import 'package:unison/services/Server/backendAPI.dart';
 
 class EvidenceService {
   ApiInteraction _api = ApiInteraction();
 
   ///Store evidence on the server
-  Future<void> storeEvidence() async {
+  Future<void> storeEvidence(Evidence ev) async {
     return;
   }
 
   ///Get the evidence for an agreement
-  Future<void> getEvidence(String id) async {
+  Future<List<Evidence>> getEvidence(String id) async {
 
-    return;
+    return [];
   }
 }
