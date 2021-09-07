7df7e19324407491b1aa7921aa3551a654f6c985
Kevin <kevindupreez8@gmail.com>
2021-09-07 10:44:49 +0200

    Added copy to clipboard function for users address - includes a custom snackbar for user feedback

--- frontend_scv/lib/widgets/app_drawer.dart
+++ frontend_scv/lib/widgets/app_drawer.dart
@@ -4,22 +4,51 @@ import 'package:unison/models/global.dart';
 import 'package:unison/screens/contacts_screen.dart';
 import 'package:unison/screens/judge_duty_screen.dart';
 
+import 'package:flutter/services.dart';
+import 'package:unison/widgets/funky_text_widget.dart';
+
 import '../providers/auth.dart';
 
 class AppDrawer extends StatelessWidget {
+  final snackBar = SnackBar(
+    backgroundColor: Color.fromRGBO(56, 61, 81, 1),
+    content: Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+      Text(
+        'Address Copied to Clipboard',
+        style: TextStyle(color: Colors.pink, fontSize: 16),
+      )
+    ]),
+  );
   @override
   Widget build(BuildContext context) {
     return Drawer(
       child: Column(
         children: [
           AppBar(
-            title: Text('Unison.'),
+            title: FunkyText('Unison.'),
             automaticallyImplyLeading: false, //never adds a back button
           ),
           Divider(),
           ListTile(
             // leading: Icon(Icons.list_alt),
             title: Text(Global.userAddress),
+            trailing: IconButton(
+              color: Colors.blue,
+              onPressed: () async {
+                ClipboardData data =
+                    ClipboardData(text: '<Text to copy goes here>');
+                await Clipboard.setData(data);
+
+                Clipboard.setData(
+                  ClipboardData(text: Global.userAddress.toString()),
+                );
+                ScaffoldMessenger.of(context).showSnackBar(snackBar);
+              },
+              icon: Icon(
+                Icons.copy,
+                color: Colors.grey,
+              ),
+            ),
           ),
           Divider(),
           ListTile(
