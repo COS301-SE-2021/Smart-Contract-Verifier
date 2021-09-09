ffa8c285c0ab5506d8e869cb16ecb39dc1a79e69
Cobus Redelinghuys <u19089296@tuks.co.za>
2021-09-09 23:30:15 +0200

    Added companion object for autogenerated documentation

--- Smart-Contract-Verifier-Server/src/main/kotlin/com/savannasolutions/SmartContractVerifierServer/evidence/requests/LinkEvidenceRequest.kt
+++ Smart-Contract-Verifier-Server/src/main/kotlin/com/savannasolutions/SmartContractVerifierServer/evidence/requests/LinkEvidenceRequest.kt
@@ -1,6 +1,18 @@
 package com.savannasolutions.SmartContractVerifierServer.evidence.requests
 
 import com.fasterxml.jackson.annotation.JsonProperty
+import org.springframework.restdocs.payload.FieldDescriptor
+import org.springframework.restdocs.payload.PayloadDocumentation
 import java.util.*
+import kotlin.collections.ArrayList
 
 data class LinkEvidenceRequest(@JsonProperty("EvidenceUrl") val url: String,)
+{
+    companion object{
+        fun request(): ArrayList<FieldDescriptor>{
+            val fieldDescriptor = ArrayList<FieldDescriptor>()
+            fieldDescriptor.add(PayloadDocumentation.fieldWithPath("EvidenceUrl"))
+            return fieldDescriptor
+        }
+    }
+}
