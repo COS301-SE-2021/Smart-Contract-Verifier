5b023e3c162863e2d450ec4076a4346c7f86225f
Cobus Redelinghuys <u19089296@tuks.co.za>
2021-09-10 23:27:26 +0200

    Added function to get evidence based on hash

--- Smart-Contract-Verifier-Server/src/main/kotlin/com/savannasolutions/SmartContractVerifierServer/evidence/repositories/EvidenceRepository.kt
+++ Smart-Contract-Verifier-Server/src/main/kotlin/com/savannasolutions/SmartContractVerifierServer/evidence/repositories/EvidenceRepository.kt
@@ -1,6 +1,7 @@
 package com.savannasolutions.SmartContractVerifierServer.evidence.repositories
 
 import com.savannasolutions.SmartContractVerifierServer.evidence.models.Evidence
+import com.savannasolutions.SmartContractVerifierServer.evidence.models.EvidenceType
 import com.savannasolutions.SmartContractVerifierServer.negotiation.models.Agreements
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
@@ -8,4 +9,5 @@ import org.springframework.stereotype.Repository
 @Repository
 interface EvidenceRepository:JpaRepository<Evidence, String>{
     fun getAllByContract(agreements: Agreements) : List<Evidence>
+    fun getEvidenceByEvidenceHash(evidenceHash: String): Evidence?
 }
