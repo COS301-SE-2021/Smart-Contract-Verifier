25c1fb438e8197666d83a3a24654bd2ede7e6c95
JasonMaritz <u19053292@tuks.co.za>
2021-09-11 13:58:27 +0200

    Added blank test stubs for link evidence api tests

--- Smart-Contract-Verifier-Server/src/test/kotlin/com/savannasolutions/SmartContractVerifierServer/IntegrationTests/APIEndPoints/evidence/LinkEvidenceTest.kt
+++ Smart-Contract-Verifier-Server/src/test/kotlin/com/savannasolutions/SmartContractVerifierServer/IntegrationTests/APIEndPoints/evidence/LinkEvidenceTest.kt
@@ -1,4 +1,71 @@
 package com.savannasolutions.SmartContractVerifierServer.IntegrationTests.APIEndPoints.evidence
 
+import com.savannasolutions.SmartContractVerifierServer.contracts.repositories.JudgesRepository
+import com.savannasolutions.SmartContractVerifierServer.evidence.configuration.EvidenceConfig
+import com.savannasolutions.SmartContractVerifierServer.evidence.repositories.EvidenceRepository
+import com.savannasolutions.SmartContractVerifierServer.evidence.repositories.LinkedEvidenceRepository
+import com.savannasolutions.SmartContractVerifierServer.evidence.repositories.UploadedEvidenceRepository
+import com.savannasolutions.SmartContractVerifierServer.negotiation.models.Agreements
+import com.savannasolutions.SmartContractVerifierServer.negotiation.repositories.AgreementsRepository
+import com.savannasolutions.SmartContractVerifierServer.user.models.User
+import com.savannasolutions.SmartContractVerifierServer.user.repositories.UserRepository
+import org.junit.jupiter.api.BeforeEach
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.mock.mockito.MockBean
+import org.springframework.test.web.servlet.MockMvc
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@AutoConfigureRestDocs(outputDir = "docs/api/post/user/userID/agreement/agreementID/evidence/link")
 class LinkEvidenceTest {
+    @Autowired
+    lateinit var mockMvc : MockMvc
+
+    @MockBean
+    lateinit var agreementsRepository : AgreementsRepository
+
+    @MockBean
+    lateinit var userRepository : UserRepository
+
+    @MockBean
+    lateinit var evidenceRepository : EvidenceRepository
+
+    @MockBean
+    lateinit var linkedEvidenceRepository: LinkedEvidenceRepository
+
+    @MockBean
+    lateinit var uploadedEvidenceRepository: UploadedEvidenceRepository
+
+    @MockBean
+    lateinit var judgesRepository: JudgesRepository
+
+    private val evidenceConfig = EvidenceConfig("TEST")
+
+    private lateinit var user: User
+    private lateinit var otherUser : User
+    private lateinit var agreement: Agreements
+
+    @BeforeEach
+    fun beforeEach(){
+
+    }
+
+    fun `Link Evidence api test successfully linked`(){
+
+    }
+
+    fun `Link Evidence api test failed agreement doesn't exist`(){
+
+    }
+
+    fun `Link Evidence api test failed user doesn't exist`(){
+
+    }
+
+    fun `Link Evidence api test failed user isn't in agreement`(){
+
+    }
 }
