1c131a4f15eea7e89ece0601ffacce9489756a21
JasonMaritz <u19053292@tuks.co.za>
2021-09-11 14:52:15 +0200

    Added empty RemoveEvidence.kt testCases

--- Smart-Contract-Verifier-Server/src/test/kotlin/com/savannasolutions/SmartContractVerifierServer/IntegrationTests/APIEndPoints/evidence/RemoveEvidenceTest.kt
+++ Smart-Contract-Verifier-Server/src/test/kotlin/com/savannasolutions/SmartContractVerifierServer/IntegrationTests/APIEndPoints/evidence/RemoveEvidenceTest.kt
@@ -1,4 +1,79 @@
 package com.savannasolutions.SmartContractVerifierServer.IntegrationTests.APIEndPoints.evidence
 
+import com.savannasolutions.SmartContractVerifierServer.contracts.repositories.JudgesRepository
+import com.savannasolutions.SmartContractVerifierServer.evidence.configuration.EvidenceConfig
+import com.savannasolutions.SmartContractVerifierServer.evidence.repositories.EvidenceRepository
+import com.savannasolutions.SmartContractVerifierServer.evidence.repositories.LinkedEvidenceRepository
+import com.savannasolutions.SmartContractVerifierServer.evidence.repositories.UploadedEvidenceRepository
+import com.savannasolutions.SmartContractVerifierServer.negotiation.models.Agreements
+import com.savannasolutions.SmartContractVerifierServer.negotiation.repositories.AgreementsRepository
+import com.savannasolutions.SmartContractVerifierServer.user.models.User
+import com.savannasolutions.SmartContractVerifierServer.user.repositories.UserRepository
+import org.junit.jupiter.api.BeforeEach
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.mock.mockito.MockBean
+import org.springframework.test.web.servlet.MockMvc
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@AutoConfigureRestDocs(outputDir = "docs/api/get/user/userID/agreement/agreementID/evidence/")
 class RemoveEvidenceTest {
+    @Autowired
+    lateinit var mockMvc : MockMvc
+
+    @MockBean
+    lateinit var agreementsRepository : AgreementsRepository
+
+    @MockBean
+    lateinit var userRepository : UserRepository
+
+    @MockBean
+    lateinit var evidenceRepository : EvidenceRepository
+
+    @MockBean
+    lateinit var linkedEvidenceRepository: LinkedEvidenceRepository
+
+    @MockBean
+    lateinit var uploadedEvidenceRepository: UploadedEvidenceRepository
+
+    @MockBean
+    lateinit var judgesRepository: JudgesRepository
+
+    private val evidenceConfig = EvidenceConfig("TEST")
+
+    private lateinit var user: User
+    private lateinit var otherUser : User
+    private lateinit var agreement: Agreements
+
+    @BeforeEach
+    fun beforeEach(){
+
+    }
+
+    fun `Remove Evidence api test successful`(){
+
+    }
+
+    fun `Remove Evidence api test failed Agreement doesn't exist`(){
+
+    }
+
+    fun `Remove Evidence api test failed User doesn't exist`(){
+
+    }
+
+    fun `Remove Evidence api test failed Evidence doesn't exist`(){
+
+    }
+
+    fun `Remove Evidence api test failed user not part of agreement`(){
+
+    }
+
+    fun `Remove Evidence api test failed user not the owner of the evidence`(){
+
+    }
 }
