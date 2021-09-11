73444c54ce71cf469c8e47028cdade26f98983ad
JasonMaritz <u19053292@tuks.co.za>
2021-09-11 14:06:36 +0200

    Added blank test stubs for fetch evidence api tests

--- Smart-Contract-Verifier-Server/src/test/kotlin/com/savannasolutions/SmartContractVerifierServer/IntegrationTests/APIEndPoints/evidence/FetchEvidenceTest.kt
+++ Smart-Contract-Verifier-Server/src/test/kotlin/com/savannasolutions/SmartContractVerifierServer/IntegrationTests/APIEndPoints/evidence/FetchEvidenceTest.kt
@@ -1,4 +1,66 @@
 package com.savannasolutions.SmartContractVerifierServer.IntegrationTests.APIEndPoints.evidence
 
+import com.savannasolutions.SmartContractVerifierServer.contracts.repositories.JudgesRepository
+import com.savannasolutions.SmartContractVerifierServer.evidence.repositories.EvidenceRepository
+import com.savannasolutions.SmartContractVerifierServer.evidence.repositories.LinkedEvidenceRepository
+import com.savannasolutions.SmartContractVerifierServer.evidence.repositories.UploadedEvidenceRepository
+import com.savannasolutions.SmartContractVerifierServer.negotiation.repositories.AgreementsRepository
+import com.savannasolutions.SmartContractVerifierServer.user.repositories.UserRepository
+import org.junit.jupiter.api.BeforeEach
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.mock.mockito.MockBean
+import org.springframework.test.web.servlet.MockMvc
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@AutoConfigureRestDocs(outputDir = "docs/api/get/user/userID/agreement/agreementID/evidence")
 class FetchEvidenceTest {
+    @Autowired
+    lateinit var mockMvc : MockMvc
+
+    @MockBean
+    lateinit var agreementsRepository : AgreementsRepository
+
+    @MockBean
+    lateinit var userRepository : UserRepository
+
+    @MockBean
+    lateinit var evidenceRepository : EvidenceRepository
+
+    @MockBean
+    lateinit var linkedEvidenceRepository: LinkedEvidenceRepository
+
+    @MockBean
+    lateinit var uploadedEvidenceRepository: UploadedEvidenceRepository
+
+    @MockBean
+    lateinit var judgesRepository: JudgesRepository
+
+    @BeforeEach
+    fun beforeEach(){
+
+    }
+
+    fun `Fetch Evidence api test successful`(){
+
+    }
+
+    fun `Fetch Evidence api test failed Agreement doesn't exist`(){
+
+    }
+
+    fun `Fetch Evidence api test failed User doesn't exist`(){
+
+    }
+
+    fun `Fetch Evidence api test failed Evidence doesn't exist`(){
+
+    }
+
+    fun `Fetch Evidence api test failed user not part of agreement`(){
+
+    }
 }
