4060f9ae24e7ad1b77868ae4cef7f55d8d7ce06e
JasonMaritz <u19053292@tuks.co.za>
2021-09-14 00:43:14 +0200

    added property based signing of tokens

--- Smart-Contract-Verifier-Server/src/main/kotlin/com/savannasolutions/SmartContractVerifierServer/security/services/SecurityService.kt
+++ Smart-Contract-Verifier-Server/src/main/kotlin/com/savannasolutions/SmartContractVerifierServer/security/services/SecurityService.kt
@@ -1,11 +1,10 @@
 package com.savannasolutions.SmartContractVerifierServer.security.services
 
-import com.savannasolutions.SmartContractVerifierServer.common.ResponseStatus
+import com.savannasolutions.SmartContractVerifierServer.common.commonDataObjects.ApiResponse
+import com.savannasolutions.SmartContractVerifierServer.common.commonDataObjects.ResponseStatus
 import com.savannasolutions.SmartContractVerifierServer.security.configuration.SecurityConfig
 import com.savannasolutions.SmartContractVerifierServer.security.requests.AddUserRequest
 import com.savannasolutions.SmartContractVerifierServer.security.requests.LoginRequest
-import com.savannasolutions.SmartContractVerifierServer.security.requests.UserExistsRequest
-import com.savannasolutions.SmartContractVerifierServer.security.responses.AddUserResponse
 import com.savannasolutions.SmartContractVerifierServer.user.repositories.UserRepository
 import com.savannasolutions.SmartContractVerifierServer.security.responses.GetNonceResponse
 import com.savannasolutions.SmartContractVerifierServer.security.responses.LoginResponse
@@ -16,6 +15,7 @@ import io.jsonwebtoken.security.Keys
 import org.komputing.khash.keccak.Keccak
 import org.komputing.khash.keccak.Keccak.digest
 import org.komputing.khash.keccak.KeccakParameter
+import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.stereotype.Service
 import org.web3j.crypto.ECDSASignature
 import org.web3j.crypto.Hash
@@ -26,6 +26,7 @@ import java.util.*
 import java.util.concurrent.ThreadLocalRandom
 
 @Service
+@EnableConfigurationProperties(SecurityConfig::class)
 class SecurityService(val userRepository: UserRepository,
                       val securityConfig: SecurityConfig
 ) {
@@ -86,10 +87,13 @@ class SecurityService(val userRepository: UserRepository,
                     }
                 }
             }
-
             if(match){
                 //val secretKey = Keys.hmacShaKeyFor(securityConfig.secretKey.encodeToByteArray())
-                val jwtToken = Jwts.builder().setSubject(userId).setExpiration(Date(System.currentTimeMillis() + 10800000)).compact()
+                val jwtToken = Jwts.builder()
+                    .setSubject(userId)
+                    .setExpiration(Date(System.currentTimeMillis() + securityConfig.timeout.toLong()))
+                    .signWith(securityConfig.signingKey)
+                    .compact()
                 val newNonce = ThreadLocalRandom.current().nextLong(1000000000, 9999999999)
                 val user = userRepository.getById(userId)
                 user.nonce = newNonce
