03a0694ed68117b876e8ee2fb6dbfff407d0bf2e
Kevin <kevindupreez8@gmail.com>
2021-09-14 00:12:26 +0200

    Updated TODO list

--- frontend_scv/assets/Todo.dart
+++ frontend_scv/assets/Todo.dart
@@ -1,11 +1,11 @@
 /*
 * TODO:
 * ---------------
+* Faucet button
 * Evidence - Current Goal
 * Add payment condition to blockchain (Based on backend payment condition)
 * Fix Duration Condition Item (display it as a formatted date, SSE)
 * Touch Juror screen's Action Area
-* Jury chat --> Double Check
-* Once Vote is cast (for parties, update)
+* Allowance ---> /10^18
 * TODO: Go through all TODO's
 * */
