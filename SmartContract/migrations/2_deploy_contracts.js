1e981849abf4fb07099b9809607219e986373ea2
Kaleb-Bruwer <u19064242@tuks.co.za>
2021-09-13 22:41:35 +0200

    Faucet

--- SmartContract/migrations/2_deploy_contracts.js
+++ SmartContract/migrations/2_deploy_contracts.js
@@ -1,6 +1,7 @@
 const Verifier = artifacts.require("Verifier");
 const UnisonToken = artifacts.require("UnisonToken");
 const RandomSource = artifacts.require("Utilities/RandomSource.sol");
+const Faucet = artifacts.require("Faucet.sol")
 
 const AgreementLib = artifacts.require("Structs/AgreementLib.sol");
 
@@ -13,12 +14,16 @@ module.exports = async (deployer) => {
   await deployer.link(AgreementLib, [Verifier]);
   await deployer.deploy(Verifier, UnisonToken.address, RandomSource.address);
 
+  await deployer.deploy(Faucet, UnisonToken.address);
+
   var obj = {
     Verifier : "",
-    UnisonToken : ""
+    UnisonToken : "",
+    Faucet : ""
   }
   obj.Verifier = Verifier.address;
   obj.UnisonToken = UnisonToken.address;
+  obj.Faucet = Faucet.address;
 
   // This file will allow front end to know where the smart contracts have been deployed
   fs.writeFile("../frontend_scv/assets/JSON/address.json", JSON.stringify(obj), function(err){
