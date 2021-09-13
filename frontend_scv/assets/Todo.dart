6dea3772f238561d762a4a3a4312626de7fe8a93
Kevin <kevindupreez8@gmail.com>
2021-09-13 22:15:08 +0200

    Updated thee todois litous maximoius

--- frontend_scv/assets/Todo.dart
+++ frontend_scv/assets/Todo.dart
@@ -2,9 +2,6 @@
 * TODO:
 * ---------------
 * Evidence - Current Goal
-* Contact List
-* - Choose a contact when making an agreement
-* - Copy Addresses
 * Add payment condition to blockchain (Based on backend payment condition)
 * Touch Juror screen's Action Area
 * Jury chat --> Double Check
