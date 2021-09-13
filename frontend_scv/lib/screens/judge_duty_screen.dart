7025711fca148adfafc346c0dbd70666463708b2
Kevin <kevindupreez8@gmail.com>
2021-09-14 00:47:25 +0200

    Added Funky Text to Page title

--- frontend_scv/lib/screens/judge_duty_screen.dart
+++ frontend_scv/lib/screens/judge_duty_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:unison/models/global.dart';
 import 'package:unison/services/Server/judgeService.dart';
 import 'package:unison/widgets/jury/judge_active_cases_panel.dart';
+import 'package:unison/widgets/miscellaneous/funky_text_widget.dart';
 
 import '../widgets/miscellaneous/app_drawer.dart';
 
@@ -10,60 +11,61 @@ class JudgeDutyScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text('Judge Duty'),
-        ),
-        drawer: AppDrawer(),
-        body: Global.isJudge
-            ? Column(
+      appBar: AppBar(
+        title: FunkyText('Judge Duty'),
+      ),
+      drawer: AppDrawer(),
+      body: Global.isJudge
+          ? Column(
+              children: [
+                SizedBox(
+                  height: 20,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    SizedBox(
+                      width: 20,
+                    ),
+                    Text(
+                      'Current Assignments:',
+                      style: TextStyle(
+                        fontSize: 16,
+                        decoration: TextDecoration.underline,
+                      ),
+                    ),
+                  ],
+                ),
+                Expanded(child: JudgeActiveCasesPanel()),
+              ],
+            )
+          : Center(
+              child: Column(
                 children: [
                   SizedBox(
                     height: 20,
                   ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    children: [
-                      SizedBox(
-                        width: 20,
-                      ),
-                      Text(
-                        'Current Assignments:',
-                        style: TextStyle(
-                          fontSize: 16,
-                          decoration: TextDecoration.underline,
-                        ),
-                      ),
-                    ],
+                  Text(
+                    'You are currently not a judge. You can up to become'
+                    ' a judge with the button below:',
+                    style: TextStyle(fontSize: 18),
                   ),
-                  Expanded(child: JudgeActiveCasesPanel()),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  ElevatedButton(
+                      onPressed: () async {
+                        JudgeService jS = JudgeService();
+                        await jS.makeUserJudge();
+                        // Global.isJudge = true;
+                        Navigator.of(context).pushNamed(
+                          JudgeDutyScreen.routeName,
+                        );
+                      },
+                      child: Text('Become a Judge'))
                 ],
-              )
-            : Center(
-                child: Column(
-                  children: [
-                    SizedBox(
-                      height: 20,
-                    ),
-                    Text(
-                      'You are currently not a judge. You can up to become'
-                      ' a judge with the button below:',
-                      style: TextStyle(fontSize: 18),
-                    ),
-                    SizedBox(
-                      height: 20,
-                    ),
-                    ElevatedButton(
-                        onPressed: () async {
-                          JudgeService jS = JudgeService();
-                          await jS.makeUserJudge();
-                          // Global.isJudge = true;
-                          Navigator.of(context).pushNamed(
-                            JudgeDutyScreen.routeName,
-                          );
-                        },
-                        child: Text('Become a Judge'))
-                  ],
-                ),
-              ));
+              ),
+            ),
+    );
   }
 }
