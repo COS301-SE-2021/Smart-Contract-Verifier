c77d190518ad5b4e7fe083cd94b5e79b2ee73978
Ronan <u19288736@tuks.co.za>
2021-09-14 00:21:53 +0200

    Removed unused methos in UnisonService and added descriptions of the methods in UnisonService.

--- frontend_scv/lib/services/Blockchain/unisonService.dart
+++ frontend_scv/lib/services/Blockchain/unisonService.dart
@@ -13,6 +13,7 @@ class UnisonService {
   SmartContract _smC =
       SmartContract("JSON/_src_Verifier_sol_Verifier.abi", 'Verifier');
 
+  ///Save an agreement to the blockchain
   Future<void> saveAgreement(Contract con) async {
     var jsn = con.toJsonChain();
 
@@ -29,37 +30,45 @@ class UnisonService {
     ]);
   }
 
+  ///Accept an agreement saved on the blockchain
   Future<void> acceptAgreement(Contract con) async {
     final res = await _smC.makeWriteCall('acceptAgreement', [con.blockchainId]);
   }
 
+  ///Get an agreement from the blockchain
   Future<BlockchainAgreement> getAgreement(BigInt id) async {
 
     final res = await _smC.makeReadCall('getAgreement', [id]);
     return BlockchainAgreement.fromCHAIN(res[0]);
   }
 
+  ///Make the current user a judge
   Future<void> addJuror() async {
     //The smart contract automatically removes the necessary funds from the user.
     await _smC.makeWriteCall('addJuror', []);
   }
 
+  ///Remove the current user from the Unison judge-pool
   Future<void> removeJuror() async {
     await _smC.makeWriteCall('removeJuror', []);
   }
 
+  ///Vote on an agreement
   Future<void> jurorVote(BigInt id, int v) async {
     //Vote yes/no on an agreement
     await _smC.makeWriteCall('jurorVote', [id, BigInt.from(v)]);
   }
 
+  ///Checks if a user is a Juror
   Future<bool> isJuror(EthereumAddress add) async {
     final res = await _smC.makeReadCall('isJuror', [add]);
     return res[0]; //Temporary
   }
 
+
   //Does the party believe the agreement was fulfilled.
   //Must happen after contract expiration
+  ///Conclusion vote for a party
   Future<void> agreementFulfilled(Contract con, bool vote) async {
 
     await _voteResolution(con.blockchainId, vote ? 2 : 1);
@@ -69,6 +78,7 @@ class UnisonService {
     await _smC.makeWriteCall('voteResolution', [id, BigInt.from(v)]);
   }
 
+  ///Get the jury assigned to an agreement
   Future<dynamic> getJury(BigInt id) async {
     final res = await _smC.makeReadCall('getJury', [id]);
 
@@ -77,6 +87,7 @@ class UnisonService {
   }
 
   //A party can pay into the agreement, after it has been moved to the blockchain
+  ///Add the payment conditions to an agreement
   Future<void> addPaymentConditions(
       BigInt id, EthereumAddress address, BigInt amount) async {
     final res = await _smC.makeWriteCall('addPaymentConditions', [
@@ -88,16 +99,9 @@ class UnisonService {
 
   //Pay the platform fee for an agreement
   //Someone (anyone) has to pay the platform fee after the agreement is accepted, and that will make it active.
+  ///Pay the platform fee for an agreement
   Future<void> payPlatformFee(BigInt id) async {
     await _smC.makeWriteCall('payPlatformFee', [id]);
   }
 
-  //This method is called by the party that should make the payment in an agreement.
-  Future<void> payToBlockchain(BigInt id, BigInt amount) async {
-    await _smC.makeWriteCall('addPaymentConditions', [
-      id,
-      [Global.getContractId('Verifier')],
-      [amount]
-    ]);
-  }
 }
