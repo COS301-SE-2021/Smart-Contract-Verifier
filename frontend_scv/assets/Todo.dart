d05abe70761a4cd80972761c4590048289dd9c6b
Ronan <u19288736@tuks.co.za>
2021-09-14 11:18:22 +0200

    Updated Todo.dart

--- frontend_scv/assets/Todo.dart
+++ frontend_scv/assets/Todo.dart
@@ -6,6 +6,5 @@
 * Add payment condition to blockchain (Based on backend payment condition)
 * Fix Duration Condition Item (display it as a formatted date, SSE)
 * Touch Juror screen's Action Area
-* Allowance ---> /10^18
 * TODO: Go through all TODO's
 * */
