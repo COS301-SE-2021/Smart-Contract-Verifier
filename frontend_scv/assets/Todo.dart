a9b0af46bd0c85293b78ae641abd598eaaad2969
Kevin <kevindupreez8@gmail.com>
2021-09-14 14:59:24 +0200

    updated Todo

--- frontend_scv/assets/Todo.dart
+++ frontend_scv/assets/Todo.dart
@@ -1,10 +1,8 @@
 /*
-* TODO:
 * ---------------
-* Faucet button
 * Evidence - Current Goal
 * Add payment condition to blockchain (Based on backend payment condition)
-* Fix Duration Condition Item (display it as a formatted date, SSE)
 * Remove prints to console
+* Faucet button ?
 * TODO: Go through all TODOs
 * */
