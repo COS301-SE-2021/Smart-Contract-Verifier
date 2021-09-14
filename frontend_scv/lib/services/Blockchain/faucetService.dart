48b250998ae62aa29815645984cfea73996ab1f0
Ronan <u19288736@tuks.co.za>
2021-09-14 21:07:16 +0200

    Added FaucetService and a getToken method.

---
+++ frontend_scv/lib/services/Blockchain/faucetService.dart
@@ -0,0 +1,13 @@
+//This service is used to interact with the faucet smart contract,
+//in order to test on the Mumbai testnet
+
+import 'package:unison/services/Blockchain/smartContract.dart';
+
+class FaucetService {
+  SmartContract _smC = SmartContract('JSON/', 'Faucet');
+
+  Future<void> getToken() async {
+    await _smC.makeWriteCall('getToken', []);
+  }
+
+}
