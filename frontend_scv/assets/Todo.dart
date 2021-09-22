f7d2f5e7d8a4419b8282ee84545b8d3fc73d3f3f
Ronan <u19288736@tuks.co.za>
2021-09-22 19:09:50 +0200

    Updated TODO.dart

--- frontend_scv/assets/Todo.dart
+++ frontend_scv/assets/Todo.dart
@@ -10,4 +10,6 @@
 * SideBar (later)
 * Agreement Status/Notification (later)
 *
+* Implement SSE (https://medium.com/flutter-community/server-sent-events-sse-with-flutter-cf331f978b4f, https://pub.dev/packages/sse)
+* Add options to track UNT and switch to Mumbai (https://developerjesse.com/2021/04/22/addEthereumChain-watchAsset.html)
 * */
