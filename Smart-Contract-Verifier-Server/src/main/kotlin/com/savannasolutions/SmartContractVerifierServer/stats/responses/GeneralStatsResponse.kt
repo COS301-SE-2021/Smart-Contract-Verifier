cc7c57b23aeedac755b66c8da73ef417261226c5
Cobus Redelinghuys <u19089296@tuks.co.za>
2021-09-28 00:01:00 +0200

    Updated the response object

--- Smart-Contract-Verifier-Server/src/main/kotlin/com/savannasolutions/SmartContractVerifierServer/stats/responses/GeneralStatsResponse.kt
+++ Smart-Contract-Verifier-Server/src/main/kotlin/com/savannasolutions/SmartContractVerifierServer/stats/responses/GeneralStatsResponse.kt
@@ -5,7 +5,9 @@ import com.fasterxml.jackson.annotation.JsonProperty
 data class GeneralStatsResponse(@JsonProperty("TotalUsers") val totalUser : Int,
                                 @JsonProperty("TotalAgreements") val totalAgreements : Int,
                                 @JsonProperty("NumberOfJudges") val numberOfJudges: Int,
-                                @JsonProperty("SuccessfulAgreements") val successfulAgreements: Int,
-                                @JsonProperty("DisputedAgreements") val disputedAgreements: Int,
-                                @JsonProperty("AverageNegotiationPeriod") val averageNegotiationPeriod: Double) {
+                                @JsonProperty("SealedAgreements") val sealedAgreements: Int,
+                                @JsonProperty("UnSealedAgreements") val unsealedAgreements: Int,
+                                @JsonProperty("AverageNegotiationPeriod") val averageNegotiationPeriod: Double,
+                                @JsonProperty("ConcludedAgreements") val concludedAgreements: Int,
+                                @JsonProperty("") val disputedAgreements: Int) {
 }
