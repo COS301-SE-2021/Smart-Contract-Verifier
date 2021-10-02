5c5fd134f5f2a8628592b566f6c773a5c13e4ca9
Cobus Redelinghuys <u19089296@tuks.co.za>
2021-10-02 23:14:56 +0200

    Fixed JPA functions

--- Smart-Contract-Verifier-Server/src/main/kotlin/com/savannasolutions/SmartContractVerifierServer/negotiation/repositories/AgreementsRepository.kt
+++ Smart-Contract-Verifier-Server/src/main/kotlin/com/savannasolutions/SmartContractVerifierServer/negotiation/repositories/AgreementsRepository.kt
@@ -3,20 +3,27 @@ package com.savannasolutions.SmartContractVerifierServer.negotiation.repositorie
 import com.savannasolutions.SmartContractVerifierServer.negotiation.models.Agreements
 import com.savannasolutions.SmartContractVerifierServer.user.models.User
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
 import org.springframework.stereotype.Repository
 import java.math.BigInteger
 import java.util.*
+import kotlin.collections.ArrayList
 
 @Repository
 interface AgreementsRepository : JpaRepository<Agreements, UUID>{
     fun getAllByUsersContaining(user: User): MutableSet<Agreements>?
     fun getAgreementsByBlockchainID(index: BigInteger): Agreements?
-    fun countAgreements(): Int
+    //fun countAgreements(): Int
     fun countAgreementsByBlockchainIDNotNull(): Int
     fun countAgreementsByBlockchainIDNull(): Int
-    fun getAllBySealedDateNotNull(): List<Agreements>?= emptyList()
-    fun countAgreementsByCreatedDateBetween(beginDate: Date, endDate:Date): Int
+    //fun getAgreements(): List<Agreements> ?= emptyList()
+
+    @Query("SELECT a FROM Agreements a")
+    fun selectAll(): List<Agreements> ?= emptyList()
+
+    //fun getAgreementsBySealedDateNotNull(): List<Agreements>?= emptyList()
+    /*fun countAgreementsByCreatedDateBetween(beginDate: Date, endDate:Date): Int
     fun countAgreementsBySealedDateBetween(beginDate: Date, endDate: Date): Int
     fun countAgreementsByCreatedDateBefore(date: Date): Int
-    fun countAgreementsBySealedDateBefore(date: Date): Int
+    fun countAgreementsBySealedDateBefore(date: Date): Int*/
 }
