4c70b60da32af07ebd81b2d77ed5198622e049f0
Ronan <u19288736@tuks.co.za>
2021-10-02 13:21:15 +0200

    Added fromJSON for generalStats.

--- frontend_scv/lib/models/generalStats.dart
+++ frontend_scv/lib/models/generalStats.dart
@@ -2,7 +2,22 @@
 
 class GeneralStats {
 
+  int totalUsers;
+  int numAgreements;
+  int numJudges;
+  int numSealed;
+  int numUnsealed;
+  double averageNegPeriod;
+  int concluded;
+  int disputed;
   GeneralStats.fromJSON(Map<String, dynamic> jsn) {
-
+      totalUsers = jsn['TotalUsers'];
+      numAgreements = jsn['TotalAgreements'];
+      numJudges = jsn['NumberOfJudges'];
+      numSealed = jsn['SealedAgreements'];
+      numUnsealed = jsn['UnSealedAgreements'];
+      averageNegPeriod = jsn['AverageNegotiationPeriod'];
+      concluded = jsn['ConcludedAgreements'];
+      disputed = jsn['DisputedAgreements'];
   }
 }
