a2e89ce3a3f0ef19cbd7907ec5d9dd3d002f85ff
JasonMaritz <u19053292@tuks.co.za>
2021-10-02 19:25:13 +0200

    Trying to fix CI cors error

--- Smart-Contract-Verifier-Server/src/main/kotlin/com/savannasolutions/SmartContractVerifierServer/security/configuration/UnisonWebSecurityConfigurer.kt
+++ Smart-Contract-Verifier-Server/src/main/kotlin/com/savannasolutions/SmartContractVerifierServer/security/configuration/UnisonWebSecurityConfigurer.kt
@@ -1,12 +1,14 @@
 package com.savannasolutions.SmartContractVerifierServer.security.configuration
 
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Import
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
 import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter
+import org.springframework.web.cors.CorsConfiguration
 
 @Configuration
 @Import(SecurityConfig::class)
@@ -15,7 +17,9 @@ class UnisonWebSecurityConfigurer: WebSecurityConfigurerAdapter() {
     lateinit var securityConfig: SecurityConfig
 
     override fun configure(http: HttpSecurity?) {
-        http?.cors()?.and()?.csrf()?.disable()?.sessionManagement()?.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+        http?.cors()?.configurationSource { CorsConfiguration().applyPermitDefaultValues() }?.and()?.csrf()?.disable()?.sessionManagement()?.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
             ?.and()?.addFilterAfter(UnisonAuthorisationFilter(securityConfig), BasicAuthenticationFilter::class.java)
     }
+
+
 }
